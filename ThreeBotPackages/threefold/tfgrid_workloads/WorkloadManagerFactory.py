b80381956bdaaf8554b15773a7fa35c545016ff6
ahmedelsayed-93 <ahmed.elsayed@gig.tech>
2019-09-05 23:16:08 +0200

    add provisioning actors

--- ThreeBotPackages/threefold/tfgrid_workloads/WorkloadManagerFactory.py
+++ ThreeBotPackages/threefold/tfgrid_workloads/WorkloadManagerFactory.py
@@ -1,6 +1,7 @@
 from Jumpscale import j
 import os
 
+
 class WorkloadManagerFactory(j.baseclasses.object, j.baseclasses.testtools):
 
     __jslocation__ = "j.threebot.package.workloadmanager"
@@ -12,23 +13,23 @@ class WorkloadManagerFactory(j.baseclasses.object, j.baseclasses.testtools):
 
         """
         try:
-            tf_workloads_bcdb = j.data.bcdb.get('tf_workloads')
+            tf_workloads_bcdb = j.data.bcdb.get("tf_workloads")
         except:
-            tf_workloads_bcdb = j.data.bcdb.new('tf_workloads')
+            tf_workloads_bcdb = j.data.bcdb.new("tf_workloads")
 
         try:
-            threebot_phonebook_bcdb = j.data.bcdb.get('threebot_phonebook')
+            threebot_phonebook_bcdb = j.data.bcdb.get("threebot_phonebook")
         except:
-            threebot_phonebook_bcdb = j.data.bcdb.new('threebot_phonebook')
+            threebot_phonebook_bcdb = j.data.bcdb.new("threebot_phonebook")
 
         basepath = os.path.dirname(os.path.dirname(__file__))
-        tf_workloads_bcdb.models_add(os.path.join(basepath, 'tfgrid_workloads/models'))
-        threebot_phonebook_bcdb.models_add(os.path.join(basepath, 'phonebook/models'))
-    
+        tf_workloads_bcdb.models_add(os.path.join(basepath, "tfgrid_workloads/models"))
+        threebot_phonebook_bcdb.models_add(os.path.join(basepath, "phonebook/models"))
+
         basepath = os.path.dirname(os.path.dirname(__file__))
         self.client = j.servers.threebot.local_start_default()
-        self.client.actors.package_manager.package_add("tf_workloads", os.path.join(basepath, 'tfgrid_workloads'))
-        self.client.actors.package_manager.package_add("threebot_phonebook", os.path.join(basepath, 'phonebook'))
+        self.client.actors.package_manager.package_add("tf_workloads", os.path.join(basepath, "tfgrid_workloads"))
+        self.client.actors.package_manager.package_add("threebot_phonebook", os.path.join(basepath, "phonebook"))
 
         self.client.reload()
 
--- ThreeBotPackages/threefold/tfgrid_workloads/tests/1_base_tests.py
+++ ThreeBotPackages/threefold/tfgrid_workloads/tests/1_base_tests.py
@@ -7,13 +7,15 @@ import logging
 import os
 from Jumpscale import j
 
-def _create_theebots():
-    threebot_tids = {}
-    for name in ['customer', 'farmer1', 'farmer2', 'signer1', 'singer2']:
-        tb = j.clients.threebot.threebot_register(name)
-        threebot_tids[name] = tb.id
-    return threebot_tids
+
+# def _create_theebots():
+#     threebot_tids = {}
+#     for name in ["customer", "farmer1", "farmer2", "signer1", "singer2"]:
+#         tb = j.clients.threebot.threebot_register(name)
+#         threebot_tids[name] = tb.id
+#     return threebot_tids
+
 
 def main(self):
-    threebots_info = _create_theebots()
-
+    pass
+    # threebots_info = _create_theebots()
