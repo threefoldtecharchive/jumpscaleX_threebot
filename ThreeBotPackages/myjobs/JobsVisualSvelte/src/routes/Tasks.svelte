97e52c439b0117a78684f9fb082149197dd6ca42
Ahmed T. Youssef <xmonader@gmail.com>
2019-09-05 12:56:10 +0200

    statistics is broken needs debugging

--- ThreeBotPackages/myjobs/JobsVisualSvelte/src/routes/Tasks.svelte
+++ ThreeBotPackages/myjobs/JobsVisualSvelte/src/routes/Tasks.svelte
@@ -1,27 +1,29 @@
 <script>
-	import TasksRendering from './TasksRendering.svelte';
-	import { getJobs } from '../data';
-	import { onMount } from 'svelte';
+  import TasksRendering from "./TasksRendering.svelte";
+  import { getJobs } from "../data";
+  import { onMount } from "svelte";
 
-	let allTasks = [];
-	//Make all the states UpperCase
-	allTasks.forEach(task => {
-		task.state = task.state.toUpperCase();
-	});
+  let allTasks = [];
+  //Make all the states UpperCase
+  allTasks.forEach(task => {
+    task.state = task.state.toUpperCase();
+  });
 
-	onMount(async () => {
-		getJobs().then(function(data) {
-			if (!data) {
-				return;
-			}
-			//allTasks = JSON.parse(data).jobs;
-			allTasks = data;
-			//Make all the states UpperCase
-			allTasks.forEach(task => {
-				task.state = task.state.toUpperCase();
-			});
-		});
-	});
+  onMount(async () => {
+    getJobs().then(function(data) {
+      console.log(`DATA : ${data}`);
+
+      if (!data) {
+        return;
+      }
+      allTasks = JSON.parse(data).jobs;
+      //   allTasks = data.jobs;
+      //Make all the states UpperCase
+      allTasks.forEach(task => {
+        task.state = task.state.toUpperCase();
+      });
+    });
+  });
 </script>
 
 <style>
