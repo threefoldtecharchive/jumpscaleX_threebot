0b226ceeaa5dfc7662e1c730a5f0673987a2025a
Ahmed T. Youssef <xmonader@gmail.com>
2019-09-05 11:20:20 +0200

    update jobviz

---
+++ ThreeBotPackages/myjobs/html/JobsVisualSvelte/src/routes/Tasks.svelte
@@ -0,0 +1,33 @@
+<script>
+	import TasksRendering from './TasksRendering.svelte';
+	import { getJobs } from '../data';
+	import { onMount } from 'svelte';
+
+	let allTasks = [];
+	//Make all the states UpperCase
+	allTasks.forEach(task => {
+		task.state = task.state.toUpperCase();
+	});
+
+	onMount(async () => {
+		getJobs().then(function(data) {
+			if (!data) {
+				return;
+			}
+			//allTasks = JSON.parse(data).jobs;
+			allTasks = data;
+			//Make all the states UpperCase
+			allTasks.forEach(task => {
+				task.state = task.state.toUpperCase();
+			});
+		});
+	});
+</script>
+
+<style>
+
+</style>
+
+<!--[Header]-->
+<h1>Tasks</h1>
+<TasksRendering {allTasks} />
