a4855b72a02d86b52a0725a5f415b85b7c132d3a
serboctor <boctor.sarah@gmail.com>
2019-09-09 16:15:53 +0200

    Add actor to migrate capacity data to directory models

--- ThreeBotPackages/threefold/directory/actors/farms.py
+++ ThreeBotPackages/threefold/directory/actors/farms.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from Jumpscale import j
 
 
@@ -5,6 +7,7 @@ class farms(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
         bcdb = j.data.bcdb.get("tf_directory")
         self.farm_model = bcdb.model_get(url="tfgrid.farm.1")
+        self.node_model = bcdb.model_get(url="tfgrid.node.2")
 
     def _get_farm(self, farm_id):
         try:
@@ -80,3 +83,30 @@ class farms(j.baseclasses.threebot_actor):
                 continue
             out.farms.append(farm)
         return out
+
+    def migrate_farms(self):
+        response = j.clients.threefold_directory.client.ListFarmers()[1]
+        farms = response.json()
+
+        for farm in farms:
+            if self.farm_model.find(name=farm["name"]):
+                continue
+
+            farm_object = self.farm_model.new(farm).save()
+            nodes = j.clients.threefold_directory.client.ListCapacity(query_params={"farmer": farm["iyo_organization"]})[1].json()
+            for node in nodes:
+                if "used_resources" in node:
+                    node["used_resource"] = node["used_resources"]
+                if "reserved_resources" in node:
+                    node["reserved_resource"] = node["reserved_resources"]
+                if "total_resources":
+                    node["total_resource"] = node["total_resources"]
+                node["farmer_id"] = farm_object.id
+
+                if "updated" in node:
+                    updated = datetime.strptime(node["updated"], "%a, %d %b %Y  %X %Z")
+                    node["updated"] = updated.strftime("%d/%m/%Y %H:%M")
+                if "created" in node:
+                    created = datetime.strptime(node["created"], "%a, %d %b %Y  %X %Z")
+                    node["created"] = updated.strftime("%d/%m/%Y %H:%M")
+                self.node_model.new(node).save()
