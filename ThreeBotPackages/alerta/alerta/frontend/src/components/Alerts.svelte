176c3d0126eb9428adbd9dd2c2f4ab6523606992
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-10 11:58:07 +0200

    Handle issues related to the UI including rearrange and change the display of filters

--- ThreeBotPackages/alerta/alerta/frontend/src/components/Alerts.svelte
+++ ThreeBotPackages/alerta/alerta/frontend/src/components/Alerts.svelte
@@ -1,5 +1,6 @@
 <script>
   import { deleteAlert } from "../data";
+  import Model from "./Model.svelte"
   // import jquery from 'jquery';
 
   export let alerts;
@@ -127,88 +128,9 @@
                 </div>
               </td>
               <!--[Modal]-->
-              <div
-                class="modal fade"
-                id="modal{i}"
-                tabindex="-1"
-                role="dialog"
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">
-                        Alert Details
-                      </h5>
-                      <button
-                        type="button"
-                        class="close"
-                        data-dismiss="modal"
-                        aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      <!--[Data-Listing]-->
-                      <ul class="list-group">
-                        <li class="list-group-item">
-                          <b>Severity :</b>
-                          {myAlert.severity}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Status :</b>
-                          {myAlert.status}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Time :</b>
-                          {myAlert.time}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Dupl. :</b>
-                          {myAlert.dupl}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Environment :</b>
-                          {myAlert.environment}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Service :</b>
-                          {myAlert.service}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Resource :</b>
-                          {myAlert.resource}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Event :</b>
-                          {myAlert.event}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Value :</b>
-                          {myAlert.value}
-                        </li>
-                        <li class="list-group-item">
-                          <b>message Type :</b>
-                          {myAlert.messageType}
-                        </li>
-                        <li class="list-group-item">
-                          <b>Text :</b>
-                          {myAlert.text}
-                        </li>
-                      </ul>
-
-                    </div>
-                    <div class="modal-footer">
-                      <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-dismiss="modal">
-                        Close
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+             <div>
+             <Model {myAlert} index={i}/>
+             </div>
             </tr>
           {/each}
         </tbody>
