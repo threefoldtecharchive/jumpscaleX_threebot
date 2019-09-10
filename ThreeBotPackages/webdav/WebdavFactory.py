a781dd938b55bbda12747faf5889cc5914490ddc
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2019-09-10 18:38:20 +0200

    Fix webdav packaging
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- ThreeBotPackages/webdav/WebdavFactory.py
+++ ThreeBotPackages/webdav/WebdavFactory.py
@@ -1,13 +1,25 @@
 from Jumpscale import j
 
-from .WebdavServer import WebdavServer
 
-JSConfigs = j.application.JSBaseConfigsClass
-
-
-class WebdavFactory(JSConfigs):
+class WebdavFactory(j.baseclasses.object, j.baseclasses.testtools):
 
     __jslocation__ = "j.threebot.package.webdav"
+    def install(self):
+        server = j.servers.threebot.default
+        server.save()
+
+        package = j.tools.threebot_packages.get("webdav", path=self._dirpath, threebot_server_name=server.name)
+        package.prepare()
+        package.save()
+        self._log_info("webdav loaded")
+
+        return "OK"
+    
+    def start(self):
+        self.install()
+        server = j.servers.threebot.default
+        server.start(web=True, ssl=False)
+
 
     def test(self, name=""):
         self.client = j.servers.threebot.local_start_default()
