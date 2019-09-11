2ff36c46a0e20f949a5d8a50b2c812d274b5d075
ahmedelsayed-93 <ahmed.elsayed@gig.tech>
2019-09-11 14:26:06 +0200

    fix package file of threebot workloads actors

--- ThreeBotPackages/threefold/tfgrid_workloads/package.py
+++ ThreeBotPackages/threefold/tfgrid_workloads/package.py
@@ -1,24 +1,25 @@
 from Jumpscale import j
-
+import os
 
 class Package(j.baseclasses.threebot_package):
     def _init(self, **kwargs):
-        self.bcdb = self._package.threebot_server.bcdb_get("tf_workloads")
+        self.workloads_bcdb = self._package.threebot_server.bcdb_get("tf_workloads")
+        self.phonebook_bcdb = self._package.threebot_server.bcdb_get("threebot_phonebook")
 
     def prepare(self):
         """
         is called at install time
         :return:
         """
-        pass
+        phonebook_models_path = os.path.join(os.path.dirname(self.package_root), 'phonebook/models')
+        self.workloads_bcdb.models_add(path=self.package_root + "/models")
+        self.phonebook_bcdb.models_add(path=phonebook_models_path)
 
     def start(self):
         """
         called when the 3bot starts
         :return:
         """
-        self.bcdb.models_add(path=self.package_root + "/models")
-
         self.gedis_server.actors_add(j.sal.fs.joinPaths(self.package_root, "actors"))
 
     def stop(self):
@@ -33,5 +34,4 @@ class Package(j.baseclasses.threebot_package):
         called when the package is no longer needed and will be removed from the threebot
         :return:
         """
-        # TODO: clean up bcdb ?
-        pass
+        self.workloads_bcdb.destroy()
