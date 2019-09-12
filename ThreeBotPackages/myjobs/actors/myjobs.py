fcaea8f50b749766abf3e2f55a6c8c4bf6d6ed9b
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-12 14:04:03 +0200

    Refactoring  my jobs Gedis client myjobs.py and make it use the new models

--- ThreeBotPackages/myjobs/actors/myjobs.py
+++ ThreeBotPackages/myjobs/actors/myjobs.py
@@ -3,16 +3,15 @@ from Jumpscale import j
 
 class myjobs(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
-        self.job_model = j.servers.myjobs.model_job
+        self.jobs = j.servers.myjobs.jobs
+        self.workers = j.servers.myjobs.workers
         self.action_model = j.servers.myjobs.model_action
-        self.worker_model = j.data.bcdb.myjobs.model_get(url="jumpscale.myjobs.worker")
 
     def list_workers(self):
         def transform_worker(worker_obj):
             states_dict = dict(zip(range(5), "NEW,ERROR,BUSY,WAITING,HALTED".split(",")))
             worker_types_dict = dict(zip(range(3), "tmux,subprocess,inprocess".split(",")))
-
-            worker_dict = worker_obj._ddict
+            worker_dict = worker_obj._data._ddict
             try:
                 worker_dict["state"] = states_dict[worker_dict["state"]]
                 worker_dict["type"] = worker_types_dict[worker_dict["type"]]
@@ -21,31 +20,18 @@ class myjobs(j.baseclasses.threebot_actor):
             return worker_dict
 
         workers = j.data.serializers.json.dumps(
-            {"workers": [transform_worker(worker) for worker in self.worker_model.find()]}
+            {"workers": [transform_worker(worker) for worker in self.workers.find()]}
         )
         print("returning workers  ", workers)
         return workers
 
-    def list_jobs(self):
-        def transform_job(job_obj):
-            states_dict = dict(zip(range(5), "NEW,ERROR,OK,RUNNING,HALTED".split(",")))
-            job_dict = job_obj._ddict
+    def list_jobs(self, schema_out):
+        """
+        ```out
+        jobs = (LO) !jumpscale.myjobs.job
+        ```
+        """
 
-            try:
-                job_dict["state"] = states_dict[job_dict["state"]]
-                job_dict["args"] = str(job_dict["args"])
-                job_dict["kwargs"] = str(job_dict["kwargs"])
-                job_dict["result"] = str(job_dict["result"])
-                job_dict["error"] = str(job_dict["error"])
-            except Exception as e:
-                print(e)
-            try:
-                job_dict["action_id"] = self.action_model.get(job_dict["action_id"]).methodname
-            except Exception as e:
-                print(e)
-            return job_dict
-
-        jobs = j.data.serializers.json.dumps({"jobs": [transform_job(job) for job in self.job_model.find()]})
-        print("returning jobs  ", jobs)
-        return jobs
-        # return JOBS
+        out = schema_out.new()
+        out.jobs = self.jobs.find()
+        return out
