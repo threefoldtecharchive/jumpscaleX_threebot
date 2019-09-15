6ed9583a0073b7523be320e22e89ea7a12241a72
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-09-15 22:04:32 +0200

    Use alerthandler model

--- ThreeBotPackages/alerta/actors/alerta.py
+++ ThreeBotPackages/alerta/actors/alerta.py
@@ -1,20 +1,21 @@
+from Jumpscale.tools.alerthandler.AlertHandler import SCHEMA_ALERT
 from Jumpscale import j
-import json
+
 
 """
 
-JSX> anew.actors.alerta.list_alerts() 
+JSX> anew.actors.alerta.list_alerts()
 ... a very long list
-JSX> anew.actors.alerta.new_alert(        severity="major", 
-   2         status="new", 
-   3         time=None, 
-   4         environment="ALL", 
-   5         service="JSX", 
-   6         resource="xmonader", 
-   7         event="event 1", 
-   8         value="n/a", 
-   9         messageType="error", 
-  10         text="rafir text")                                                                                        
+JSX> anew.actors.alerta.new_alert(        severity="major",
+   2         status="new",
+   3         time=None,
+   4         environment="ALL",
+   5         service="JSX",
+   6         resource="xmonader",
+   7         event="event 1",
+   8         value="n/a",
+   9         messageType="error",
+  10         text="rafir text")
 ## actors.default.alerta.new_alert.16c54214bfcd2a5b61f789be085a1d14
 res                 : True
 
@@ -25,41 +26,7 @@ res                 : True
 
 class alerta(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
-
-        from random import choice
-
-        alert_schema = """
-        @url = jumpscale.alerta.alert
-
-        severity="" (S)
-        status="" (S)
-        time="" (S)  
-        environment = "" (S)
-        service = "" (S)
-        resource = "" (S)
-        event = "" (S)
-        value = "" (S)
-        messageType = "" (S)
-        text = "" (S)
-
-        """
-
-        self.alert_model = j.data.bcdb.system.model_get(schema=alert_schema)
-        # self.alert_model.save()
-        for i in range(100):
-            alert = self.alert_model.new()
-            alert.severity = choice(["critical", "major", "minor", "warning"])
-            alert.status = choice(["closed", "new"])
-            alert.time = j.data.time.epoch
-            alert.environment = choice(["production", "staging", "testing", "infrastructure", "all"])
-            alert.service = choice(["jsx", "0-os", "portal", "threebot"])
-            alert.resource = choice(["xmonader", "rafy", "andrew"])
-            alert.event = choice(["event 1", "event 2"])
-            alert.value = "n/a"
-            alert.messageType = choice(["error", "information", "warning"])
-            alert.text = choice(["text 1 errr", "text 2 errr", "text 3 err"])
-
-            alert.save()
+        self.alert_model = j.data.bcdb.system.model_get(schema=SCHEMA_ALERT)
 
     def list_alerts(self):
         alerts = j.data.serializers.json.dumps({"alerts": [alert._ddict for alert in self.alert_model.find()]})
@@ -95,10 +62,10 @@ class alerta(j.baseclasses.threebot_actor):
         schema_out=None,
     ):
         """
-        ```in 
+        ```in
         severity="" (S)
         status="" (S)
-        time="" (S)  
+        time="" (S)
         environment = "" (S)
         service = "" (S)
         resource = "" (S)
@@ -110,7 +77,7 @@ class alerta(j.baseclasses.threebot_actor):
 
         ```out
         res = (B)
-        ```        
+        ```
 
         """
 
