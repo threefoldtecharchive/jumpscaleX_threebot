7a61f3971933d770b9fcedb9629ed5efcbd1cafc
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-16 12:20:17 +0200

    Handle the actor to work

--- ThreeBotPackages/myjobs/actors/myjobs.py
+++ ThreeBotPackages/myjobs/actors/myjobs.py
@@ -3,15 +3,15 @@ from Jumpscale import j
 
 class myjobs(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
-        self.jobs = j.servers.myjobs.jobs
-        self.workers = j.servers.myjobs.workers
-        self.action_model = j.servers.myjobs.model_action
+        self.job_model = j.data.bcdb.myjobs.model_get(url="jumpscale.myjobs.job")
+        self.action_model = j.data.bcdb.myjobs.model_get(url="jumpscale.myjobs.action")
+        self.worker_model = j.data.bcdb.myjobs.model_get(url="jumpscale.myjobs.worker")
 
     def list_workers(self):
         def transform_worker(worker_obj):
             states_dict = dict(zip(range(5), "NEW,ERROR,BUSY,WAITING,HALTED".split(",")))
             worker_types_dict = dict(zip(range(3), "tmux,subprocess,inprocess".split(",")))
-            worker_dict = worker_obj._data._ddict
+            worker_dict = worker_obj._ddict
             try:
                 worker_dict["state"] = states_dict[worker_dict["state"]]
                 worker_dict["type"] = worker_types_dict[worker_dict["type"]]
@@ -20,18 +20,10 @@ class myjobs(j.baseclasses.threebot_actor):
             return worker_dict
 
         workers = j.data.serializers.json.dumps(
-            {"workers": [transform_worker(worker) for worker in self.workers.find()]}
+            {"workers": [transform_worker(worker) for worker in self.worker_model.find()]}
         )
         print("returning workers  ", workers)
         return workers
 
-    def list_jobs(self, schema_out):
-        """
-        ```out
-        jobs = (LO) !jumpscale.myjobs.job
-        ```
-        """
-
-        out = schema_out.new()
-        out.jobs = self.jobs.find()
-        return out
+    def list_jobs(self):
+        return j.data.serializers.json.dumps([job._ddict_hr for job in self.job_model.find()])
