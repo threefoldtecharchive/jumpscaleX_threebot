522c4f4e69b7ac9123ded745bdb757ee76a0e65e
kristof de spiegeleer <>
2019-09-16 12:31:30 +0200

    error handling working in gedis server again

--- ThreeBotPackages/examples/ibiza/actors/sailor.py
+++ ThreeBotPackages/examples/ibiza/actors/sailor.py
@@ -6,10 +6,10 @@ class sailor(j.baseclasses.threebot_actor):
     """
     """
 
-    def sail(self, msg):
+    def sail(self, msg, schema_out=None, user_session=None):
         print("sail:%s" % msg)
         return msg
 
-    def rain(self):
+    def rain(self, schema_out=None, user_session=None):
         print("rain")
         return None
