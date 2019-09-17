76daaf9d9748c5b434641573df490325e24b8351
xmonader <xmonader@gmail.com>
2019-09-17 14:02:15 +0200

    wip community page

--- ThreeBotPackages/threefold/websites/website_ui/src/components/Community.svelte
+++ ThreeBotPackages/threefold/websites/website_ui/src/components/Community.svelte
@@ -1,13 +1,17 @@
 <script>
   import { onMount } from "svelte";
   import { api } from "./api.js";
+  import Partner from "./Partner.svelte";
+  export let partners = [];
   onMount(() => {
-    console.log("onmount comm");
-
     api.getPartners().then(data => {
+      partners = data;
       console.log("data..", data);
     });
   });
 </script>
 
+{#each partners as partner}
+  <Partner {partner} />
+{:else}no partners..{/each}
 <div>COOMMUNITY page</div>
---
+++ ThreeBotPackages/threefold/websites/website_ui/src/components/Stars.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Star from "./Star.svelte";
+  export let count = 5;
+
+  $: starsArr = new Array(count);
+</script>
+
+{#if count > 0}
+  <div class="stars">
+    {#each starsArr as item}
+      <Star />
+    {/each}
+  </div>
+{/if}
