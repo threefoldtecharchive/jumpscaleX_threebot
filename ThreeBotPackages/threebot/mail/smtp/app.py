0a55bc39588c5510edc1a0eeb36d03852fe8f812
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-18 16:06:00 +0200

    Make SMTP store the mail data into BCDB smtp instance (proof of concept)

--- ThreeBotPackages/threebot/mail/smtp/app.py
+++ ThreeBotPackages/threebot/mail/smtp/app.py
@@ -6,6 +6,7 @@ from .gsmtpd import SMTPServer
 class MailServer(SMTPServer):
     # Do something with the gathered message
     def process_message(self, peer, mailfrom, rcpttos, data):
+        j.debug()
         inheaders = 1
         lines = data.split("\n")
         print("---------- MESSAGE FOLLOWS ----------")
@@ -16,3 +17,15 @@ class MailServer(SMTPServer):
                 inheaders = 0
             print(line)
         print("------------ END MESSAGE ------------")
+        self.store_mail(mailfrom, rcpttos, data)
+        print("------------ Data saved In bcdb ------------")
+
+    def store_mail(self, mailform, rcpttos, data):
+        smtpInstance = j.data.bcdb.get("smtp")
+        mail_model = smtpInstance.model_get(url="jumpscale.smtp.models")
+        mail = mail_model.new()
+        mail.sender = mailform
+        mail.receiver = rcpttos
+        mail.body = data
+        mail.save()
+
