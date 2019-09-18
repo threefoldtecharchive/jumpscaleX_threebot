6f9a7d5bc91bf11677836e7afba7799b6cc7fb59
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-18 13:11:25 +0200

    Make smtp server works directly from the Factory to solve some issues and make it work

--- ThreeBotPackages/threebot/mail/smtp/SmtpdFactory.py
+++ ThreeBotPackages/threebot/mail/smtp/SmtpdFactory.py
@@ -1,11 +1,24 @@
 from Jumpscale import j
 
-JSConfigs = j.application.JSBaseConfigsClass
 
-
-class SmtpdFactory(JSConfigs):
+class SmtpdFactory(j.baseclasses.object, j.baseclasses.testtools):
 
     __jslocation__ = "j.threebot.package.smtp"
+
+    def start(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        # TODO: ADD REVERSE PROXY
+
+        # Couldn't import file app.py directly!
+        from .app import MailServer
+
+        rack = j.servers.rack.get()
+        server = MailServer(("0.0.0.0", 7002))
+        rack.add(name="smtp", server=server)
+        rack.start()
 
     def test(self, name=""):
         self.client = j.servers.threebot.local_start_default()
