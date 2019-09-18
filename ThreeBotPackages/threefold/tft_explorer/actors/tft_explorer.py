663b1c1c7cd637f9d43cfab743f5e2912b2dac06
Benjamin <benjamin.mateo@protonmail.com>
2019-09-18 15:31:50 +0200

    tftexplorer test

--- ThreeBotPackages/threefold/tft_explorer/actors/tft_explorer.py
+++ ThreeBotPackages/threefold/tft_explorer/actors/tft_explorer.py
@@ -10,24 +10,20 @@ class tft_explorer(j.baseclasses.threebot_actor):
         self.tft_ex_w = bcdb.model_get(url="tft.explorer.wallet.1")
 
     def data_dump_transaction(self, transaction_data, schema_out=None, user_session=None):
-        """
-        ```in
-        transaction_data = (O) !tft.explorer.transaction.1
-        ```
 
-        ```out
-        !tft.explorer.transaction.1
-        ```
+        # we can't use schema_out because it is not linked to a bcdb
+        # neither to a namespace id
+        # t = schema_out.new()
+        print("****************************** transaction_data:%s " % transaction_data)
+        transaction_data = j.data.serializers.json.loads(transaction_data)
+        t = self.tft_ex_t.new()
+        t.from_addr = transaction_data["from_addr"]
+        t.to_addr = transaction_data["to_addr"]
+        t.amount = transaction_data["amount"]
+        print("******************************t:%s   " % t)
 
-        """
-        t = schema_out.new()
-        print("******************************model:%s schema_out:%s t:%s" % (self.tft_ex_t, schema_out, t))
-
-        t.from_addr = transaction_data.from_addr
-        t.to_addr = transaction_data.to_addr
-        t.amount = transaction_data.amount
-        self.tft_ex_t.set(t)
-        return t
+        r = self.tft_ex_t.set(t)
+        return r
 
     def get_transactions(self, schema_out=None, user_session=None):
         ts = j.data.serializers.json.dumps({"transactions": [t for t in self.tft_ex_t.find()]})
