1bba3dcd8877ca09592f1f14db148190fda8973f
gmachtel <30384423+gmachtel@users.noreply.github.com>
2019-09-20 13:35:48 +0200

    Update browser.py

--- ThreeBotPackages/threebot/ffbrowser/actors/browser.py
+++ ThreeBotPackages/threebot/ffbrowser/actors/browser.py
@@ -7,18 +7,6 @@ class tft_explorer(j.baseclasses.threebot_actor):
     def _init(self, *args, **kwargs):
         bcdb = j.data.bcdb.get("ffbrowser")
         self.tft_ex_t = bcdb.model_get(url="ffbrowser.transaction.1")
-
-    def launch(self, event, schema_out=None, user_session=None):
-        """
-        ```in
-        event = (O) !calendar.people.1
-        ```
-
-        ```out
-        !calendar.people.1
-        ```
-
-        """
     
     def get_cookie(self, text, schema_out=None, user_session=None):
 
