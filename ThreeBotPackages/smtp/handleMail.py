df022c335117d43c77a78fc654720ede67dd30de
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-09-22 17:08:57 +0200

    Save  the mail into BCDB

--- ThreeBotPackages/smtp/handleMail.py
+++ ThreeBotPackages/smtp/handleMail.py
@@ -22,61 +22,64 @@ def parse_email_body(body):
 
     message = email.message_from_string(body)
 
-    if not message.is_multipart():
-        return message.get_payload(decode=True).decode(), []
+    # if not message.is_multipart():
+    #     return message.get_payload(decoTo: testing@dd.comFrom: Rafy <rafy@incubaid.com>Subject: trial 2Message-ID: <d0077f3f-b80b-57b3-7a0e-f5289d09f566@incubaid.com>Date: Sun, 22 Sep 2019 16:04:18 +0200User-Agent: Mozilla/5.0 (X11; Linuxx86_64; rv:60.0) Gecko/20100101 Thunderbird/60.8.0MIME-Version: 1.0Content-Type: text/plain; charset=utf-8; format=flowedContent-Transfer-Encoding: 7bitContent-Language: en-USe=True).decode(), []
 
-    to = message.get("To")
-    from_ = message.get("From")
-    subject = message.get("Subject")
-    body = ""
+    to_mail = message.get("To")
+    from_mail = message.get("From")
+    subject = message.get("Subject") if message.get("Subject") is not None else ""
+    headers = get_headers(message.items())
+    body = b""
+    html_body = b""
     attachments = []
-
     g = message.walk()
+    if message.is_multipart():
+        next(g)  # SKIP THE ROOT ONE.
 
-    next(g)  # SKIP THE ROOT ONE.
     for part in g:
         part_content_type = part.get_content_type()
-        part_body = part.get_payload()
+        part_body = part.get_payload(decode=True)
 
         part_filename = part.get_param("filename", None, "content-disposition")
 
-        # make sure to check if gmail sends 2 versions always
+        # get the body of the mail
         if part_content_type == "text/plain" and part_filename is None:
             body += part_body
 
-        elif part_content_type == "text/plain" and part_filename is not None:
-            attachments.append({"name": part_filename, "content": part_body})
+        elif part_content_type == "text/html" and part_filename is None:
+            html_body += part_body
 
-        # part_content_type = "application/octet-stream" or application/json or whatever file
-        elif part_filename is not None:
-            bhash = blake2b()
-            part_binary_content = part.get_payload(decode=True)
-            bhash.update(part_binary_content)
-            part_extension = os.path.splitext(part_filename)[1]
-            hashedfilename = bhash.hexdigest() + part_extension
+        elif part_content_type is not None and part_filename is not None:
+            attachments.append({"name": part_filename, "content": part_body, "contentType": part_content_type})
 
-            hashedfilepath = os.path.join("/attachment/test/", hashedfilename)
-
-            hashedfileurl = os.path.join("/attachment/static/", "uploads", "attachments", hashedfilename)
-
-            attachments.append(
-                Attachment(
-                    hashedfilename=hashedfilename,
-                    hashedfilepath=hashedfilepath,
-                    hashedfileurl=hashedfileurl,
-                    originalfilename=part_filename,
-                    binarycontent=part_binary_content,
-                    type=part_content_type,
-                )
-            )
-
-    return {"body": body, "attachments": attachments, "to": to, "from": from_, "subject": subject}
+    bhash = blake2b()
+    allData = body + html_body
+    bhash.update(allData)
+    hashed_data = bhash.hexdigest()
+    return {
+        "name": hashed_data,
+        "body": body.decode("ascii"),
+        "attachments": attachments,
+        "to": to_mail,
+        "from": from_mail,
+        "subject": subject,
+        "htmlbody": html_body.decode("ascii"),
+        "headers": headers,
+    }
 
 
-data = 'To: rafy@gmail.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: testing subject\nMessage-ID: <ace3555b-1715-3177-d707-ad8a982f0aeb@incubaid.com>\nDate: Thu, 19 Sep 2019 12:29:06 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: multipart/mixed;\n boundary="------------54B39785043A597FAEBBBA3E"\nContent-Language: en-US\n\nThis is a multi-part message in MIME format.\n--------------54B39785043A597FAEBBBA3E\nContent-Type: multipart/alternative;\n boundary="------------7235A5D86007E410F6A6660D"\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\n\ntesting body\n\n\n  Testing html\n\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/html; charset=utf-8\nContent-Transfer-Encoding: 7bit\n\n<html>\n  <head>\n\n    <meta http-equiv="content-type" content="text/html; charset=UTF-8">\n  </head>\n  <body text="#000000" bgcolor="#FFFFFF">\n    <p>testing body <br>\n    </p>\n    <h1>Testing html </h1>\n    <p><br>\n    </p>\n  </body>\n</html>\n\n--------------7235A5D86007E410F6A6660D--\n\n--------------54B39785043A597FAEBBBA3E\nContent-Type: text/plain; charset=UTF-8;\n name="how to scedule a job"\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment;\n filename="how to scedule a job"\n\nZnJvbSBKdW1wc2NhbGUuc2VydmVycy5teWpvYnMuTXlKb2JzRmFjdG9yeSBpbXBvcnQgYWRk\nCmouc2VydmVycy5teWpvYnMud29ya2VycwpqLnNlcnZlcnMubXlqb2JzLnNjaGVkdWxlKGFk\nZCw1NDU0NTMpCgo=\n--------------54B39785043A597FAEBBBA3E--'
+def get_headers(headers):
+    rest_headers = []
+    reserved_headers = ["To", "From", "Subject"]
+    for key, val in headers:
+        if key not in reserved_headers:
+            rest_headers.append({"Key": key, "value": val})
+    return rest_headers
 
-result = parse_email_body(data)
 
-print("done")
+# data = 'To: rafy@gmail.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: testing subject\nMessage-ID: <ace3555b-1715-3177-d707-ad8a982f0aeb@incubaid.com>\nDate: Thu, 19 Sep 2019 12:29:06 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: multipart/mixed;\n boundary="------------54B39785043A597FAEBBBA3E"\nContent-Language: en-US\n\nThis is a multi-part message in MIME format.\n--------------54B39785043A597FAEBBBA3E\nContent-Type: multipart/alternative;\n boundary="------------7235A5D86007E410F6A6660D"\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\n\ntesting body\n\n\n  Testing html\n\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/html; charset=utf-8\nContent-Transfer-Encoding: 7bit\n\n<html>\n  <head>\n\n    <meta http-equiv="content-type" content="text/html; charset=UTF-8">\n  </head>\n  <body text="#000000" bgcolor="#FFFFFF">\n    <p>testing body <br>\n    </p>\n    <h1>Testing html </h1>\n    <p><br>\n    </p>\n  </body>\n</html>\n\n--------------7235A5D86007E410F6A6660D--\n\n--------------54B39785043A597FAEBBBA3E\nContent-Type: text/plain; charset=UTF-8;\n name="how to scedule a job"\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment;\n filename="how to scedule a job"\n\nZnJvbSBKdW1wc2NhbGUuc2VydmVycy5teWpvYnMuTXlKb2JzRmFjdG9yeSBpbXBvcnQgYWRk\nCmouc2VydmVycy5teWpvYnMud29ya2VycwpqLnNlcnZlcnMubXlqb2JzLnNjaGVkdWxlKGFk\nZCw1NDU0NTMpCgo=\n--------------54B39785043A597FAEBBBA3E--'
+# data = "To: testing@dd.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: trial 2\nMessage-ID: <34cefd88-2af6-906d-0ce1-889ba50ea117@incubaid.com>\nDate: Sun, 22 Sep 2019 16:23:31 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\nContent-Language: en-US\n\ntesting the second trial\n"
+# result = parse_email_body(data)
+# print("done")
 
 # https://github.com/incubaid/crm/blob/master/crm/mailer.py
--- ThreeBotPackages/threebot/mail/smtp/app.py
+++ ThreeBotPackages/threebot/mail/smtp/app.py
@@ -7,32 +7,28 @@ from .handleMail import parse_email_body
 class MailServer(SMTPServer):
     # Do something with the gathered message
     def process_message(self, peer, mailfrom, rcpttos, data):
-        # import ipdb
-
-        # ipdb.set_trace()
-        # inheaders = 1
-        # lines = data.split("\n")
-        # print("---------- MESSAGE FOLLOWS ----------")
-        # for line in lines:
-        #     # headers first
-        #     if inheaders and not line:
-        #         print("X-Peer:", peer[0])
-        #         inheaders = 0
-        #     print(line)
-        # print("------------ END MESSAGE ------------")
         self.store_mail(data)
         print("------------ Data saved In bcdb ------------")
 
     def store_mail(self, data):
-        # j.debug()
         result = parse_email_body(data)
+        print("data")
+        print(data)
+
+        print("..................................")
         print("The result")
         print(result)
+        print("..................................")
         smtpInstance = j.data.bcdb.get("mails")
         mail_model = smtpInstance.model_get(url="jumpscale.email.message")
         mail = mail_model.new()
-        # mail.sender = mailform
-        # mail.receiver = rcpttos
-        # mail.body = data
-        # mail.save()
+        mail.name = result["name"]
+        mail.from_email = result["from"]
+        mail.to_email = result["to"]
+        mail.subject = result["subject"]
+        mail.body = result["body"]
+        mail.htmlbody = result["htmlbody"]
+        mail.headers = result["headers"]
+        mail.attachments = result["attachments"]
+        mail.save()
 
