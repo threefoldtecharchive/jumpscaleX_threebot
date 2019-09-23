2059b69808c1cfa03d70ad9e18a08fe362c96ce0
Jo De Boeck <deboeck.jo@gmail.com>
2019-09-23 10:01:05 +0200

    Small cleanups in mail
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- ThreeBotPackages/threebot/mail/handleMail.py
+++ ThreeBotPackages/threebot/mail/handleMail.py
@@ -1,10 +1,5 @@
 import email
-
-import os
-import base64
-import os.path
 from collections import namedtuple
-
 import email.utils
 from pyblake2 import blake2b
 
@@ -21,9 +16,6 @@ def parse_email_body(body):
     """
 
     message = email.message_from_string(body)
-
-    # if not message.is_multipart():
-    #     return message.get_payload(decoTo: testing@dd.comFrom: Rafy <rafy@incubaid.com>Subject: trial 2Message-ID: <d0077f3f-b80b-57b3-7a0e-f5289d09f566@incubaid.com>Date: Sun, 22 Sep 2019 16:04:18 +0200User-Agent: Mozilla/5.0 (X11; Linuxx86_64; rv:60.0) Gecko/20100101 Thunderbird/60.8.0MIME-Version: 1.0Content-Type: text/plain; charset=utf-8; format=flowedContent-Transfer-Encoding: 7bitContent-Language: en-USe=True).decode(), []
 
     to_mail = message.get("To")
     from_mail = message.get("From")
@@ -73,13 +65,14 @@ def get_headers(headers):
     reserved_headers = ["To", "From", "Subject"]
     for key, val in headers:
         if key not in reserved_headers:
-            rest_headers.append({"Key": key, "value": val})
+            rest_headers.append({"key": key, "value": val})
     return rest_headers
 
 
-# data = 'To: rafy@gmail.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: testing subject\nMessage-ID: <ace3555b-1715-3177-d707-ad8a982f0aeb@incubaid.com>\nDate: Thu, 19 Sep 2019 12:29:06 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: multipart/mixed;\n boundary="------------54B39785043A597FAEBBBA3E"\nContent-Language: en-US\n\nThis is a multi-part message in MIME format.\n--------------54B39785043A597FAEBBBA3E\nContent-Type: multipart/alternative;\n boundary="------------7235A5D86007E410F6A6660D"\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\n\ntesting body\n\n\n  Testing html\n\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/html; charset=utf-8\nContent-Transfer-Encoding: 7bit\n\n<html>\n  <head>\n\n    <meta http-equiv="content-type" content="text/html; charset=UTF-8">\n  </head>\n  <body text="#000000" bgcolor="#FFFFFF">\n    <p>testing body <br>\n    </p>\n    <h1>Testing html </h1>\n    <p><br>\n    </p>\n  </body>\n</html>\n\n--------------7235A5D86007E410F6A6660D--\n\n--------------54B39785043A597FAEBBBA3E\nContent-Type: text/plain; charset=UTF-8;\n name="how to scedule a job"\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment;\n filename="how to scedule a job"\n\nZnJvbSBKdW1wc2NhbGUuc2VydmVycy5teWpvYnMuTXlKb2JzRmFjdG9yeSBpbXBvcnQgYWRk\nCmouc2VydmVycy5teWpvYnMud29ya2VycwpqLnNlcnZlcnMubXlqb2JzLnNjaGVkdWxlKGFk\nZCw1NDU0NTMpCgo=\n--------------54B39785043A597FAEBBBA3E--'
-# data = "To: testing@dd.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: trial 2\nMessage-ID: <34cefd88-2af6-906d-0ce1-889ba50ea117@incubaid.com>\nDate: Sun, 22 Sep 2019 16:23:31 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\nContent-Language: en-US\n\ntesting the second trial\n"
-# result = parse_email_body(data)
-# print("done")
+if __name__ == "__main__":
+    data = 'To: rafy@gmail.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: testing subject\nMessage-ID: <ace3555b-1715-3177-d707-ad8a982f0aeb@incubaid.com>\nDate: Thu, 19 Sep 2019 12:29:06 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: multipart/mixed;\n boundary="------------54B39785043A597FAEBBBA3E"\nContent-Language: en-US\n\nThis is a multi-part message in MIME format.\n--------------54B39785043A597FAEBBBA3E\nContent-Type: multipart/alternative;\n boundary="------------7235A5D86007E410F6A6660D"\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\n\ntesting body\n\n\n  Testing html\n\n\n\n--------------7235A5D86007E410F6A6660D\nContent-Type: text/html; charset=utf-8\nContent-Transfer-Encoding: 7bit\n\n<html>\n  <head>\n\n    <meta http-equiv="content-type" content="text/html; charset=UTF-8">\n  </head>\n  <body text="#000000" bgcolor="#FFFFFF">\n    <p>testing body <br>\n    </p>\n    <h1>Testing html </h1>\n    <p><br>\n    </p>\n  </body>\n</html>\n\n--------------7235A5D86007E410F6A6660D--\n\n--------------54B39785043A597FAEBBBA3E\nContent-Type: text/plain; charset=UTF-8;\n name="how to scedule a job"\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment;\n filename="how to scedule a job"\n\nZnJvbSBKdW1wc2NhbGUuc2VydmVycy5teWpvYnMuTXlKb2JzRmFjdG9yeSBpbXBvcnQgYWRk\nCmouc2VydmVycy5teWpvYnMud29ya2VycwpqLnNlcnZlcnMubXlqb2JzLnNjaGVkdWxlKGFk\nZCw1NDU0NTMpCgo=\n--------------54B39785043A597FAEBBBA3E--'
+    data = "To: testing@dd.com\nFrom: Rafy <rafy@incubaid.com>\nSubject: trial 2\nMessage-ID: <34cefd88-2af6-906d-0ce1-889ba50ea117@incubaid.com>\nDate: Sun, 22 Sep 2019 16:23:31 +0200\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101\n Thunderbird/60.8.0\nMIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8; format=flowed\nContent-Transfer-Encoding: 7bit\nContent-Language: en-US\n\ntesting the second trial\n"
+    result = parse_email_body(data)
+    print("done")
 
 # https://github.com/incubaid/crm/blob/master/crm/mailer.py
