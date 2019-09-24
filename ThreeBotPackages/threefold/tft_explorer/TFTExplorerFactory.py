912cf22dc1e16161989d99f550d208a2e180b417
Benjamin <benjamin.mateo@protonmail.com>
2019-09-24 10:25:24 +0200

    tft explorer and tests

--- ThreeBotPackages/threefold/tft_explorer/TFTExplorerFactory.py
+++ ThreeBotPackages/threefold/tft_explorer/TFTExplorerFactory.py
@@ -1,4 +1,6 @@
 from Jumpscale import j
+import uuid
+import random
 
 
 class TFTExplorerFactory(j.baseclasses.object, j.baseclasses.testtools):
@@ -25,8 +27,12 @@ class TFTExplorerFactory(j.baseclasses.object, j.baseclasses.testtools):
         """
         kosmos -p 'j.threebot.package.tftexplorer.test()'
         """
-
-        self.client_get()
+        if "tft_explorer" in [b.name for b in j.data.bcdb.instances]:
+            m = j.data.bcdb.get("tft_explorer")
+        else:
+            m = j.data.bcdb.new("tft_explorer")
+        m.models_add(path=self._dirpath + "/models")
+        gedis_cli = self.client_get()
 
         cl = j.clients.redis.get(port=8901)
         assert cl.execute_command("PING")
@@ -34,28 +40,34 @@ class TFTExplorerFactory(j.baseclasses.object, j.baseclasses.testtools):
         data2 = j.data.serializers.json.dumps(data)
         data3 = cl.execute_command("default.tft_explorer.data_dump_transaction", data2)
 
-        # we can't decode the jsx data because we don't have the schema
-        # so we need to import the schema somehow ?
         data_dec = j.data.serializers.jsxdata.loads(data3)
 
         assert data_dec.amount == 3
-        assert data_dec.from_addr == "0xmkjxlhxzledhzkjhdzekhdze"
-        assert data_dec.to_addr == "54d65dez46ez4de564d6z4de3dz"
-        # bcdb = j.data.bcdb.get("tft_explorer")
-        """  tft_ex_t = bcdb.model_get(url="tft.explorer.transaction.1")
-        o = tft_ex_t.new()
-        o.from_addr = "0xmkjxlhxzledhzkjhdzekhdze"
-        o.to_addr = "54d65dez46ez4de564d6z4de3dz"
-        o.amount = 8
-        self.client.actors.tft_explorer.data_dump_transaction(o) """
-        # j.shell()
-        # e = self.client.actors.tft_explorer.data_dump_transaction(data2)
-        # z = self.client.actors.tft_explorer.data_dump_transaction(data2)
-        # x = self.client.actors.tft_explorer.get_transactions()
-        # lets now do example where we go over redis interface
+        assert data_dec.senders == ["0xmkjxlhxzledhzkjhdzekhdze"]
+        assert data_dec.recipient == "54d65dez46ez4de564d6z4de3dz"
+        trans = []
+        for x in range(10):
+            self.tft_ex_t = m.model_get(url="tft.explorer.transaction.1")
+            t = self.tft_ex_t.new()
+            t.senders = [str(uuid.uuid4().hex), str(uuid.uuid4().hex)]
+            if x % 2 == 1:
+                t.recipient = str(uuid.uuid4().hex)
+            else:
+                t.recipient = "54d65dez46ez4de564d6z4de3dz"
+            t.amount = random.uniform(0.1, 142.42)
+            trans.append(t._ddict)
+        data4 = gedis_cli.actors.tft_explorer.set_transactions(trans)
 
-        # print(name)
-        # self._test_run(name=name)
+        assert len(data4) == 10
 
+        trans = gedis_cli.actors.tft_explorer.get_transaction_by_recipient("54d65dez46ez4de564d6z4de3dz")
+        assert trans[0].recipient == "54d65dez46ez4de564d6z4de3dz"
+        assert trans[len(trans) - 1].recipient == "54d65dez46ez4de564d6z4de3dz"
+
+        t = gedis_cli.actors.tft_explorer.get(trans[0].id)
+        assert trans[0].recipient == t.recipient
+        assert trans[0].senders == t.senders
+        assert trans[0].amount == t.amount
+        print("test ok")
         self._log_info("All TESTS DONE")
         return "OK"
--- ThreeBotPackages/threefold/tft_explorer/actors/tft_explorer.py
+++ ThreeBotPackages/threefold/tft_explorer/actors/tft_explorer.py
@@ -16,17 +16,62 @@ class tft_explorer(j.baseclasses.threebot_actor):
         print("****************************** transaction_data:%s " % transaction_data)
         transaction_data = j.data.serializers.json.loads(transaction_data)
         t = self.tft_ex_t.new()
-        t.from_addr = transaction_data["from_addr"]
-        t.to_addr = transaction_data["to_addr"]
+        t.senders = [transaction_data["from_addr"]]
+        t.recipient = str(transaction_data["to_addr"])
         t.amount = transaction_data["amount"]
         print("******************************t:%s   " % t)
 
         r = self.tft_ex_t.set(t)
         return r
 
-    def get_transactions(self, schema_out=None, user_session=None):
-        ts = j.data.serializers.json.dumps({"transactions": [t for t in self.tft_ex_t.find()]})
-        return ts
+    def set_transactions(self, trans, schema_out=None, user_session=None):
+        """
+        ```in
+        trans = (LO) !tft.explorer.transaction.1
+        ```
+
+        ```out
+        success = (LO) !tft.explorer.transaction.1
+        ```
+        """
+        res = []
+        for t in trans:
+            tmp = self.tft_ex_t.new()
+            tmp.senders = t.senders
+            tmp.recipient = t.recipient
+            tmp.amount = t.amount
+            res.append(self.tft_ex_t.set(tmp))
+        return res
+
+    def get_transaction_by_recipient(self, recipient, schema_out=None, user_session=None):
+        """
+        ```in
+        recipient = (S) 
+        ```
+
+        ```out
+        trans =  (LO) !tft.explorer.transaction.1
+        ```
+        """
+        return self.tft_ex_t.find(recipient=recipient)
+
+    def get(self, transaction_id, schema_out=None, user_session=None):
+        """
+        ```in
+        transaction_id = (I)
+        ```
+
+        ```out
+        transaction = (O) !tft.explorer.transaction.1
+        ```
+        """
+        return self._get_trans(transaction_id)
+
+    def _get_trans(self, transaction_id):
+        try:
+            return self.tft_ex_t.get(transaction_id)
+        except j.exceptions.NotFound:
+            raise j.exceptions.NotFound("transaction %s not found" % transaction_id)
 
     def set_block(self, blockheight, transactions, balances):
         pass
