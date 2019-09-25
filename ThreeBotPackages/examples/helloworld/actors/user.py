96769de71e48033f97e3010b8e24e618b9d12d73
hamdy <hamdy.a.farag@gmail.com>
2019-09-25 13:38:42 +0200

    added helloworld example

---
+++ ThreeBotPackages/examples/helloworld/actors/user.py
@@ -0,0 +1,20 @@
+from Jumpscale import j
+
+
+class user(j.baseclasses.threebot_actor):
+    def _init(self, *args, **kwargs):
+        bcdb = j.data.bcdb.get('rafy')
+        self.model = bcdb.model_get(url='jumpscale.rafy.user')
+
+    def add(self, username="", schema_out=None, user_session=None):
+        """
+        ```in
+        username = (S)
+        ```
+        """
+        import ipdb; ipdb.set_trace()
+        user = self.model.new()
+        user.username = username
+        user.save()
+        response = {"result": True, "error_code": "", "error_message": ""}
+        return j.data.serializers.json.dumps(response)
