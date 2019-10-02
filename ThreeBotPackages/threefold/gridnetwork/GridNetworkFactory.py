fde132c0173919d847a7dc5a7c4d2bbf9f12b067
andrewayoub <andrew.a.mikhaeel@gmail.com>
2019-10-02 14:32:57 +0200

    use threbot base factory in gridnetwork package factory

--- ThreeBotPackages/threefold/gridnetwork/GridNetworkFactory.py
+++ ThreeBotPackages/threefold/gridnetwork/GridNetworkFactory.py
@@ -1,23 +1,6 @@
 from Jumpscale import j
 
 
-class GridNetworkFactory(j.baseclasses.object, j.baseclasses.testtools):
+class GridNetworkFactory(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.grid_network"
-
-    def install(self):
-
-        server = j.servers.threebot.default
-        server.save()
-
-        package = j.tools.threebot_packages.get("gridnetwork", path=self._dirpath)
-        package.prepare()
-        package.save()
-        self._log_info("gridnetwork package loaded")
-
-        return "OK"
-
-    def start(self):
-        self.install()
-        server = j.servers.threebot.default
-        server.start(web=False, ssl=False)
