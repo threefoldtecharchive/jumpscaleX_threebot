69ba498e9a7fbfcff13f530e40a5e490c331c4f6
kristof de spiegeleer <>
2019-10-03 16:55:30 +0200

    threebotserver starts for non web situation

--- ThreeBotPackages/threefold/phonebook/TFPhonebookFactory.py
+++ ThreeBotPackages/threefold/phonebook/TFPhonebookFactory.py
@@ -13,6 +13,7 @@ class TFPhonebookFactory(j.baseclasses.threebot_factory):
         self.client = j.servers.threebot.local_start_default(web=True)
 
         return self.client
+
     def test(self, name=""):
         """
         kosmos -p 'j.threebot.package.phonebook.test()'
