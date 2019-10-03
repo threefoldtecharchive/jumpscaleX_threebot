9a5e22e2d2e37c787750305020ca89410c85e11f
alichaddad <chaddada@greenitglobe.com>
2019-10-03 16:37:19 +0200

    radicale package actor

--- ThreeBotPackages/radicaleserver/actors/radicale.py
+++ ThreeBotPackages/radicaleserver/actors/radicale.py
@@ -1,9 +1,33 @@
+import caldav
 from Jumpscale import j
 
 
-class caldav_actor(j.baseclasses.threebot_actor):
+class radicale(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
+        self.base_url = "http://{}:{}@127.0.0.1:8851"
+        self.client = None
         pass
+
+    def login(self, username, password):
+        url = self.base_url.format(username, password)
+        client = caldav.DAVClient(url)
+        self.client = client.principal()
+
+    def _verify_client(self):
+        if not self.client:
+            raise j.exceptions.Runtime("Use login method to enable the actor")
+
+    def add_calender(self, name, cal_id=None):
+        self._verify_client()
+        calender = self.client.make_calendar(name, cal_id)
+        return j.sal.fs.getBaseName(calender.canonical_url)
+
+    def delete_calender(self, cal_id):
+        self._verify_client()
+        for calender in self.client.calendars():
+            if j.sal.fs.getBaseName(calender.canonical_url) == cal_id:
+                calender.delete()
+                break
 
     def add_event(self, username, title, content):
         """
@@ -18,5 +42,5 @@ class caldav_actor(j.baseclasses.threebot_actor):
         user_path = f"/var/lib/radicale/collections/collection-root/{username}"
         calendars = j.sal.bcdbfs.list_files_and_dirs(user_path)
         if calendars:
-            calendar = calendar[0]
+            calendar = calendars[0]
             j.sal.bcdbfs.write_file(f"{calendar}/{title}", content)
