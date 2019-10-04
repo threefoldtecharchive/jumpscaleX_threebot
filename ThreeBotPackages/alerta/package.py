e7e5fb7f69150f87821e3a696cb50a50a857772a
xmonader <xmonader@gmail.com>
2019-10-04 06:43:23 +0200

    updates for alerta

--- ThreeBotPackages/alerta/package.py
+++ ThreeBotPackages/alerta/package.py
@@ -38,6 +38,29 @@ class Package(j.baseclasses.threebot_package):
         fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
         website_location.path_location = fullpath
 
+        # if you start with a different port please enable gedis http part down.
+        website = server.get_from_port(443)
+
+        locations = website.locations.get("alerta_locations")
+
+        website_location = locations.locations_spa.new()
+        website_location.name = "alerta"
+        website_location.path_url = "/"
+        website_location.use_jumpscale_weblibs = False
+        fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+        website_location.path_location = fullpath
+
+        ## IF U WANT PER APP (specific endpoints or relative urls u can proxy the calls on ur website to :8903)
+        # PROXY for gedis HTTP
+        # proxy_location = website.locations.get().locations_proxy.new()
+        # proxy_location.name = "gedishttp"
+        # proxy_location.path_url = "/api/actors/"
+        # proxy_location.ipaddr_dest = "127.0.0.1"
+        # proxy_location.port_dest = 8903
+        # proxy_location.path_dest = "/"
+        # proxy_location.type = "http"
+        # proxy_location.scheme = "http"
+
         locations.configure()
         website.configure()
 
