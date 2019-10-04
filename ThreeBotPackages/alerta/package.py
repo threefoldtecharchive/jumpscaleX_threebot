a98a57ec00cb8e25b6dc8782958440e7a77eff40
xmonader <xmonader@gmail.com>
2019-10-04 05:59:09 +0200

    update alerta package

--- ThreeBotPackages/alerta/package.py
+++ ThreeBotPackages/alerta/package.py
@@ -27,48 +27,17 @@ class Package(j.baseclasses.threebot_package):
         server.install(reset=False)
         server.configure()
 
-        website = server.get_from_port(80)
-        website.ssl = False
+        website = server.get_from_port(443)
+
         locations = website.locations.get("alerta_locations")
 
-        website_location = locations.locations_static.new()
+        website_location = locations.locations_spa.new()
         website_location.name = "alerta"
         website_location.path_url = "/"
         website_location.use_jumpscale_weblibs = False
         fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
         website_location.path_location = fullpath
 
-        ## START BOTTLE ACTORS ENDPOINT
-        # get gedis http server
-        app = j.servers.gedishttp.get_app()
-
-        # add gedis http server to the rack
-        self.rack_server.bottle_server_add(name="gedishttp", port=9201, app=app)
-
-        # create location `/actors` to on your website `8084` to forward
-        # requests to `9201` where the bottle server is running
-        proxy_location = locations.locations_proxy.new()
-        proxy_location.name = "gedishttp"
-        proxy_location.path_url = "/actors"
-        proxy_location.ipaddr_dest = "0.0.0.0"
-        proxy_location.port_dest = 9201
-        proxy_location.scheme = "http"
-        ## END BOTTLE ACTORS ENDPOINT
-
         locations.configure()
         website.configure()
 
-    def stop(self):
-        """
-        called when the 3bot stops
-        :return:
-        """
-        pass
-
-    def uninstall(self):
-        """
-        called when the package is no longer needed and will be removed from the threebot
-        :return:
-        """
-        # TODO: clean up bcdb ?
-        pass
