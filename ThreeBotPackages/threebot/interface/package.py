a73065bb9d3f6f02d57be6a1c3b9503fa446078c
xmonader <xmonader@gmail.com>
2019-10-04 14:01:17 +0200

    update package methods stub

--- ThreeBotPackages/threebot/interface/package.py
+++ ThreeBotPackages/threebot/interface/package.py
@@ -2,6 +2,34 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
+    def _init(self, **kwargs):
+        if "branch" in kwargs.keys():
+            self.branch = kwargs["branch"]
+        else:
+            self.branch = "master"
+
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
+
+    def uninstall(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        # TODO: clean up bcdb ?
+        pass
+
+    def prepare(self):
+        """
+        is called at install time
+        :return:
+        """
+        pass
+
     def start(self):
         server = self.openresty
         server.install(reset=False)
