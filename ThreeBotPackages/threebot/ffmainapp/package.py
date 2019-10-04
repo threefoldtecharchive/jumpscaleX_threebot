c9c723889b851221401ca121b3e22276f651ac2a
xmonader <xmonader@gmail.com>
2019-10-04 14:01:17 +0200

    update package methods stub

--- ThreeBotPackages/threebot/ffmainapp/package.py
+++ ThreeBotPackages/threebot/ffmainapp/package.py
@@ -8,3 +8,24 @@ class Package(j.baseclasses.threebot_package):
                 pkg,
                 path="/sandbox/code/github/threefoldtech/jumpscaleX_threebot/ThreeBotPackages/threebot/{}/".format(pkg),
             )
+
+    def _init(self, **kwargs):
+        if "branch" in kwargs.keys():
+            self.branch = kwargs["branch"]
+        else:
+            self.branch = "master"
+
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
+
+    def uninstall(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        # TODO: clean up bcdb ?
+        pass
