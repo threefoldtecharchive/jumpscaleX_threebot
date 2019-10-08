a86e5234a1297c39102902fb5e05fa7b101e407b
Waleed Hammam <waleed.hammam@gmail.com>
2019-10-08 11:17:31 +0200

    fix paging in svelte

--- ThreeBotPackages/threebot/blog/sapper-blog/src/routes/blog/[theuser]/pages/[slug].svelte
+++ ThreeBotPackages/threebot/blog/sapper-blog/src/routes/blog/[theuser]/pages/[slug].svelte
@@ -3,7 +3,7 @@
     // the `slug` parameter is available because
     // this file is called [slug].svelte
     const res = await this.fetch(
-      `blog/${params.username}/pages/${params.slug}.json`
+      `blog/${params.theuser}/pages/${params.slug}.json`
     );
     const data = await res.json();
 
