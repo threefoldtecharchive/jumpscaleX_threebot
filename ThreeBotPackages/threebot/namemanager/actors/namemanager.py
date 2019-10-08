792526f7446fc2d8bd73719f99f39ce6d25dff47
ahmedelsayed-93 <ahmed.elsayed@gig.tech>
2019-10-08 13:16:05 +0200

    validate user signature when registering its domain

--- ThreeBotPackages/threebot/namemanager/actors/namemanager.py
+++ ThreeBotPackages/threebot/namemanager/actors/namemanager.py
@@ -1,20 +1,29 @@
 from Jumpscale import j
 
+THREEBOT_DOMAIN = "3bot.grid.tf"
+PHONEBOOK_DOMAIN = f"phonebook.{THREEBOT_DOMAIN}"
+GATEWAY_DOMAIN = f"gateway.{THREEBOT_DOMAIN}"
+
 
 class namemanager(j.baseclasses.threebot_actor):
-    def domain_register(self, doublename, privateip, user_session=None):
+    def _init(self, **kwargs):
+        self.phonebook = j.clients.gedis.get(host=PHONEBOOK_DOMAIN, port=8901)
+
+    def domain_register(self, doublename, privateip, signature, user_session=None):
         """
         Registers a domain in coredns (needs to be authoritative)
 
         ```in
         doublename = (S) # 3bot doublename
         privateip = (S)
+        signature = (S) the signature of the payload "{doublename}{privateip}"
         ```
         """
-        domain = "3bot.grid.tf"
-        gateway_domain = "gateway.3bot.grid.tf"
+        payload = f"{doublename}{privateip}"
+        self.phonebook.actors.phonebook.validate_signature(name=doublename, payload=payload, signature=signature)
+
         first, last = doublename.split(".")
-        fqdn = f"{doublename}.{domain}"
+        fqdn = f"{doublename}.{THREEBOT_DOMAIN}"
         j.tools.tf_gateway.tcpservice_register(fqdn, fqdn, privateip)
-        j.tools.tf_gateway.domain_register_cname(first, f"{last}.{domain}", gateway_domain)
+        j.tools.tf_gateway.domain_register_cname(first, f"{last}.{THREEBOT_DOMAIN}", GATEWAY_DOMAIN)
         return True
