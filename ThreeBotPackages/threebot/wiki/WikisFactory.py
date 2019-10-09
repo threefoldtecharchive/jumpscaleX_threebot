14caa3e49bfb9e7401e5510f73c78b3f64dbe693
kristof de spiegeleer <>
2019-10-09 10:33:58 +0200

    refactor

--- ThreeBotPackages/threebot/wiki/WikisFactory.py
+++ ThreeBotPackages/threebot/wiki/WikisFactory.py
@@ -1,5 +1,7 @@
 from Jumpscale import j
 
+# TODO: use bottle server to generate home page for wiki
+
 
 class WikisFactory(j.baseclasses.threebot_factory):
     __jslocation__ = "j.threebot.package.wikis"
