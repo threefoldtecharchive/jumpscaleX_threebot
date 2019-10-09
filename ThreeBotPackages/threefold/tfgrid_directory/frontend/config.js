8fc50686f09c8eb2e6fdbdc2cea4a2990632485b
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-10-09 17:39:58 +0200

    cockpit package: add frontend to package

---
+++ ThreeBotPackages/threefold/tfgrid_directory/frontend/config.js
@@ -0,0 +1,5 @@
+export default ({
+  // tfApiUrl: 'https://radar.threefold.io/api/'
+  tfApiUrl: 'https://bcdb.test.grid.tf/api/actors/',
+  tfExplorerUrl: 'https://explorer.threefoldtoken.com/explorer'
+})
