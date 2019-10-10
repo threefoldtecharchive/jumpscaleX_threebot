18f134c47b93338d1e1cb57ed3894bd930686794
Waleed Hammam <waleed.hammam@gmail.com>
2019-10-10 18:31:18 +0200

    fix sidebar images blog

--- ThreeBotPackages/threebot/blog/sapper-blog/src/components/LatestPostCard.svelte
+++ ThreeBotPackages/threebot/blog/sapper-blog/src/components/LatestPostCard.svelte
@@ -3,17 +3,35 @@
   const { preloading, page, session } = stores();
   export let username = $page.params.theuser;
   export let posts = [];
-  </script>
+
+  export function select_image(post) {
+    let post_image = post.post_image;
+    let post_image_link = "";
+    if (!post_image) {
+      post_image_link = "img/blog-post-1.jpeg";
+    } else if (!post_image.startsWith("http")) {
+      post_image_link = `/blog_${username}/assets/images/${post_image}`;
+    } else {
+      post_image_link = post_image;
+    }
+    return post_image_link;
+  }
+</script>
 
 <header>
   <h3 class="h6">Latest Posts</h3>
 </header>
+
 {#each posts as post}
   <div class="blog-posts">
     <a rel="prefetch" href="{username}/posts/{post.slug}">
       <div class="item d-flex align-items-center">
         <div class="image">
-          <img src={post.post_image} onerror="this.src = img/blog-post-1.jpeg" alt="..." class="img-fluid" />
+          <img
+            src={select_image(post)}
+            onerror="this.src = img/blog-post-1.jpeg"
+            alt="..."
+            class="img-fluid" />
         </div>
         <div class="title">
           <strong>{post.title}</strong>
