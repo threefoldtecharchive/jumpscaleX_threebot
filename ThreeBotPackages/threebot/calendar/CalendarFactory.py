6fb33ac28152aafbfea45fc0fcbabd51c98212f3
alichaddad <chaddada@greenitglobe.com>
2019-10-14 12:36:58 +0200

    update calander package with user session

--- ThreeBotPackages/threebot/calendar/CalendarFactory.py
+++ ThreeBotPackages/threebot/calendar/CalendarFactory.py
@@ -4,3 +4,9 @@ from Jumpscale import j
 class CalendarFactory(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.calendar"
+
+    def install(self):
+        j.builders.runtimes.python3.pip_package_install("filetype")
+        j.builders.runtimes.python3.pip_package_install("vobject")
+        j.builders.runtimes.python3.pip_package_install("caldav")
+        j.baseclasses.threebot_factory.install(self)
