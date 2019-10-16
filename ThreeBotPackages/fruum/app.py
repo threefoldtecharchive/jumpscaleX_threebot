e96719dc77851a6a99e6de04ae090fb462810a55
hamdy <hamdy.a.farag@gmail.com>
2019-10-16 16:41:24 +0200

    more fruum

--- ThreeBotPackages/fruum/app.py
+++ ThreeBotPackages/fruum/app.py
@@ -18,9 +18,26 @@ sio = socketio.Server(async_mode="gevent", cors_allowed_origins="*")
 appws = socketio.WSGIApp(sio)
 
 
+# decorator
+def setonline(func):
+    def inner1(sid, *args, **kwargs):
+        with sio.session(sid) as session:
+            if "user" in session:
+                user = user_model.find(username=session["user"]["username"])[0]
+                user.onboard = int(time.time())
+                user.save()
+        return func(sid, *args, **kwargs)
+
+    return inner1
+
+
 bcdb = j.data.bcdb.get("fruum")
 app_model = bcdb.model_get(url="jumpscale.fruum.application")
 doc_model = bcdb.model_get(url="jumpscale.fruum.document")
+user_model = bcdb.model_get(url="jumpscale.fruum.user")
+
+# In memory Store
+user_sessions = {}
 
 
 def get_view(doc):
@@ -28,9 +45,7 @@ def get_view(doc):
     app_name = doc.pop("app_name")
 
     doc["id"] = doc.pop("doc_id")
-
     doc_breadcrumb = doc["breadcrumb"]
-
     breadcrumb = []
 
     for bc in doc_breadcrumb:
@@ -43,6 +58,10 @@ def get_view(doc):
 
     children = []
     for d in doc_model.find(app_name=app_name, parent=doc["id"]):
+        if d.inappropriate:
+            continue
+        if d.archived:
+            continue
         d = d._ddict
         d["id"] = d.pop("doc_id")
         d.pop("app_name")
@@ -51,84 +70,22 @@ def get_view(doc):
     return {"id": doc["id"], "breadcrumb": breadcrumb, "documents": children, "online": {}}
 
 
-def get_user(username=None):
-    user = {
-        "id": username,
-        "anonymous": False,
-        "admin": True,
-        "blocked": False,
-        "username": username,
-        "displayname": username,
-        "email": "",
-        "avatar": "",
-        "created": 0,
-        "last_login": "online",
-        "last_logout": 0,
-        "onboard": 0,
-        "karma": 0,
-        "logout_karma": 0,
-        "watch": [],
-        "tags": [],
-        "notifications": [],
-        "meta": {},
-        "last_visit": 1569166490774,
-        "server_now": 1569166490774,
-    }
-    return user
-
-
-def list_users(start, size):
-
-    return [
-        {
-            "id": "hamdy",
-            "anonymous": False,
-            "admin": False,
-            "blocked": False,
-            "username": "hamdy",
-            "displayname": "hamdy",
-            "email": "",
-            "avatar": "",
-            "created": 0,
-            "last_login": 0,
-            "last_logout": 0,
-            "onboard": 0,
-            "karma": 0,
-            "logout_karma": 0,
-            "watch": [],
-            "tags": [],
-            "notifications": [],
-            "meta": {},
-            "last_visit": 1569166490774,
-            "server_now": 1569166490774,
-        },
-        {
-            "id": "aly",
-            "anonymous": False,
-            "admin": False,
-            "blocked": False,
-            "username": "aly",
-            "displayname": "aly",
-            "email": "",
-            "avatar": "",
-            "created": 0,
-            "last_login": 0,
-            "last_logout": 0,
-            "onboard": 0,
-            "karma": 0,
-            "logout_karma": 0,
-            "watch": [],
-            "tags": [],
-            "notifications": [],
-            "meta": {},
-            "last_visit": 1569166490774,
-            "server_now": 1569166490774,
-        },
-    ]
-
-
+@sio.event
 def connect(sid, environ):
     print("connect ", sid)
+
+
+@sio.event
+def disconnect(sid):
+    with sio.session(sid) as session:
+        if "user" in session:
+            user_id = session["user"]["id"]
+            if user_id in user_sessions:
+                user_sessions.pop(user_id)
+            user = user_model.get(user_id)
+            user.last_logout = int(time.time())
+            user.save()
+        print("disconnect ", sid)
 
 
 @sio.on("fruum:auth")
@@ -166,10 +123,102 @@ def auth(sid, data):
             doc.order = 0
             doc.save()
 
-        user = get_user(username=data["user"]["id"])
+            # @TODO: REMOVE ME LATER, WHEN INTEGRATING WITH 3BOT AUTH
+            # Adding dummy users
+            user1 = user_model.new()
+            user1.anonymous = False
+            user1.admin = True
+            user1.blocked = False
+            user1.username = "hamdy"
+            user1.displayname = "Hamdy Farag"
+            user1.email = "hamdy@incubaid.com"
+            user1.created = int(time.time())
+            user1.save()
 
-        session["user"] = user
-        return sio.emit("fruum:auth", {"user": user}, room=sid)
+            user2 = user_model.new()
+            user2.anonymous = False
+            user2.admin = True
+            user2.blocked = False
+            user2.username = "aly"
+            user2.displayname = "Aly Aly"
+            user2.email = "aly@incubaid.com"
+            user2.created = int(time.time())
+            user2.save()
+
+        users = user_model.find(username=data["user"]["id"])
+        if users:
+            user = users[0]
+            if user.blocked:
+                return
+            now = int(time.time())
+            user.last_login = now
+            user.onboard = now
+            user.save()
+            session["user"] = user._ddict
+            user_sessions[user.id] = sid
+            sio.emit("fruum:auth", {"user": user._ddict}, room=sid)
+
+
+@sio.on("fruum:user:block")
+def block(sid, data):
+    with sio.session(sid) as session:
+        user_id = data["id"]
+        if user_id in user_sessions:
+            user_sid = user_sessions.pop(user_id)
+            user = user_model.get(user_id)
+            user.blocked = True
+            user.last_logout = int(time.time())
+            user.save()
+            sio.emit("fruum:user:block", {"id": user_id})
+            sio.emit("disconnect", {}, room=user_sid)
+
+
+@sio.on("fruum:user:unblock")
+def unblock(sid, data):
+    with sio.session(sid) as session:
+        user_id = data["id"]
+        user = user_model.get(user_id)
+        user.blocked = False
+        user.save()
+        sio.emit("fruum:user:unblock", {"id": user_id})
+
+
+@sio.on("fruum:user:remove")
+def user_remove(sid, data):
+    with sio.session(sid) as session:
+        user_id = data["id"]
+        if user_id in user_sessions:
+            user_sid = user_sessions.pop(user_id)
+            sio.emit("disconnect", {}, room=user_sid)
+        user = user_model.get(user_id)
+        user.delete()
+        sio.emit("fruum:user:remove", {"id": user_id})
+
+
+@sio.on("fruum:user:feed")
+def feed(sid, data):
+    return "ok"
+
+
+@sio.on("fruum:user:list")
+def user_list(sid, data):
+    users = user_model.find()[data["from"] : data["size"]]
+    for i, user in enumerate(users):
+        users[i] = user._ddict
+    sio.emit("fruum:user:list", {"users": users}, room=sid)
+
+
+@sio.on("fruum:profile")
+def profile(sid, data):
+    # Anonymous
+    if "id" not in data:
+        return
+    users = user_model.find(id=data["id"])
+    if users:
+        with sio.session(sid) as session:
+            sio.emit("fruum:profile", users[0]._ddict, room=sid)
+    else:
+        sio.emit("fruum:profile", room=sid)
 
 
 @sio.on("fruum:view")
@@ -178,11 +227,11 @@ def view(sid, data):
     with sio.session(sid) as session:
         docs = doc_model.find(app_name=session["app_name"], doc_id=doc_id)
         if not docs:
-            pass
+            return
         doc = docs[0]
         if doc.type == "channel":
             sio.enter_room(sid, doc.doc_id)
-    sio.emit("fruum:view", get_view(doc), room=sid)
+        sio.emit("fruum:view", get_view(doc), room=sid)
 
 
 @sio.on("fruum:add")
@@ -208,30 +257,85 @@ def add(sid, data):
         doc.permission = data["permission"]
         doc.order = data["order"]
         parent = doc_model.find(app_name=session["app_name"], doc_id=data["parent"])[0]
+        parent.children_count += 1
+        parent.updated = now
+        parent.save()
         doc.parent_type = parent.type
-
+        doc.order = parent.children_count
         doc.breadcrumb = list(parent.breadcrumb) + [parent.doc_id]
         doc.user_id = session["user"]["id"]
         doc.user_username = session["user"]["username"]
         doc.user_displayname = session["user"]["displayname"]
         doc.user_avatar = session["user"]["avatar"]
+
         doc.save()
+
+        if doc.type == "channel":
+            sio.enter_room(sid, doc.doc_id)
 
         doc = doc._ddict
         doc["id"] = doc.pop("doc_id")
         doc.pop("app_name")
 
+        sio.emit("fruum:add", doc, room=sid)
+        sio.emit("fruum:dirty", doc)
+        sio.emit(
+            "fruum:info",
+            {
+                "id": parent.doc_id,
+                "type": parent.type,
+                "children_count": parent.children_count,
+                "updated": parent.updated,
+            },
+        )
+
         if doc["type"] == "post" and doc["parent_type"] == "channel":
             room = parent.doc_id
             sio.emit("fruum:add", doc, room=room)
-        else:
-            sio.emit("fruum:add", doc)
 
 
-@sio.on("fruum:profile")
-def profile(sid, data):
+@sio.on("fruum:archive")
+def archive(sid, data):
     with sio.session(sid) as session:
-        sio.emit("fruum:profile", session["user"], room=sid)
+        doc = doc_model.find(doc_id=data["id"])[0]
+        doc.archived = True
+        doc.save()
+        doc = doc._ddict
+        doc["id"] = doc.pop("doc_id")
+        sio.emit("fruum:archive", doc)
+
+
+@sio.on("fruum:restore")
+def archive(sid, data):
+    with sio.session(sid) as session:
+        doc = doc_model.find(doc_id=data["id"])[0]
+        doc.archived = False
+        doc.save()
+        doc = doc._ddict
+        doc["id"] = doc.pop("doc_id")
+        sio.emit("fruum:restore", doc)
+
+
+@sio.on("fruum:field")
+def archive(sid, data):
+    field = data["field"]
+    value = data["value"]
+    with sio.session(sid) as session:
+        doc = doc_model.find(app_name=session["app_name"], doc_id=data["id"])[0]
+        if field != "order":
+            setattr(doc, field, value)
+        else:
+            docs = doc_model.find(app_name=session["app_name"], parent=doc.parent)
+            for doc2 in docs:
+                if doc2.order == value:
+                    doc2.order = doc.order
+                    doc2.save()
+            doc.order = value
+            doc2.save()
+        doc.save()
+        doc = doc._ddict
+        doc["id"] = doc.pop("doc_id")
+        sio.emit("fruum:field", doc)
 
 
 @sio.on("fruum:delete")
@@ -239,23 +343,8 @@ def delete(sid, data):
     return "ok"
 
 
-@sio.on("fruum:archive")
-def archive(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:restore")
-def restore(sid, data):
-    return "ok"
-
-
 @sio.on("fruum:update")
 def update(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:field")
-def field(sid, data):
     return "ok"
 
 
@@ -291,7 +380,24 @@ def report(sid, data):
 
 @sio.on("fruum:react")
 def react(sid, data):
-    return "ok"
+    up = data["reaction"] == "up"
+    with sio.session(sid) as session:
+        doc = doc_model.find(doc_id=data["id"])[0]
+        username = session["user"]["username"]
+        if up:
+            if username in doc.react_down:
+                doc.react_down.remove(username)
+            if username not in doc.react_up:
+                doc.react_up.append(session["user"]["username"])
+        else:
+            if username in doc.react_up:
+                doc.react_up.remove(username)
+            if username not in doc.react_down:
+                doc.react_down.append(session["user"]["username"])
+        doc.save()
+        doc = doc._ddict
+        doc["id"] = doc.pop("doc_id")
+        sio.emit("fruum:react", doc)
 
 
 @sio.on("fruum:search")
@@ -316,7 +422,8 @@ def categories(sid, data):
 
 @sio.on("fruum:typing")
 def typing(sid, data):
-    sio.emit("fruum:typing", {})
+    with sio.session(sid) as session:
+        sio.emit("fruum:typing", session["user"]["username"])
 
 
 @sio.on("fruum:onboard")
@@ -327,31 +434,6 @@ def onboard(sid, data):
 @sio.on("fruum:optimize")
 def optimize(sid, data):
     return "ok"
-
-
-@sio.on("fruum:user:block")
-def block(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:user:unblock")
-def unblock(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:user:remove")
-def user_remove(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:user:feed")
-def feed(sid, data):
-    return "ok"
-
-
-@sio.on("fruum:user:list")
-def user_list(sid, data):
-    sio.emit("fruum:user:list", {"users": list_users(data["from"], data["size"])}, room=sid)
 
 
 @sio.event
