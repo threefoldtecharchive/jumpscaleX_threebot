211b8768d5154d0a6e4c9fb799cd6a132fd522ce
Jo De Boeck <deboeck.jo@gmail.com>
2019-10-22 18:24:50 +0200

    Fix when node moves out of network
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- ThreeBotPackages/threebot_provisioning/actors/provisioning.py
+++ ThreeBotPackages/threebot_provisioning/actors/provisioning.py
@@ -1,11 +1,11 @@
 from Jumpscale import j
 
 
-class provisioning_actor(j.baseclasses.threebot_actor):
+class provisioning(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
         pass
 
-    def threebot_create(self, name, do_token, do_project_name, ssh_key):
+    def threebot_create(self, name, do_token, do_project_name, ssh_key, user_session=None):
         """
         ```in
         name = (S)
@@ -25,14 +25,18 @@ class provisioning_actor(j.baseclasses.threebot_actor):
             threebot_machine.create_new_do_machine()
         threebot_machine.machine_init()
         threebot_machine.jsx_install()
+        threebot_machine.threebot_start()
+        threebot_machine.wireguard_install()
 
-    def threebot_registration(self, name, doublename):
+    def threebot_registration(self, name, doublename, email, description, user_session=None):
         """
         ```in
         name = (S)
         doublename = (S)
+        email = (S)
+        description = (S)
         ```
         """
-        threebot_machine = j.tools.threebot_deploy.get(name=name, create=False)
-
-        pass
+        threebot_machine = j.tools.threebot_deploy.get(name=name, needexist=True)
+        client = threebot_machine.threebot_client()
+        client.actors.registration.register(doublename, email, description)
