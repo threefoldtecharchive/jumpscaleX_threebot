338bcc219674c259469b1da343bc551689d943c3
0xIslamTaha <islamtaha2012@gmail.com>
2019-10-23 16:06:52 +0200

    calendar tests

--- ThreeBotPackages/threebot/tests/test_calendar_actors.py
+++ ThreeBotPackages/threebot/tests/test_calendar_actors.py
@@ -24,8 +24,30 @@ class CalenderActorsTests(BaseTest):
         gedis_client = j.clients.gedis.get("test", port=8901)
         gedis_client.actors.package_manager.package_add(
             path="/sandbox/code/github/threefoldtech/jumpscaleX_threebot/ThreeBotPackages/threebot/calendar")
-        self.actors = gedis_client.actors
         self.actors.login("admin", "admin")
+        self.actors = gedis_client.actors.addressbook
+
+    def _create_addressbook(self):
+        self.info("create new addressbook")
+        self.address_name = self.generate_random_str()
+        self.address_desc = self.generate_random_str()
+        new_addressbook_id = self.actors.add_addressbook(name=self.address_name, description=self.address_desc).decode()
+        return new_addressbook_id
+
+    def _create_vcard(self, href):
+        self.info('create new vcard')
+        vcard = """
+        begin:VCARD
+        source:ldap://cn=bjorn%20Jensen, o=university%20of%20Michigan, c=US
+        name:Bjorn Jensen
+        fn:Bj=F8rn Jensen
+        n:Jensen;Bj=F8rn
+        email;type=internet:bjorn@umich.edu
+        tel;type=work,voice,msg:+1 313 747-4454
+        key;type=x509;encoding=B:dGhpcyBjb3VsZCBiZSAKbXkgY2VydGlmaWNhdGUK
+        end:VCARD
+        """
+        return self.actors.add_vcard(vcard, href)
 
     def test001_create_addressbook(self):
         """
@@ -34,18 +56,42 @@ class CalenderActorsTests(BaseTest):
 
         :return:
         """
-        old_addressbooks = self.actors.list_addressbooks()
-        self.info("create new addressbook")
+        old_addressbooks = self.actors.list_addressbooks().addressbooks
 
-        address_name = self.generate_random_str()
-        address_desc = self.generate_random_str()
-        self.actors.add_addressbook(name=address_name, description=address_desc)
+        new_addressbook_id = self._create_addressbook()
 
-        new_addressbooks = self.actors.list_addressbooks()
+        new_addressbooks = self.actors.list_addressbooks().addressbooks
 
         self.info("assert there is a new addressbook")
         self.assertEqual(len(old_addressbooks)+1, len(new_addressbooks))
 
+    def test002_get_address_metadata(self):
+        new_addressbook_id = self._create_addressbook()
+
+        self.info("get the address book meta data")
+        description = self.actors.get_addressbook_meta(new_addressbook_id).addressbook[new_addressbook_id]["description"]
+
+        self.info("assert it has the correct value")
+        self.assertEqual(self.address_desc, description)
 
 
+    def test003_delete_addressbook(self):
+        new_addressbook_id = self._create_addressbook()
+
+        self.info("get the address book meta data")
+        self.actors.get_addressbook_meta(new_addressbook_id)
+
+        self.info("delete the address book")
+        self.actors.delete_addressbook(new_addressbook_id)
+
+        self.info("try to get the address book meta data, should fail")
+        with self.assertRaises(Exception):
+            self.actors.get_addressbook_meta(new_addressbook_id)
+
+    def test004_create_vcard(self):
+        new_addressbook_id = self._create_addressbook()
+        new_vcard_id = self._create_vcard(href=new_addressbook_id)
+
+        self.info("get vcard")
+        vcard = self.actors.get_vcard(href=new_vcard_id)
 
