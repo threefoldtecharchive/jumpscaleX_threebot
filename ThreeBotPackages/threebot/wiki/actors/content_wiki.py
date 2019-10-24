b11b4768c5fb8dc917410ce6e8cfdb737140ff5d
serboctor <boctor.sarah@gmail.com>
2019-10-24 17:41:01 +0200

    Add reload wiki actor

--- ThreeBotPackages/threebot/wiki/actors/content_wiki.py
+++ ThreeBotPackages/threebot/wiki/actors/content_wiki.py
@@ -1,14 +1,14 @@
 from Jumpscale import j
-import binascii
-from io import BytesIO
 
 
 class content_wiki(j.baseclasses.threebot_actor):
-    pass
 
+    def reload(self, name, user_session):
+        """
+        :param name: name of the wiki to reload
 
-#     def _init(self, *args, **kwargs):
-#         bcdb = j.data.bcdb.get("ffcontent")
-#
-#
-# # let's take the blog actor ?
+        ```in
+        name = (S)
+        ```
+        """
+        j.tools.markdowndocs.reload(name)
