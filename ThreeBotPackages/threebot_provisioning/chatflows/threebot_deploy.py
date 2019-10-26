445a63e565f47dbe2a3638d61f0d7d0da3483271
Jo De Boeck <deboeck.jo@gmail.com>
2019-10-26 15:50:33 +0200

    Give some 3bot depyloyment updates
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- ThreeBotPackages/threebot_provisioning/chatflows/threebot_deploy.py
+++ ThreeBotPackages/threebot_provisioning/chatflows/threebot_deploy.py
@@ -5,6 +5,17 @@ def chat(bot):
     """
     to call http://localhost:5050/chat/session/threebot_deploy
     """
+
+    progress = """\
+# Deploying your 3Bot
+{1}
+ <div class="progress">
+  <div class="progress-bar active" role="progressbar" aria-valuenow="{0}"
+  aria-valuemin="0" aria-valuemax="100" style="width:{0}%">
+    {0}%
+  </div>
+</div> 
+"""
     phonebook = j.clients.gedis.get(name="phonebook", port=8901, host="phonebook.3bot.grid.tf")
     token = j.clients.digitalocean.provisioning.token_
     question = "Please enter your 3BOT doublename: {}"
@@ -35,15 +46,21 @@ def chat(bot):
     threebot_machine = j.tools.threebot_deploy.get(
         name, do_machine_name=f"threebot-{name}", do_token=token, do_project_name="3bots"
     )
+    bot.md_show_update(progress.format(0, "Creating 3Bot"))
     if not threebot_machine.exists():
         threebot_machine.create_new_do_machine()
+    bot.md_show_update(progress.format(20, "Configuring 3Bot"))
     threebot_machine.machine_init()
+    bot.md_show_update(progress.format(30, "Installing 3Bot software"))
     threebot_machine.jsx_install()
+    bot.md_show_update(progress.format(60, "Installing 3Bot network software"))
     threebot_machine.wireguard_install()
+    bot.md_show_update(progress.format(70, "Starting 3Bot"))
     threebot_machine.threebot_start()
     print("Finished installing threebot")
     print("Start registration installing threebot")
 
+    bot.md_show_update(progress.format(80, "Registering 3Bot"))
     client = threebot_machine.threebot_client()
     client.actors.registration.register(name, email, description)
 
