8e42bf2a3dc75045279b28fd9c1090780d7791fb
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2019-10-27 17:42:43 +0200

    Oauth flow (#135)
    * oauth2 package
    
    * add chat login end point
    
    * add missing import
    
    * small fixes
    
    * add docs
    
    * update docs
    
    * improvements
    
    * use base template for views
    
    * allow dev setup
    
    * fix chat dependency

--- ThreeBotPackages/threebot/chat/ChatFactory.py
+++ ThreeBotPackages/threebot/chat/ChatFactory.py
@@ -3,11 +3,23 @@ from Jumpscale import j
 from bottle import abort, response, request, Bottle, redirect
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+try:
+    from beaker.middleware import SessionMiddleware
+except (ModuleNotFoundError, ImportError):
+    j.builders.runtimes.python3.pip_package_install("beaker")
+    from beaker.middleware import SessionMiddleware
+
+
+OAUTH_SERVER = "https://oauth2.3bot.grid.tf/auth"
+
+
 templates_path = j.sal.fs.joinPaths(j.sal.fs.getDirName(__file__), "templates")
 
 env = Environment(loader=FileSystemLoader(templates_path), autoescape=select_autoescape(["html", "xml"]))
 
 app = Bottle()
+
+PROVIDERS = ["github"]
 
 
 def enable_cors(fn):
@@ -24,6 +36,20 @@ def enable_cors(fn):
             return fn(*args, **kwargs)
 
     return _enable_cors
+
+
+def auth(fn):
+    def _auth(*args, **kwargs):
+        session = request.environ.get("beaker.session")
+        if "username" not in session:
+            if j.data.types.ipaddr.check(request.headers["HOST"]):
+                session["username"] = "Guest"
+            else:
+                session["next-url"] = request.path
+                redirect("/chat/login")
+        return fn(*args, **kwargs)
+
+    return _auth
 
 
 def get_ws_url():
@@ -45,22 +71,56 @@ def _get_chatflows():
     return [chatflow.decode() for chatflow in chatflows]
 
 
+@app.route("/chat/login")
+def login():
+    session = request.environ.get("beaker.session")
+    session_uid = j.data.idgenerator.generateGUID()
+    session["uid"] = session_uid
+    server_domain = "https://" + request.headers["HOST"]
+    query_params = f"uid={session_uid}&redirect_url={server_domain}/chat/authorize"
+    return env.get_template("chat/login.html").render(
+        oauth_server=OAUTH_SERVER, query=query_params, providers=PROVIDERS
+    )
+
+
+@app.route("/chat/authorize")
+def chat_authorize():
+    data = request.query
+    session = request.environ.get("beaker.session")
+    if data.get("uid") and data["uid"] == session.get("uid"):
+        session["username"] = data["username"]
+        next_url = session.get("next-url", "/chat")
+        redirect(next_url)
+    else:
+        response.status = 401
+        error = f"Couldn't authorize user"
+        return env.get_template("chat/error.html").render(error=error)
+
+
 @app.route("/chat")
+@auth
 def home_handler():
+    session = request.environ.get("beaker.session")
     chatflows = _get_chatflows()
     data = [(chatflow, chatflow.capitalize().replace("_", " ")) for chatflow in chatflows]
-    return env.get_template("chat/home.html").render(chatflows=data)
+    return env.get_template("chat/home.html").render(chatflows=data, username=session["username"])
 
 
 @app.route("/chat/session/<topic>", method="get")
 @enable_cors
+@auth
 def topic_handler(topic):
+    session = request.environ.get("beaker.session")
     if topic not in _get_chatflows():
         response.status = 404
         error = f"Specified chatflow {topic} is not registered on the system"
-        return env.get_template("chat/error.html").render(error=error)
+        return env.get_template("chat/error.html").render(error=error, username=session["username"])
     ws_url = get_ws_url()
-    return env.get_template("chat/index.html").render(topic=topic, url=ws_url)
+    return env.get_template("chat/index.html").render(topic=topic, url=ws_url, username=session["username"])
+
+
+session_opts = {"session.type": "file", "session.data_dir": "./data", "session.auto": True}
+app = SessionMiddleware(app, session_opts)
 
 
 class ChatFactory(j.baseclasses.threebot_factory):
