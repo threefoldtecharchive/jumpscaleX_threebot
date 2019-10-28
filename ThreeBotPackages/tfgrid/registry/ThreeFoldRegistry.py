a2bd320dc82f87012053f17fbbaa9a6c5fdf34e9
kristof de spiegeleer <>
2019-10-28 16:33:15 +0200

    specs for registry

--- ThreeBotPackages/tfgrid/registry/ThreeFoldRegistry.py
+++ ThreeBotPackages/tfgrid/registry/ThreeFoldRegistry.py
@@ -1,7 +1,7 @@
 from Jumpscale import j
 
 
-class TFPhonebookFactory(j.baseclasses.threebot_factory):
+class ThreeFoldRegistry(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.threefold.registry"
 
@@ -21,5 +21,44 @@ class TFPhonebookFactory(j.baseclasses.threebot_factory):
         cl = self.client_get()
 
         print(name)
+
+        # lets test for registration of a wiki page
+
+        schema = """]
+            @url = threebot.registry.test.schema.1
+            url = ""                              #unique url, points to where the wiki is
+            description = ""
+            topic = "travel,food,it" (E)
+            tags = (LS)
+        """
+
+        self.client.actors....schema_register(schema_text=schema)
+
+        s=j.data.schemas.get(schema=schema)
+        o = s.new()
+        o...
+
+        author_tid=j.tools.threebot.me.default.tid
+
+
+        j.data.schema.add_from_path(path=self._dirpath+"/models") #this will make sure the schema is known if this packafge
+        s = j.data.schemas.get_from_url(url="threebot.registry.entry.data.1")
+        dataobj=s.new()
+
+        #TODO: fill in required info in  the data obj
+        #we should create 2 examples, one where we encrypt for multiple threebot identities (j.tools.threebot...)
+
+        #need to sign with private key of author, we sign the bin data from dataobj (_data)
+        signature_hex = j.tools.threebot.me.sign(dataobj._data)
+
+        self.client.actors....register(author=author_tid, object=dataobj, signature_hex=None
+
+
+        #TODO: lets now verify if we can get the data and if we can decrypt (if encrypted)
+        #
+        obj=self.client.actors....get(...)
+        #TODO: Find for encrypted and not
+
+
 
         return "OK"
--- ThreeBotPackages/tfgrid/registry/actors/registry.py
+++ ThreeBotPackages/tfgrid/registry/actors/registry.py
@@ -5,130 +5,118 @@ from io import BytesIO
 JSBASE = j.baseclasses.object
 NONE = 2147483647
 
-from ..models.ENTRY import ENTRY
-
 
 class registry(j.baseclasses.threebot_actor):
     def _init(self, *args, **kwargs):
         self.bcdb = self._bcdb_get("threebot_phonebook")
-        self.registration_model = ENTRY()
+        self.registration_model = ...
 
-    def record_register(self, tid=None, data=None, sender_signature_hex=None, schema_out=None, user_session=None):
+    def register(self, author=0, object=None, signature_hex=None, schema_out=None, user_session=None):
         """
+        register an object of the type threebot.registry.entry.data.1
+        can be given in multiple formats
+
+        signature hex is done on the capnp output of the object
 
         ```in
-        tid = (I)
-        name = (S)
-        email = (S)
-        pubkey = ""                             #public key of the 3bot (is hexflify)
-        ipaddr = ""                             #how to reach the digitalme (3bot)
-        description = ""                        #optional
-        sender_signature_hex = ""               #in hex
+        author = (I)  #tid of the author
+        object = (O)  !threebot.registry.entry.data.1
+        signature_hex = "" (S)
         ```
 
         ```out
-        !threebot.phonebook.user.1
+        result = (I)
         ```
 
-        this is the 2nd step
+        :return: return the id of the object
+        """
 
-        :param: name you want to register can eg $name.$extension of $name if no extension will be $name.3bot
-                needs to correspond on the name as used in threebot_wallet_create
-        :param email:
-        :param ipaddr:
-        :param description:
+        # TODO: need to verify if author is correct (use user_session tid or author if specified
+        # verification happens by means of signature, signature was done on data
+
+        return
+
+    def get(self, tid=None, id=None, schema_out=None, user_session=None):
+        """
+        ```in
+        tid = (I)
+        id = (I)
+        ```
+
+        ```out
+        !threebot.registry.entry.1
+        ```
+        """
+
+    def schema_register(self,schema_url=None,schema_text=None,schema_out=None, user_session=None):
+        """
+
+        make sure the registrar knows about the schema's used
+
+        ```in
+        schema_url = ""
+        schema_text = ""
+        ```
+
+        ```out
+        schema_md5=""
+        ```
+        """
+
+    def find_encrypted(self, schema_url=None,...schema_out=None, user_session=None):
+        """
+        ```in
+        tid = (I)
+        #search arguments
+        schema_url = ""  #jumpscale schema url
+        country_code = ""
+        format = "website,blog,wiki,doc,solutionpackage,threebotpackage" (E)
+        category = "publicity,info,knowledge,code,spec,config,varia" (E)
+        topic = "travel,food,it,spirituality,health,education,finance,varia" (E)
+        description = ""
+        ```
+
+        only return if < 50 results
+
+        ```out
+        (LO) !threebot.registry.entry.data.1
+        ```
         :return:
         """
 
-        assert tid
-        assert sender_signature_hex
-
-        # verify data has been correctly signed, so the data corresponds to signature & pubkey used for signature
-        ok = j.data.nacl.payload_verify(tid, data, signature=sender_signature_hex, die=False)
-        if not ok:
-            raise j.exceptions.Input("data cannot be registered, signature wrong\n%s" % data)
-
-        res = self.registration_model.find(name=name)
-        if len(res) == 1:
-            u = res[0]
-            id = res[0].id
-            if not tid == id:
-                raise j.exceptions.Input("id does not match")
-            if pubkey != res[0].pubkey:
-                raise j.exceptions.Input(
-                    "public key cannot be changed once registered, it serves as the security for making changes"
-                )
-        elif len(res) > 1:
-            raise j.exceptions.JSBUG("more thant 1 should never be the case")
-        else:
-            raise j.exceptions.JSBUG(
-                "there should have been 1 threebot, first use self.name_register() and then this method."
-            )
-
-        assert u.id == tid
-        assert u.name == name
-        u.email = email
-        u.ipaddr = ipaddr
-        u.description = description
-        u.pubkey = pubkey
-        u.signature = sender_signature_hex
-        u.save()
-        return u
-
-    def get(self, tid=None, name=None, email=None, die=True, schema_out=None, user_session=None):
+    def find_formatted(self, registered_info_format=None, schema_url=None,...schema_out=None, format=,user_session=None):
         """
+
+        only works for non encrypted
+        will return the data as requested
+
         ```in
-        tid = (I)
-        name = (S)
-        email = (S)
-        die = true (B)
+        registered_info_format = "yaml,json,msgpack,unstructured" (E)
+        #search arguments
+        schema_url = ""  #jumpscale schema url
+        country_code = ""
+        format = "website,blog,wiki,doc,solutionpackage,threebotpackage" (E)
+        category = "publicity,info,knowledge,code,spec,config,varia" (E)
+        topic = "travel,food,it,spirituality,health,education,finance,varia" (E)
+        description = ""
         ```
+
+        only return if < 50 results
 
         ```out
-        !threebot.phonebook.user.1
+        (LS)
         ```
+
+        :param schema_url:
+        :param user_session:
+        :return:
         """
-        if tid and not tid == NONE:
-            user = self.phonebook_model.get(tid, die=False)
-            if not user:
-                raise j.exceptions.NotFound("user not found id:{user_id}" % locals())
-            users = [user]
-        elif name:
-            users = self.phonebook_model.find(name=name)
-        elif email:
-            users = self.phonebook_model.find(email=email)
-        else:
-            raise j.exceptions.NotFound("param error need to specify user_id or name or email")
 
-        if len(users) <= 0:
-            if die == False:
-                return None
-            raise j.exceptions.NotFound("user not found (%s)" % locals())
-        if len(users) > 1:
-            raise j.exceptions.NotFound("more than 1 user found (%s)" % locals())
-
-        jsxobject = users[0]
-
-        return jsxobject
-
-    def validate_signature(
-        self, tid=None, name=None, email=None, payload=None, signature=None, schema_out=None, user_session=None
-    ):
-        """
-        ```in
-        tid = (I)
-        name = (S)
-        email = (S)
-        payload = (S)
-        signature = (S)
-        ```
-        
-        ```out
-        is_valid = (B)
-        ```
-        """
-        u = self.get(tid=tid, name=name, email=email)
-        is_valid = j.data.nacl.payload_verify(payload, verifykey=u.pubkey, signature=signature, die=False)
-        out = schema_out.new()
-        out.is_valid = is_valid
-        return out
+        #find the data
+        #if found: see if there was a jumpscale schema used
+        #if jumpscale schema
+            # deserialize the data if needed and put in jumpscale object
+            # serialize the coming from the jumpscaleobject to the format asked for
+            # return the data
+        #if not jumpscale schema
+            # deserialize & re-serialize if needed to make sure the return format is the right one
