bd7a6c86c1c33b2d421cddf6976267f2b96ab69a
Mathias <mathias.de.weerdt@gmail.com>
2019-10-29 15:39:10 +0100

    initialize package

---
+++ ThreeBotPackages/threebot/interface/html/config/config.local.js
@@ -0,0 +1,8 @@
+export default ({
+  tfApiUrl: 'https://radar.threefold.io/api/',
+  jsApiUrl: `${window.location.origin}/web/gedis/http/`,
+  apiUrl: 'https://ffc-signaling.staging.jimber.org',
+
+  botFrontEnd: 'https://login.threefold.me/',
+  botBackend: 'https://login.threefold.me'
+})
