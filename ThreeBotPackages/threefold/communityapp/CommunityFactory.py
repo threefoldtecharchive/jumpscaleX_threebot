ea76a935191d3312eb2fc11020397158881dbead
BolaNasr <bola.nasr@live.com>
2019-10-29 11:41:13 +0200

    fix some typo stuff

--- ThreeBotPackages/threefold/communityapp/CommunityFactory.py
+++ ThreeBotPackages/threefold/communityapp/CommunityFactory.py
@@ -1,6 +1,6 @@
 from Jumpscale import j
 
 
-class CommunityFactory(j.baseclasses.object, j.baseclasses.testtools):
+class CommunityFactory(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.communiy_bot"
