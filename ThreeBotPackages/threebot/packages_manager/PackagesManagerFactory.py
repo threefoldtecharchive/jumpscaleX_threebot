8c25818db8db3f79802bfb415aee58b71b248be8
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2019-10-30 09:16:12 +0200

    add 3bot packages manager user interface (#152)
    * add 3bot packages manager user interface
    
    * use local javascript gedis client
    
    * remove useless files

---
+++ ThreeBotPackages/threebot/packages_manager/PackagesManagerFactory.py
@@ -0,0 +1,6 @@
+from Jumpscale import j
+
+
+class PackagesManagerFactory(j.baseclasses.threebot_factory):
+
+    __jslocation__ = "j.threebot.package.packages_manager_interface"
---
+++ ThreeBotPackages/threebot/packages_manager/build_frontend.sh
@@ -0,0 +1,2 @@
+cd packages_manager && npm run export
+cp __sapper__/export/packages/* ../html/ -R
