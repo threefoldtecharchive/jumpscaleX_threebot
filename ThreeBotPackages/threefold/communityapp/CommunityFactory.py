d7c7c7efc05f532596c824a26a8bfe70d79590f9
kristof de spiegeleer <>
2019-10-31 13:08:13 +0200

    specs for registry & fixes for packages

--- ThreeBotPackages/threefold/communityapp/CommunityFactory.py
+++ ThreeBotPackages/threefold/communityapp/CommunityFactory.py
@@ -4,3 +4,13 @@ from Jumpscale import j
 class CommunityFactory(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.communiy_bot"
+
+    def test(self):
+        """
+        kosmos 'j.threebot.package.communiy_bot.test()'
+        :return:
+        """
+        # to do manually:
+        # jsx wiki-load -u /sandbox/code/github/threefoldtech/jumpscaleX_threebot/ThreeBotPackages/threefold/communityapp/wiki -n community -f
+
+        j.shell()
