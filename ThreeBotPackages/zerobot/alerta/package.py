7c20dcc50d7c970d0e3062670462a832ef9a6387
Ahmed T. Youssef <xmonader@gmail.com>
2019-10-31 12:42:22 +0200

    Alerta updates

--- ThreeBotPackages/zerobot/alerta/package.py
+++ ThreeBotPackages/zerobot/alerta/package.py
@@ -3,41 +3,29 @@ from os.path import dirname, abspath, join
 
 
 class Package(j.baseclasses.threebot_package):
-    def _init(self, **kwargs):
-        if "branch" in kwargs.keys():
-            self.branch = kwargs["branch"]
-        else:
-            self.branch = "*"
-
-    def prepare(self):
-        """
-        is called at install time
-        :return:
-        """
-        pass
-
     def start(self):
         """
-        called when the 3bot starts
+        called when the 3bot starts the package
         :return:
         """
         server = self.openresty
         server.install(reset=False)
         server.configure()
 
-        website = server.get_from_port(443)
+        for port in (443, 80):
+            website = server.get_from_port(port)
 
-        locations = website.locations.get("alerta_locations")
+            locations = website.locations.get("alerta_locations")
 
-        website_location = locations.locations_spa.new()
-        website_location.name = "alerta"
-        website_location.path_url = "/alerta"
-        website_location.use_jumpscale_weblibs = False
-        fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
-        website_location.path_location = fullpath
+            website_location = locations.locations_spa.new()
+            website_location.name = "alerta"
+            website_location.path_url = "/alerta"
+            website_location.use_jumpscale_weblibs = False
+            fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+            website_location.path_location = fullpath
 
-        locations.configure()
-        website.configure()
+            locations.configure()
+            website.configure()
 
-        # setup alert handler to intercept errors
-        j.tools.alerthandler.setup()
+            # setup alert handler to intercept errors
+            j.tools.alerthandler.setup()
