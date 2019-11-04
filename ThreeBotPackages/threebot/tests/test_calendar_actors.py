c117eda9121e2e29deca4762b1df9f381268c959
0xIslamTaha <islamtaha2012@gmail.com>
2019-11-04 17:37:00 +0200

    cal test

--- ThreeBotPackages/threebot/tests/test_calendar_actors.py
+++ ThreeBotPackages/threebot/tests/test_calendar_actors.py
@@ -1,98 +1,83 @@
-from test.base_test import BaseTest
+from base_test import BaseTest
 from Jumpscale import j
 from time import sleep
+import requests
+import json
 
 
 class CalenderActorsTests(BaseTest):
     @classmethod
     def setUpClass(cls):
-        cls.info("start default zdb")
-        j.servers.zdb.default.start()
         cls.info("start calendar server")
-        cmd = 'kosmos -p "j.threebot.package.calendar.start()"'
-        cls.info("execute in main tmux : {} ".format(cmd))
-        cls.pan = j.servers.tmux.execute(cmd)
+        cl = j.servers.threebot.local_start_default(web=True)
+        cls.info("load calendar actors")
+        cl.actors.package_manager.package_add(
+            path='"/sandbox/code/github/threefoldtech/jumpscaleX_threebot/ThreeBotPackages/threebot/calendar"')
         sleep(30)
 
     @classmethod
     def tearDownClass(cls):
         cls.info("Destroy the bcdb")
         j.data.bcdb.destroy_all()
-        cls.info('kill the running calendar server')
-        cls.pan.kill()
 
     def setUp(self):
-        self.info("load calendar actors")
-        gedis_client = j.clients.gedis.get("test", port=8901)
-        gedis_client.actors.package_manager.package_add(
-            path="/sandbox/code/github/threefoldtech/jumpscaleX_threebot/ThreeBotPackages/threebot/calendar")
-        self.actors = gedis_client.actors.addressbook
-        self.actors.login("admin", "admin")
-        self.info("* Test case : {}".format(self._testMethodName))
+        self.setUp()
+        self.info("test case : {}".format(self._testMethodName))
         print('\n')
 
-    def _create_addressbook(self):
-        self.info("create new addressbook")
-        self.address_name = self.generate_random_str()
-        self.address_desc = self.generate_random_str()
-        new_addressbook_id = self.actors.add_addressbook(name=self.address_name, description=self.address_desc).decode()
-        return new_addressbook_id
+    def test001_add_calendar(self):
+        name = self.generate_random_str()
+        description = self.generate_random_str()
+        response = self.add_calendar(name=name, description=description)
+        self.info('assert status code is 200')
+        self.assertEqual(response.status_code, 200)
 
-    def _create_vcard(self, href):
-        self.info('create new vcard')
-        vcard = """
-        begin:VCARD
-        source:ldap://cn=bjorn%20Jensen, o=university%20of%20Michigan, c=US
-        name:Bjorn Jensen
-        fn:Bj=F8rn Jensen
-        n:Jensen;Bj=F8rn
-        email;type=internet:bjorn@umich.edu
-        tel;type=work,voice,msg:+1 313 747-4454
-        key;type=x509;encoding=B:dGhpcyBjb3VsZCBiZSAKbXkgY2VydGlmaWNhdGUK
-        end:VCARD
-        """
-        return self.actors.add_vcard(vcard, href)
+        self.info('assert name is matching with {}'.format(name))
+        self.assertEqual(response.json()['display_name'], name)
 
-    def test001_create_addressbook(self):
-        """
-        - Create addressbook, should success.
-        - List addressbook, should return the new addressbook
+        self.info('assert description is matching with {}'.format(description))
+        self.assertEqual(response.json()['description'], description)
 
-        :return:
-        """
-        old_addressbooks = self.actors.list_addressbooks().addressbooks
-        self._create_addressbook()
-        new_addressbooks = self.actors.list_addressbooks().addressbooks
-
-        self.info("assert there is a new addressbook")
-        self.assertEqual(len(old_addressbooks)+1, len(new_addressbooks))
-
-    def test002_get_address_metadata(self):
-        new_addressbook_id = self._create_addressbook()
-
-        self.info("get the address book meta data")
-        description = self.actors.get_addressbook_meta(new_addressbook_id).addressbook['{}/'.format(new_addressbook_id)]["description"]
-
-        self.info("assert it has the correct value")
-        self.assertEqual(self.address_desc, description)
-
-    def test003_delete_addressbook(self):
-        new_addressbook_id = self._create_addressbook()
-
-        self.info("get the address book meta data")
-        self.actors.get_addressbook_meta(new_addressbook_id)
-
-        self.info("delete the address book")
-        self.actors.delete_addressbook(new_addressbook_id)
-
-        self.info("try to get the address book meta data, should fail")
-        with self.assertRaises(Exception):
-            self.actors.get_addressbook_meta(new_addressbook_id)
-
-    def test004_create_vcard(self):
-        new_addressbook_id = self._create_addressbook()
-        new_vcard_id = self._create_vcard(href=new_addressbook_id)
-
-        self.info("get vcard")
-        vcard = self.actors.get_vcard(href=new_vcard_id)
-
+# def test001_create_addressbook(self):
+    #     """
+    #     - Create addressbook, should success.
+    #     - List addressbook, should return the new addressbook
+    #
+    #     :return:
+    #     """
+    #     old_addressbooks = self.actors.list_addressbooks().addressbooks
+    #     self._create_addressbook()
+    #     new_addressbooks = self.actors.list_addressbooks().addressbooks
+    #
+    #     self.info("assert there is a new addressbook")
+    #     self.assertEqual(len(old_addressbooks)+1, len(new_addressbooks))
+    #
+    # def test002_get_address_metadata(self):
+    #     new_addressbook_id = self._create_addressbook()
+    #
+    #     self.info("get the address book meta data")
+    #     description = self.actors.get_addressbook_meta(new_addressbook_id).addressbook['{}/'.format(new_addressbook_id)]["description"]
+    #
+    #     self.info("assert it has the correct value")
+    #     self.assertEqual(self.address_desc, description)
+    #
+    # def test003_delete_addressbook(self):
+    #     new_addressbook_id = self._create_addressbook()
+    #
+    #     self.info("get the address book meta data")
+    #     self.actors.get_addressbook_meta(new_addressbook_id)
+    #
+    #     self.info("delete the address book")
+    #     self.actors.delete_addressbook(new_addressbook_id)
+    #
+    #     self.info("try to get the address book meta data, should fail")
+    #     with self.assertRaises(Exception):
+    #         self.actors.get_addressbook_meta(new_addressbook_id)
+    #
+    # def test004_create_vcard(self):
+    #     new_addressbook_id = self._create_addressbook()
+    #     new_vcard_id = self._create_vcard(href=new_addressbook_id)
+    #
+    #     self.info("get vcard")
+    #     vcard = self.actors.get_vcard(href=new_vcard_id)
+    #
