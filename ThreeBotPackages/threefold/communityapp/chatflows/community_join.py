dd4241ad2842bba276fd7ac94616e0e868836f9e
BolaNasr <bola.nasr@live.com>
2019-11-06 18:23:37 +0200

    get all spaces from FFP and if you have account you can join any spaces you are intersts

--- ThreeBotPackages/threefold/communityapp/chatflows/community_join.py
+++ ThreeBotPackages/threefold/communityapp/chatflows/community_join.py
@@ -11,7 +11,6 @@ def chat(bot):
     html = bot.html_show("community")
     bot.md_show_update(html.format(100, "community"))
 
-    # TODO: change it to get email not user_name after fixing outh
     user_info = bot.user_info()
     user_name = user_info["username"]
     user_email = user_info["email"]  # still being added to the oauth user info
@@ -23,24 +22,35 @@ def chat(bot):
     gedis_client.actors.community_manager.set_current_user(user=user_email)
 
     bot.single_choice(
-        "Welcome to our ThreeFold World! Our dream is a complemetary responsible Internet, everywhere and owned by everyone, \n without borders \
+        f"Welcome {user_name} ...  to our ThreeFold World! Our dream is a complemetary responsible Internet, everywhere and owned by everyone, \n without borders \
     nor geographical discrimination. We envision a future where all humans are given an equal opportunity \
     to partake, learn, and succeed.",
         ["YES! Count me in!!!"],
     )
 
     if invited:
-        interests = bot.multi_choice(
-            "Choose your interests: ", ["TF Tokens", "ThreeFold Foundation", "Veda-egypt", "BetterToken"]
-        )
-        res = """
-        # You will join {{interests}}:
-        - Email : {{user_email}}
-        ### Click next
-        for the final step which will redirect you to dynamic macro
-        """
+        spaces = gedis_client.actors.community_manager.spaces_list()
+        interests = bot.multi_choice("Choose your interests: ", [space.decode() for space in spaces])
+        result = gedis_client.actors.community_manager.community_join(user_email=user_email, spaces=interests)
+        if not result:
+            res = """
+                # Sorry you can't joined to those spaces because:
+                - you don't have account in Freeflowpages
+                - or we have problem in FFP server
+                ### Click next
+                to try again
+                """
+
+        else:
+            res = """
+            # You joined {{interests}}:
+            - Email : {{user_email}}
+            ### Click next
+            for the final step which will redirect you to dynamic macro
+            """
     else:
         bot.single_choice("We'll send you an email with invitation soon! Stay tuned", ["OK"])
+        result = gedis_client.actors.community_manager.user_create(user_email=user_email, user_name=user_name)
         res = """
         ### Click next
         for the final step which will redirect you to dynamic macro
@@ -49,4 +59,6 @@ def chat(bot):
     res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
     bot.md_show(res)
     gevent.sleep(1)
+    if not result:
+        bot.redirect("/chat/session/community_join")
     bot.redirect("/community/wiki")
