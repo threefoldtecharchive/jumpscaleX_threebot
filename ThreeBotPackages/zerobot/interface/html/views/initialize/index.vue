36d68d03e58841431720c0f73369d4f090ce736c
Mathias <mathias.de.weerdt@gmail.com>
2019-11-06 14:33:32 +0100

    Removed interface

--- ThreeBotPackages/zerobot/interface/html/views/initialize/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="/views/initialize/initialize.html"></template>
-<script src="/views/initialize/initialize.js"></script>
-<style src="/views/initialize/initialize.css" lang="css"></style>
