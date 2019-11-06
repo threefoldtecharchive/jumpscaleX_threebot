2c8fceccf07497035c0e68d7793ad0c546020b2c
Mathias <mathias.de.weerdt@gmail.com>
2019-11-06 14:31:30 +0100

    Relocated interface to webinterface

---
+++ ThreeBotPackages/zerobot/webinterface/html/interface/main.js
@@ -0,0 +1,74 @@
+/* eslint no-undef: 0 */
+/* eslint-disable no-new */
+import httpVueLoader from './packages/ems/httpVueLoader.js'
+import VueRouter from './packages/ems/vue-router.js'
+import './packages/legacy/fontawesome-pro/js/all.js'
+import config from './config/index.js'
+import store from './store/index.js'
+import userService from './services/userService.js'
+
+Vue.prototype.$rules = {
+  required: v => !!v || 'This is required',
+  email: v => (/.+@.+\..+/.test(v) || !v) || 'E-mail must be valid',
+  noLongerThan100: v => (v && v.length <= 100) || 'This must be less than 100 characters'
+}
+
+window.config = config
+window.userService = userService
+
+const router = new VueRouter({
+  routes: [{
+    path: '/',
+    component: httpVueLoader('/interface/views/home/index.vue'),
+    name: 'home',
+    meta: {
+      icon: 'fa-home',
+      position: 'top'
+    }
+  }, {
+    path: '/appstore',
+    component: httpVueLoader('/interface/views/appstore/index.vue'),
+    name: 'appstore',
+    meta: {
+      icon: 'fa-th',
+      position: 'bottom'
+    }
+  }, {
+    path: '/login',
+    name: 'login',
+    component: httpVueLoader('/interface/views/login/index.vue'),
+    meta: {
+      position: 'none'
+    }
+  }, {
+    path: '/initialize',
+    name: 'initialize',
+    component: httpVueLoader('/interface/views/initialize/index.vue'),
+    meta: {
+      position: 'none'
+    }
+  }]
+})
+
+store.dispatch('setRoutes', router.options.routes)
+
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify({
+    iconfont: 'fa',
+    theme: {
+      themes: {
+        light: {
+          primary: '#2d4052',
+          secondary: '#57be8e'
+        }
+      }
+    }
+  }),
+  components: {
+    app: httpVueLoader('/interface/App/index.vue')
+  },
+  router,
+  store,
+  template: '<app></app>'
+})
---
+++ ThreeBotPackages/zerobot/webinterface/html/interface/services/appServices.js
@@ -0,0 +1,35 @@
+/* eslint no-undef: 0 */
+/* eslint-disable no-new */
+import config from '../config/index.js'
+
+export default ({
+  getVueApps () {
+    const frontendPackages = Axios.post(`${config.jsApiUrl}package_manager/packages_list`, {
+      args: {
+        frontend: true
+      }
+    })
+    return frontendPackages
+    // Checking wether the packages
+    // console.log('frontendPackages: ', frontendPackages)
+  },
+  checkPathForFile (path) {
+    return Axios.get(path)
+  },
+  installApp (app) {
+    app.installed = true
+    return Axios.post(`${config.jsApiUrl}apps/put`, {
+      args: {
+        app
+      }
+    })
+  },
+  uninstallApp (app) {
+    app.installed = false
+    return Axios.post(`${config.jsApiUrl}apps/put`, {
+      args: {
+        app
+      }
+    })
+  }
+})
