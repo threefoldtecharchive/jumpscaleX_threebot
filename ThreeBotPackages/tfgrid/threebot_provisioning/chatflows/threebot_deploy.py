2062a7a1fe7a02af71bd83d36842f8cfbe2b711f
serboctor <boctor.sarah@gmail.com>
2019-11-06 13:24:07 +0200

    Update threebot_deploy chatflow to set the identity

--- ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_deploy.py
+++ ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_deploy.py
@@ -25,17 +25,19 @@ def chat(bot):
     if not name or not email:
         bot.md_show("Username or email not found in session. Please log in properly")
 
+    url = f"https://{name}.3bot.testnet.grid.tf"
+
+    deployer = j.tools.threebot_deploy.get()
     try:
-        explorer.actors.phonebook.get(name=name)
-        bot.md_show(f"Doublename {name} is already in use")
-    except:
+        deployer.get_by_double_name(name)
+        bot.md_show(f"Doublename {name} has already been used to deploy a 3bot. You can find it here [{url}]({url})")
+    except j.exceptions.NotFound:
         pass
 
     question = "Please enter a description for your 3BOT:"
     description = bot.string_ask(question)
     bot.md_show("Deployment will start this might take several minutes")
 
-    deployer = j.tools.threebot_deploy.get()
     bot.md_show_update(progress.format(0, "Creating 3Bot"))
     machine = deployer.machines.get_available()
     bot.md_show_update(progress.format(10, "Configuring 3Bot"))
@@ -47,14 +49,11 @@ def chat(bot):
     bot.md_show_update(progress.format(90, "Registering 3Bot"))
     client = container.threebot_client
     client.actors.registration.register(name, email, description)
-    container.set_identity(name)
+    record = explorer.actors.phonebook.get(name=name)
+    container.set_identity(record)
     bot.md_show_update(progress.format(100, "Registering 3Bot completed"))
 
-    url = f"https://{name}.3bot.testnet.grid.tf"
-    res = """\
-# Your 3bot has been registered successfully you can find it here [{0}]({0})
-    """.format(
-        url
-    )
+    res = f"""\
+# Your 3bot has been registered successfully you can find it here [{url}]({url})
+    """
     bot.md_show(res)
-    # bot.redirect("https://threefold.me")
