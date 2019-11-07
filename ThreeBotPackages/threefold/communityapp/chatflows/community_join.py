808755a5e590359deac13eccb8dace18a05d9c7b
Waleed Hammam <waleed.hammam@gmail.com>
2019-11-07 19:10:36 +0200

    add non registered membered for chatbot

--- ThreeBotPackages/threefold/communityapp/chatflows/community_join.py
+++ ThreeBotPackages/threefold/communityapp/chatflows/community_join.py
@@ -21,51 +21,83 @@ def chat(bot):
 
     gedis_client.actors.community_manager.set_current_user(user=user_email)
 
-    choice = bot.single_choice(
-        f"Welcome {user_name} ...  to our ThreeFold World! Our dream is a complemetary responsible Internet, everywhere and owned by everyone, \n without borders \
-    nor geographical discrimination. We envision a future where all humans are given an equal opportunity \
-    to partake, learn, and succeed.",
-        ["Count me in", "get invitation url"],
+    main_page = bot.single_choice(
+        "<h3>Welcome to ThreeFold</h3> <br /> Are you registerd or not?", ["Registerd", "Not Registerd"]
     )
+    spaces = [
+        "TF Tokens",
+        "TF Grid User",
+        "TF Grid Farmer",
+        "TF Grid Developer",
+        "Threebot Early Adopter",
+        "Threebot Developer",
+        "IEO",
+        "ThreeFold Technology",
+        "3bot Technology",
+    ]
+    if main_page == "Not Registerd":
+        form = bot.new_form()
+        name = form.string_ask("<h3>Please fill in your name and Email</h3> <br /> Name: ")
+        email = form.string_ask("Email: ")
+        form.ask()
+        interests = bot.multi_choice("Choose your interests: ", [space for space in spaces])
+        result = gedis_client.actors.community_manager.community_join(user_email=email, spaces=interests)
+        code_id = gedis_client.actors.community_manager.get_invitation_code(email=email, user_name=name)
 
-    if choice == "get invitation url":
-        code_id = gedis_client.actors.community_manager.get_invitation_code(email=user_email, user_name=user_name)
         md = f"""
-            # please copy this url and send it to your friends
-            - https://community.app.3bot.testnet.grid.tf/chat/session/community_join?referral={code_id.decode()}
-            """
-        res = j.tools.jinja2.template_render(text=j.core.text.strip(md), **locals())
-        bot.md_show(res)
+        Thanks for registeration, we will Email you soon.
+        please copy this url and send it to your friends
 
-    if invited:
-        spaces = gedis_client.actors.community_manager.spaces_list()
-        interests = bot.multi_choice("Choose your interests: ", [space.decode() for space in spaces])
-        result = gedis_client.actors.community_manager.community_join(user_email=user_email, spaces=interests)
-        if not result:
-            res = f"""
-                ## You invited by {invited.decode()}
-                # Sorry you can't joined to those spaces because:
-                - you don't have account in Freeflowpages
-                - or we have problem in FFP server
-                ### Click next
-                to try again
+        <a>https://threefold.io/chat/session/community_join?referral={code_id.decode()} </a>
+        """
+        bot.single_choice(f"{md}", ["OK"])
+        return
+    else:
+        choice = bot.single_choice(
+            f"Welcome {user_name} ...  to our ThreeFold World! Our dream is a complemetary responsible Internet, everywhere and owned by everyone, \n without borders \
+        nor geographical discrimination. We envision a future where all humans are given an equal opportunity \
+        to partake, learn, and succeed.",
+            ["Count me in", "get invitation url"],
+        )
+
+        if choice == "get invitation url":
+            code_id = gedis_client.actors.community_manager.get_invitation_code(email=user_email, user_name=user_name)
+            md = f"""
+                # please copy this url and send it to your friends
+                - https://community.app.3bot.testnet.grid.tf/chat/session/community_join?referral={code_id.decode()}
                 """
+            res = j.tools.jinja2.template_render(text=j.core.text.strip(md), **locals())
+            bot.md_show(res)
 
+        if invited:
+            spaces = gedis_client.actors.community_manager.spaces_list()
+            interests = bot.multi_choice("Choose your interests: ", [space.decode() for space in spaces])
+            result = gedis_client.actors.community_manager.community_join(user_email=user_email, spaces=interests)
+            if not result:
+                res = f"""
+                    ## You invited by {invited.decode()}
+                    # Sorry you can't joined to those spaces because:
+                    - you don't have account in Freeflowpages
+                    - or we have problem in FFP server
+                    ### Click next
+                    to try again
+                    """
+
+            else:
+                res = f"""
+                ## You invited by {invited.decode()}
+                # You joined {interests}:
+                - Email : {user_email}
+                ### Click next
+                for the final step which will redirect you to dynamic macro
+                """
         else:
-            res = f"""
-            ## You invited by {invited.decode()}
-            # You joined {interests}:
-            - Email : {user_email}
+            bot.single_choice("We'll send you an email with invitation soon! Stay tuned", ["OK"])
+            result = gedis_client.actors.community_manager.user_create(user_email=user_email, user_name=user_name)
+            res = """
             ### Click next
             for the final step which will redirect you to dynamic macro
             """
-    else:
-        bot.single_choice("We'll send you an email with invitation soon! Stay tuned", ["OK"])
-        result = gedis_client.actors.community_manager.user_create(user_email=user_email, user_name=user_name)
-        res = """
-        ### Click next
-        for the final step which will redirect you to dynamic macro
-        """
 
     res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
     bot.md_show(res)
--- ThreeBotPackages/zerobot/chat/ChatFactory.py
+++ ThreeBotPackages/zerobot/chat/ChatFactory.py
@@ -20,6 +20,21 @@ oauth_app = j.tools.oauth_proxy.get(app, client, "/chat/login")
 bot_app = j.tools.threebotlogin_proxy.get(app)
 
 PROVIDERS = list(client.providers_list())
+OAUTH_SERVER = ""
+
+
+def auth(fn):
+    def _auth(*args, **kwargs):
+        session = request.environ.get("beaker.session")
+        if "username" not in session:
+            if j.data.types.ipaddr.check(request.headers["HOST"]):
+                session["username"] = "Guest"
+            else:
+                session["next-url"] = request.path
+                redirect("/chat/login")
+        return fn(*args, **kwargs)
+
+    return _auth
 
 
 def enable_cors(fn):
@@ -59,15 +74,14 @@ def _get_chatflows():
 
 @app.route("/chat/login")
 def login():
-    provider = request.query.get("provider")
-    if provider:
-        if provider == "3bot":
-            return bot_app.login(request.headers["HOST"], "/chat/3botlogin")
-
-        redirect_url = f"https://{request.headers['HOST']}/chat/authorize"
-        return oauth_app.login(provider, redirect_url=redirect_url)
-
-    return env.get_template("chat/login.html").render(providers=PROVIDERS)
+    session = request.environ.get("beaker.session")
+    session_uid = j.data.idgenerator.generateGUID()
+    session["uid"] = session_uid
+    server_domain = "https://" + request.headers["HOST"]
+    query_params = f"uid={session_uid}&redirect_url={server_domain}/chat/authorize"
+    return env.get_template("chat/login.html").render(
+        oauth_server=OAUTH_SERVER, query=query_params, providers=PROVIDERS
+    )
 
 
 @app.route("/chat/3botlogin")
@@ -77,25 +91,32 @@ def chat_botcallback():
 
 @app.route("/chat/authorize")
 def chat_authorize():
-    user_info = oauth_app.callback()
-    oauth_app.session["email"] = user_info["email"]
-    return redirect(oauth_app.next_url)
+    data = request.query
+    session = request.environ.get("beaker.session")
+    if data.get("uid") and data["uid"] == session.get("uid"):
+        session["username"] = data["username"]
+        next_url = session.get("next-url", "/chat")
+        redirect(next_url)
+    else:
+        response.status = 401
+        error = f"Couldn't authorize user"
+        return env.get_template("chat/error.html").render(error=error)
 
 
 @app.route("/chat")
-@oauth_app.login_required
+@auth
 def home_handler():
-    session = oauth_app.session
+    session = request.environ.get("beaker.session")
     chatflows = _get_chatflows()
     data = [(chatflow, chatflow.capitalize().replace("_", " ")) for chatflow in chatflows]
-    return env.get_template("chat/home.html").render(chatflows=data, email=session["email"])
+    return env.get_template("chat/home.html").render(chatflows=data, username=session["username"])
 
 
 @app.route("/chat/session/<topic>", method="get")
 @enable_cors
-@oauth_app.login_required
+@auth
 def topic_handler(topic):
-    session = oauth_app.session
+    session = request.environ.get("beaker.session")
     query = request.urlparts.query
     if query:
         query = query.split("&")
@@ -113,11 +134,12 @@ def topic_handler(topic):
         return env.get_template("chat/error.html").render(error=error, email=session["email"])
     ws_url = get_ws_url()
     return env.get_template("chat/index.html").render(
-        topic=topic, url=ws_url, email=session["email"], qs=session["kwargs"], username=session.get("username")
+        topic=topic, url=ws_url, email=session.get("email", ""), qs=session["kwargs"], username=session.get("username")
     )
 
 
-app = oauth_app.app
+session_opts = {"session.type": "file", "session.data_dir": "./data", "session.auto": True}
+app = SessionMiddleware(app, session_opts)
 
 
 class ChatFactory(j.baseclasses.threebot_factory):
