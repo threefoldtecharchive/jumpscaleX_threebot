a5765dc86467ae8ca9ffa3042de4e53148652409
serboctor <boctor.sarah@gmail.com>
2019-11-07 20:33:24 +0200

    comment out conflicting locations

--- ThreeBotPackages/zerobot/webinterface/package.py
+++ ThreeBotPackages/zerobot/webinterface/package.py
@@ -63,11 +63,11 @@ class Package(j.baseclasses.threebot_package):
         website = server.get_from_port(443)
         locations = website.locations.get("interface_location")
 
-        website_location = locations.locations_spa.new()
-        website_location.name = "interface"
-        website_location.path_url = "/"
-        fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
-        website_location.path_location = fullpath
+        # website_location = locations.locations_spa.new()
+        # website_location.name = "interface"
+        # website_location.path_url = "/"
+        # fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+        # website_location.path_location = fullpath
 
         locations.configure()
         website.configure()
