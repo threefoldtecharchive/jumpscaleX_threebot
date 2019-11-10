b39dae6ab854d600971c36be1b50fac669ebac05
xmonader <xmonader@gmail.com>
2019-11-10 08:26:06 +0200

    update packages to use the new dir conventions for spa locations

--- ThreeBotPackages/demo/pastebin/package.py
+++ ThreeBotPackages/demo/pastebin/package.py
@@ -2,26 +2,4 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
-    def start(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        server = self.openresty
-        server.install(reset=False)
-        server.configure()
-
-        for port in (443, 80):
-            website = server.get_from_port(port)
-
-            locations = website.locations.get("pastebin_locations")
-
-            website_location = locations.locations_spa.new()
-            website_location.name = "pastebin"
-            website_location.path_url = "/pastebin"
-            website_location.use_jumpscale_weblibs = False
-            fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
-            website_location.path_location = fullpath
-
-            locations.configure()
-            website.configure()
+    pass
