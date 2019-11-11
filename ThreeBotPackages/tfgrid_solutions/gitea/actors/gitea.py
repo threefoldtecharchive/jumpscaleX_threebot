42ffaa05f2dcf816b4cea3a1e19a137f5d86fdcb
Rana Tarek Hassan <rana.t.hassan@gmail.com>
2019-11-11 17:11:42 +0000

    Fix gitea chatflow package

--- ThreeBotPackages/tfgrid_solutions/gitea/actors/gitea.py
+++ ThreeBotPackages/tfgrid_solutions/gitea/actors/gitea.py
@@ -1,17 +1,12 @@
 from Jumpscale import j
 
 
-class provision(j.baseclasses.threebot_actor):
-    def _init(self, **kwargs):
-        self.bcdb = j.data.bcdb.get("tfgrid_solutions")
+class gitea(j.baseclasses.threebot_actor):
+    def _init(self, *args, **kwargs):
+        self.bcdb = j.data.bcdb.get("gitea")
         self.model = j.data.bcdb.system.model_get(url="tfgridsolutions.gitea.instance.1")
 
     def getall(self, user_session=None):
-        """
-        ```out
-        result = (LO) !...
-        ```
-        """
         return self.model.find()
 
     def chatbot_install_url_get(self):
@@ -19,18 +14,21 @@ class provision(j.baseclasses.threebot_actor):
 
         :return: the url (normally relative to threebot) for the chatbot which will create a threebot
         """
+        pass
 
     def chatbot_recovery_url_get(self):
         """
 
         :return: the url (normally relative to threebot) for the chatbot which will create a threebot
         """
+        pass
 
     def wiki_doc_url_get(self):
         """
         :return: the url (normally relative to threebot) for the chatbot which will create a threebot
 
         """
+        pass
 
     def wiki_solution_url_get(self, name=None):
         """
@@ -38,6 +36,7 @@ class provision(j.baseclasses.threebot_actor):
         :return: the url (normally relative to threebot) for the chatbot which will create a threebot
 
         """
+        pass
 
     def delete(self, name=None):
         """
@@ -48,9 +47,11 @@ class provision(j.baseclasses.threebot_actor):
         state = "OK,ERROR" (E)
         ```
         """
+        pass
 
     def monitor_up(self):
         """
         does monitoring returns True if ok
         :return:
         """
+        pass
