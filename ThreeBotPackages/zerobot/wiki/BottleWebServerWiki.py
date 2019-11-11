d555a5a7893a48c7c43b14911dab284b18144ec0
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-11-11 21:04:42 +0200

    WIP update gdrive handlers and docs and add gpdf macro docs

--- ThreeBotPackages/zerobot/wiki/BottleWebServerWiki.py
+++ ThreeBotPackages/zerobot/wiki/BottleWebServerWiki.py
@@ -72,13 +72,17 @@ def wiki_handler(docsite):
     return env.get_template("wiki/index.html").render(name=docsite, metadata=get_metadata(docsite), url=ws_url)
 
 
+@app.route("/wiki/gdrive/<doc_type>/<guid1>")
 @app.route("/wiki/gdrive/<doc_type>/<guid1>/<guid2>")
-def gdrive_handler(doc_type, guid1, guid2):
+def gdrive_handler(doc_type, guid1, guid2=""):
     cl = j.clients.gedis.get("wiki_gdrive_client", port=8901)
     try:
         result = cl.actors.gdrive.file_get(doc_type, guid1, guid2)
-        redirect(result["res"])
-    except Exception as ex:
+        redirect(result.res)
+    except j.exceptions.Input:
+        response.status = 400
+        return f"invalid document type of '{doc_type}'"
+    except j.exceptions.Base as ex:
         err = "".join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__))
         response.status = 400
         result = {"error": err}
