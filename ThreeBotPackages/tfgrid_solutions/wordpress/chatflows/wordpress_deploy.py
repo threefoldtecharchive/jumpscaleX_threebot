5848cf5dd05f64982c0bdd407f4dc843474d7091
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-11-11 16:26:33 +0200

    Update the chatbot of worpress package and minecraft package #147

--- ThreeBotPackages/tfgrid_solutions/wordpress/chatflows/wordpress_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/wordpress/chatflows/wordpress_deploy.py
@@ -1,21 +1,32 @@
 from Jumpscale import j
+import gevent
 
 
 def chat(bot):
     """
     to call http://localhost:5050/chat/session/wordpress_deploy
     """
-    path = bot.string_ask("Path:")
-    host_url = bot.string_ask("Host URL:")
-    title = bot.string_ask("Titlte:")
-    admin_user = bot.string_ask("Admin user:")
-    admin_password = bot.string_ask("Admin password:")
-    admin_email = bot.string_ask("Admin Email:")
+
+    # all the required data related to the database so they will be static
+
+    html = """\
+# Loading Wordpress configuration...
+ <div class="progress">
+  <div class="progress-bar active" role="progressbar" aria-valuenow="{0}"
+  aria-valuemin="0" aria-valuemax="100" style="width:{0}%">
+    {0}%
+  </div>
+</div>
+"""
+    waittime = 5
+    for x in range(waittime):
+        bot.md_show_update(html.format((x / waittime) * 100))
+        gevent.sleep(1)
 
     # j.builders.apps.wordpress.install(path, host_url, title, admin_user, admin_password, admin_email)
 
     res = f"""
-    # Wordpress has been deployed successfully: 
-    """
+# Wordpress has been deployed successfully:
+"""
     bot.md_show(res)
     bot.redirect("https://threefold.me")
