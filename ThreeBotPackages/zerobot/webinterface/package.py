c3d7aba12781d4118f0448033a74e94918ec1765
Mathias <mathias.de.weerdt@gmail.com>
2019-11-12 11:53:52 +0100

    Fixed models

--- ThreeBotPackages/zerobot/webinterface/package.py
+++ ThreeBotPackages/zerobot/webinterface/package.py
@@ -53,7 +53,25 @@ class Package(j.baseclasses.threebot_package):
 
     def start(self):
         self.prepare()
+        
+        self.bcdb.models_add(path=self.package_root + "/models")
         self.gedis_server.actors_add(path=self.package_root + "/actors")
+
+        server = self.openresty
+        server.install(reset=False)
+        server.configure()
+        website = server.get_from_port(443)
+        locations = website.locations.get("interface_location")
+
+        website_location = locations.locations_spa.new()
+        website_location.name = "interface"
+        website_location.path_url = "/"
+        fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+        website_location.path_location = fullpath
+
+        locations.configure()
+        website.configure()
+        website.save()
 
     def stop(self):
         pass
