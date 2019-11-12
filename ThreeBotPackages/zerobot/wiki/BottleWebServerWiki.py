f98cdffe42fb964408e6d334d0f9b692f3fc119f
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-11-12 15:37:09 +0200

    add error page for gdrive tool endpoints

--- ThreeBotPackages/zerobot/wiki/BottleWebServerWiki.py
+++ ThreeBotPackages/zerobot/wiki/BottleWebServerWiki.py
@@ -77,11 +77,10 @@ def wiki_handler(docsite):
 def gdrive_handler(doc_type, guid1, guid2=""):
     cl = j.clients.gedis.get("wiki_gdrive_client", port=8901)
     try:
-        result = cl.actors.gdrive.file_get(doc_type, guid1, guid2)
-        redirect(result.res)
-    except j.exceptions.Input:
-        response.status = 400
-        return f"invalid document type of '{doc_type}'"
+        ret = cl.actors.gdrive.file_get(doc_type, guid1, guid2)
+        if not ret.error_code:
+            return redirect(ret.res)
+        return env.get_template("wiki/gdrive_error.html").render(code=ret.error_code, message=ret.error_message)
     except j.exceptions.Base as ex:
         err = "".join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__))
         response.status = 400
