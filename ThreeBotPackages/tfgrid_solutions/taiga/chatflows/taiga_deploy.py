9b513701f0192ca9fe3ab0b3d482f007f4e6c14f
Rana Tarek Hassan <rana.t.hassan@gmail.com>
2019-11-13 16:27:12 +0000

    WIP Use threebot tool to reserve container

--- ThreeBotPackages/tfgrid_solutions/taiga/chatflows/taiga_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/taiga/chatflows/taiga_deploy.py
@@ -18,31 +18,7 @@ def chat(bot):
 
     bot.loading_show("taiga", 4)
 
-    ## Register and deploy container
-    bcdb = j.servers.threebot.default.bcdb_get("tf_workloads")
-    reservation_model = bcdb.model_get(url="tfgrid.reservation.1")
-
-    ph_bcdb = j.servers.threebot.default.bcdb_get("threebot_phonebook")
-
-    model = ph_bcdb.model_get(url="threebot.phonebook.user.1")
-    tbot = model.new()
-
-    tbot.name = bot.user_info.username
-    tbot.email = bot.user_info.email
-    tbot.pubkey = j.clients.sshkey.default.pubkey  # TODO
-    tbot.save()
-
-    # Create container reservation
-    reservation = reservation_model.new()
-    reservation.customer_tid = tbot.id
-
-    container_model = bcdb.model_get(url="tfgrid.reservation.container.1")
-    container = container_model.new()
-    container.node_id = "1"
-    container.workload_id = 2
-    container.flist = "https://hub.grid.tf/hosnys/hossnys-taiga-latest.flist"
-    container.hub_url = "https://hub.grid.tf/hosnys/hossnys-taiga-latest.flist.md"
-    container.environment = {
+    environment = {
         "SECRET_KEY": SECRET_KEY,
         "EMAIL_HOST": email_host,
         "EMAIL_HOST_USER": email_host_user,
@@ -50,25 +26,17 @@ def chat(bot):
         "HOST_IP": HOST_IP,
     }
 
-    container.entrypoint = "/bin/bash"
-    container.interactive = "yes"  # yes or no
-    reservation.data_reservation.containers.append(container)
+    # Create and register new reservation with container information(credentials will be obtained from threebot.me)
+    reservation = j.tools.threebot_reserve_container(
+        flist="https://hub.grid.tf/hosnys/hossnys-taiga-latest.flist",
+        hub_url="https://hub.grid.tf/hosnys/hossnys-taiga-latest.flist.md",
+        environment=environment,
+        entrypoint="/bin/bash",
+    )
 
-    # Sign model
-    reservation.json = j.data.serializers.json.dumps(reservation.data_reservation._ddict)
-    reservation_data = reservation._ddict
-
-    # temp key & signature
-    # TODO change to use key of threebot
-    signing_key = signing.SigningKey.generate()
-    signature = signing_key.sign(reservation.json.encode())
-    cl = j.clients.gedis.get(name="threebot")
-    cl.actors.workload_manager.sign_customer(reservation.id, binascii.hexlify(signature.signature))
-
-    reservation = cl.actors.workload_manager.reservation_register(reservation_data)
+    # bot.md_show(reservation)
 
     res = f"""# Taiga has been deployed successfully:
     """
-
     bot.md_show(res)
     bot.redirect("https://threefold.me")
