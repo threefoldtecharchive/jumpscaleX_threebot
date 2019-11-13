11353576768d44d768c032a379f137bdedf6384c
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-11-13 14:25:25 +0100

    3bot initialization: move reseed from registration actor to initialize

--- ThreeBotPackages/tfgrid/registration/actors/registration.py
+++ ThreeBotPackages/tfgrid/registration/actors/registration.py
@@ -61,41 +61,6 @@ class registration(j.baseclasses.threebot_actor):
 
         print(f"Done, your url is: {doublename}.{THREEBOT_DOMAIN}")
 
-    def reseed(self, newseed, user_session):
-        """
-        ```in
-        newseed = (S)
-        ```
-        :param newseed:
-        :param user_session:
-        :return:
-        """
-        nacl = j.data.nacl.default
-        explorer = j.clients.gedis.get(name="explorer", host=EXPLORER_DOMAIN, port=8901)
-
-        # create a signature used to update the public key in the phonebook
-        tid = j.tools.threebot.me.default.tid
-        seed = j.data.encryption.mnemonic.to_entropy(newseed)
-        new_key = SigningKey(seed).encode(HexEncoder)
-        signature = j.data.nacl.payload_sign(tid, new_key, nacl=nacl)
-
-        exportpath = j.sal.fs.getTmpDirPath()
-        j.data.bcdb.system.export(exportpath, False)
-        j.data.nacl.configure(privkey_words=newseed, reset=True)
-        j.tools.threebot_packages.delete("registration")
-        j.sal.process.execute(f"kosmos -p 'system = j.data.bcdb.get_system(); system.import_(\"{exportpath}\")'")
-        j.sal.fs.remove(exportpath)
-
-        # update the public key in the phonebook
-        explorer.actors.phonebook.update_public_key(tid, new_key, signature)
-
-        me = j.tools.threebot.me.default
-        me.pubkey = new_key
-        me.save()
-
-        # restart myself
-        gevent.spawn_later(5, j.sal.process.restart_program)
-
     def set_identity(self, tid, tname, email, pubkey, user_session):
         """
         ```in
