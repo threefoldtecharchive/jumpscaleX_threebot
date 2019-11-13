70ab76e76d495ed96ed4ac7218678ba96b99bcf1
tobiaschielens <tobias.chielens@gmail.com>
2019-11-13 19:00:50 +0100

    added name to identity (#236)

--- ThreeBotPackages/zerobot/webinterface/actors/identity.py
+++ ThreeBotPackages/zerobot/webinterface/actors/identity.py
@@ -7,3 +7,15 @@ class identity(j.baseclasses.threebot_actor):
 
     def pubkey(self, schema_out=None, user_session=None):
         return j.tools.threebot.me.default.pubkey
+
+    def name(self, schema_out=None, user_session=None):
+        """
+        ```out
+        name  = ""
+        ```
+        """
+
+        out = schema_out.new()
+        out.name = j.tools.threebot.me.default.tname
+
+        return out
