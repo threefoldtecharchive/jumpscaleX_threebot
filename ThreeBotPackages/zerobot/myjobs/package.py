9185a7935f342e7c6e448fec98ecaf5a1fc7d091
xmonader <xmonader@gmail.com>
2019-11-14 21:21:48 +0200

    don't start myjobs from the package myjobs dashboard

--- ThreeBotPackages/zerobot/myjobs/package.py
+++ ThreeBotPackages/zerobot/myjobs/package.py
@@ -2,9 +2,4 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
-    def prepare(self):
-        """
-        is called at install time
-        :return:
-        """
-        j.servers.myjobs.workers_tmux_start()
+    pass
