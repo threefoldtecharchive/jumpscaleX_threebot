37f73f4e0ab063962e33fce53306110efaa4c6c4
BolaNasr <bola.nasr@live.com>
2019-11-20 15:36:58 +0200

    add more feature in chatflow and add threefold logo

--- ThreeBotPackages/threefold/communityapp/CommunityFactory.py
+++ ThreeBotPackages/threefold/communityapp/CommunityFactory.py
@@ -5,6 +5,14 @@ class CommunityFactory(j.baseclasses.threebot_factory):
 
     __jslocation__ = "j.threebot.package.community_bot"
 
+    def _get_admin_referral(self):
+        bcdb = j.data.bcdb.get("tf_community_app")
+        model = bcdb.model_get(url="threefold.community.user.1")
+        users = model.find(name="admin")
+        if users:
+            return users[0].referral_code
+        return "Sorry , admin not created yet"
+
     def test(self):
         """
         kosmos 'j.threebot.package.community_bot.test()'
--- ThreeBotPackages/threefold/communityapp/wiki/structure.md
+++ ThreeBotPackages/threefold/communityapp/wiki/structure.md
@@ -1,4 +1,5 @@
-## your Tree
+<img style="float: right;width: 162px;" src="logo.svg">
+##  Impact Tree
 ```
 !!!dynamic_content
 actor = "community_manager"
