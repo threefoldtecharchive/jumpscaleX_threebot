6c9d856905e982210bf3cd8b4538ff90fc518382
tobiaschielens <tobias.chielens@gmail.com>
2019-11-20 16:59:38 +0100

    moved contacts back to threebot

---
+++ ThreeBotPackages/threebot/contacts/html/components/fullcontact/index.vue
@@ -0,0 +1,3 @@
+<template src="contacts/components/fullcontact/fullcontact.html"></template>
+<script src="contacts/components/fullcontact/fullcontact.js"></script>
+<style src="contacts/components/fullcontact/fullcontact.css" scoped lang="css"></style>
