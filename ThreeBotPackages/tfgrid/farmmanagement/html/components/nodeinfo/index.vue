e997a3fe9c716eaeafad56186a02f7c533fb5725
tobiaschielens <tobias.chielens@gmail.com>
2019-11-20 15:11:21 +0100

    moving farmmanagement

---
+++ ThreeBotPackages/tfgrid/farmmanagement/html/components/nodeinfo/index.vue
@@ -0,0 +1,4 @@
+<template src="./farmmanagement/components/nodeinfo/nodeinfo.html"></template>
+<script src="./farmmanagement/components/nodeinfo/nodeinfo.js"></script>
+<style src="./farmmanagement/components/nodeinfo/nodeinfo.css" scoped lang="css"></style>
+
