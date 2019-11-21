a56c0a824c21428551d6142d65413221161af02b
Mathias <mathias.de.weerdt@gmail.com>
2019-11-21 12:40:11 +0100

    Reformatting package store

--- ThreeBotPackages/zerobot/webplatform/html/interface/store/packageStore.js
+++ ThreeBotPackages/zerobot/webplatform/html/interface/store/packageStore.js
@@ -1,4 +1,5 @@
 import packageService from '../services/packageServices.js'
+
 export default ({
   state: {
     packages: []
@@ -9,57 +10,61 @@ export default ({
       context.dispatch('wait/start', 'getInstalledPackages', {
         root: true
       })
-      let response = await packageService.getInstalledPackages();
-      for (let pkg of response.data.packages) {
+      const response = await packageService.getInstalledPackages()
+      for (const pkg of response.data.packages) {
         try {
-          let routesResponse = await packageService.checkPathForFile(`${window.location.origin}/${pkg.name}/router.json`);
+          const routesResponse = await packageService.checkPathForFile(`${window.location.origin}/${pkg.name}/router.json`)
           if (response.status === 200) {
             pkg.routes = routesResponse.data
             pkg.store = `/${pkg.name}/store.js`
           }
           tmpPackages.push(pkg)
-
         } catch (err) {
           console.log(`Could not add frontendpackage: ${pkg.name}`)
         }
       }
       context.commit('setPackages', tmpPackages)
-  },
-  installApp: (context, app) => {
-    context.dispatch('wait/start', 'installApp', {
-      root: true
-    })
-    packageService.installApp(app).then(response => {
-      context.dispatch('wait/end', 'installApp', {
+    },
+    installApp: (context, app) => {
+      context.dispatch('wait/start', 'installApp', {
         root: true
       })
+      packageService.installApp(app).then(response => {
+        context.dispatch('wait/end', 'installApp', {
+          root: true
+        })
 
-      setTimeout(function () { console.log("trying to get pkgs after timeout"), context.dispatch('getInstalledPackages') }, 2000)
-
-    })
-  },
-  uninstallApp: (context, app) => {
-    context.dispatch('wait/start', 'uninstallApp', {
-      root: true
-    })
-    packageService.uninstallApp(app.appname.toLowerCase()).then(response => {
-      context.dispatch('wait/end', 'uninstallApp', {
+        setTimeout(function () {
+          console.log('trying to get pkgs after timeout')
+          context.dispatch('getInstalledPackages')
+        }, 2000)
+      })
+    },
+    uninstallApp: (context, app) => {
+      context.dispatch('wait/start', 'uninstallApp', {
         root: true
       })
-      setTimeout(function () { console.log("trying to get pkgs after timeout"), context.dispatch('getInstalledPackages') }, 2000)
-    })
-  }
-},
+      packageService.uninstallApp(app.appname.toLowerCase()).then(response => {
+        context.dispatch('wait/end', 'uninstallApp', {
+          root: true
+        })
+        setTimeout(function () {
+          console.log('trying to get pkgs after timeout')
+          context.dispatch('getInstalledPackages')
+        }, 2000)
+      })
+    }
+  },
   mutations: {
-  setPackages: (state, pkg) => {
-    console.log("setpkgs: ", pkg)
-    state.packages = pkg
-  },
+    setPackages: (state, pkg) => {
+      console.log('setpkgs: ', pkg)
+      state.packages = pkg
+    },
     updatePackage: (state, pkg) => {
       state.packages.find(x => x.name === pkg.name).installed = pkg.installed
     }
-},
-getters: {
-  installedPackages: (state) => state.packages
-}
+  },
+  getters: {
+    installedPackages: (state) => state.packages
+  }
 })
