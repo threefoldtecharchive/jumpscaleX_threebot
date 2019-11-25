c1d1a7554acefe080981b4c09f8464e58ebf9ed8
kristof de spiegeleer <>
2019-11-25 10:20:40 +0100

    package rework

--- ThreeBotPackages/zerobot/alerta_ui/package.py
+++ ThreeBotPackages/zerobot/alerta_ui/package.py
@@ -2,6 +2,11 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
+    def prepare(self):
+        bcdb = self.bcdb
+        j.shell()
+        w
+
     def start(self):
         """
         called when the 3bot starts the package
