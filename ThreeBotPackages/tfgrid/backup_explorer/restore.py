cf99be8e5d9dc1e130b8734dd06dfe7183f4da44
serboctor <boctor.sarah@gmail.com>
2019-11-25 17:06:17 +0200

    Modify restore script to use zdb

--- ThreeBotPackages/tfgrid/backup_explorer/restore.py
+++ ThreeBotPackages/tfgrid/backup_explorer/restore.py
@@ -4,13 +4,29 @@ from Jumpscale import j
 dest = "/bcdb_export"
 reset = True
 
-j.shell()
+namespaces = {
+    "tfgrid_directory": "tfgrid",
+    "tf_workloads": "tfgrid",
+    "tf_grid_token": "tfgrid",
+    "threebot_phonebook": "tfgrid",
+    "tf_directory": "tfgrid",
+    "users": "zerobot",
+}
+sonic, zdb = j.data.bcdb.threebot_zdb_sonic_start()
+adminsecret_ = j.data.hash.md5_string(zdb.adminsecret_)
+zdb_admin = zdb.client_admin_get()
 
-for bcdb_name in bcdbs:
-    bcdb = j.data.bcdb.get(name=bcdb_name)
-    bcdb.import_(f"{dest}/{bcdb_name}", reset=False)
 
-j.shell()
+for path in j.sal.fs.listDirsInDir(dest):
+    bcdb_name = j.sal.fs.getBaseName(path)
+    if bcdb_name == "system":
+        bcdb = j.data.bcdb.system
+    else:
+        namespace = namespaces[bcdb_name]
+        if not zdb_admin.namespace_exists(namespace):
+            zdb_admin.namespace_new(namespace, secret=adminsecret_, maxsize=0, die=True)
 
+        storclient = zdb.client_get(namespace, adminsecret_)
+        bcdb = j.data.bcdb.get(name="threebot_%s_%s" % ("zdb", namespace), storclient=storclient)
 
-# download()
+    bcdb.import_(path)
