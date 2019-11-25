2f54b36eeff8c12b194b85ec592e3b7b4d85597d
xmonader <xmonader@gmail.com>
2019-11-25 20:17:46 +0200

    update webhooks

--- ThreeBotPackages/zerobot/webhooks/package.py
+++ ThreeBotPackages/zerobot/webhooks/package.py
@@ -10,19 +10,20 @@ class Package(j.baseclasses.threebot_package):
 
         server = self.openresty
         server.configure()
-        website = server.get_from_port(port=443)
-        locations = website.locations.get("main_webhooks")
+        for port in (443, 80):
+            website = server.get_from_port(port=port)
+            locations = website.locations.get("main_webhooks")
 
-        from threebot_packages.zerobot.webhooks.bottle.main import app
+            from threebot_packages.zerobot.webhooks.bottle.main import app
 
-        self.gevent_rack.bottle_server_add(name="webhooks", port=8530, app=app)
+            self.gevent_rack.bottle_server_add(name="webhooks", port=8530, app=app)
 
-        proxy_location = locations.locations_proxy.new()
-        proxy_location.name = "webhooks"
-        proxy_location.path_url = "/webhooks"
-        proxy_location.ipaddr_dest = "0.0.0.0"
-        proxy_location.port_dest = 8530
-        proxy_location.scheme = "http"
+            proxy_location = locations.locations_proxy.new()
+            proxy_location.name = "webhooks"
+            proxy_location.path_url = "/webhooks"
+            proxy_location.ipaddr_dest = "0.0.0.0"
+            proxy_location.port_dest = 8530
+            proxy_location.scheme = "http"
 
-        locations.configure()
-        website.configure()
+            locations.configure()
+            website.configure()
