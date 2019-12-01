c2a06cb18cd986b71ca74b0fb819ed02644bfe2e
kristof de spiegeleer <>
2019-12-01 10:57:31 +0100

    large refactor for BCBD/threebotserver

--- ThreeBotPackages/tfgrid/tfgrid_network/examples/serverprepation.py
+++
@@ -1,7 +0,0 @@
-# first we need to create our network
-
-from Jumpscale import j
-
-client = j.clients.gedis.get(port=9901)
-client.actors.gridnetwork.network_add("overlay", "10.2.0.0/16")
-client.actors.gridnetwork.network_endpoint_add("overlay", "do")
