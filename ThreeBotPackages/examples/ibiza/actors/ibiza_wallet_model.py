fe628ed43c8d5a9ad55154d3fbf670fd571e68e5
kristof de spiegeleer <>
2019-12-12 19:08:37 +0200

    format

---
+++ ThreeBotPackages/examples/ibiza/actors/ibiza_wallet_model.py
@@ -0,0 +1,101 @@
+from Jumpscale import j
+
+
+class ibiza_wallet_model(j.baseclasses.threebot_actor):
+    def _init(self, **kwargs):
+        # get bcdb from package
+        self.bcdb = self.package.bcdb
+        self.model = self.bcdb.model_get(url="jumpscale.ibiza.ibiza.wallet")
+
+    @j.baseclasses.actor_method
+    def new(self, schema_out=None, user_session=None, **kwargs):
+        """
+        ```in
+        jwt = "" (s)                # jwt token
+        addr**= ""                  # address
+        ipaddr = (ipaddr)           # ip address
+        email = "" (s)              # email address
+        username = "" (s)           # user name
+        ```
+        ```out
+        res = (O) !jumpscale.ibiza.ibiza.wallet
+        ```
+        """
+        assert user_session.admin  # for now only allow admin
+        return self.model.set_dynamic(kwargs)
+
+    @j.baseclasses.actor_method
+    def set(self, object_id=None, values=None, schema_out=None, user_session=None):
+        """
+        ```in
+        object_id = 0
+        values = (dict)
+        ```
+        ```out
+        res = (O) !jumpscale.ibiza.ibiza.wallet
+        ```
+        """
+        # TODO: use user_session for authentication
+        assert user_session.admin  # for now only allow admin
+        obj = self.model.get(object_id)
+
+        for key, val in values.items():
+            setattr(obj, key, val)
+        obj.save()
+
+        return obj
+
+    @j.baseclasses.actor_method
+    def get_by_name(self, name=None, schema_out=None, user_session=None):
+        """
+        ```in
+        name = (S)
+        ```
+        ```out
+        res = (O) !jumpscale.ibiza.ibiza.wallet
+        ```
+        """
+        assert user_session.admin  # for now only allow admin
+        return self.model.get_by_name(name)
+
+    @j.baseclasses.actor_method
+    def get(self, object_id=None, schema_out=None, user_session=None):
+        """
+        ```in
+        object_id = 0
+        ```
+        ```out
+        res = (O) !jumpscale.ibiza.ibiza.wallet
+        ```
+        """
+        assert user_session.admin  # for now only allow admin
+        return self.model.get(object_id)
+
+    @j.baseclasses.actor_method
+    def find(self, query=None, schema_out=None, user_session=None):
+        """
+        ```in
+        query = (dict)
+        ```
+        ```out
+        res = (LO) !jumpscale.ibiza.ibiza.wallet
+        ```
+        """
+        assert user_session.admin  # for now only allow admin
+        return self.model.find(query)
+
+    @j.baseclasses.actor_method
+    def delete(self, object_id=None, schema_out=None, user_session=None):
+        """
+        ```in
+        object_id = 0
+        ```
+        """
+        assert user_session.admin  # for now only allow admin
+        obj = self.model.get(object_id)
+        obj.delete()
+
+    @j.baseclasses.actor_method
+    def destroy(self, schema_out=None, user_session=None):
+        assert user_session.admin  # for now only allow admin
+        return self.model.destroy()
