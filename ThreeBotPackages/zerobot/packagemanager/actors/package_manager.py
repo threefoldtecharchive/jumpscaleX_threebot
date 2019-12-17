72b1883977b8f49332975849a303b1c86566ad57
kristof de spiegeleer <>
2019-12-17 15:42:06 +0200

    threebot fixes

--- ThreeBotPackages/zerobot/packagemanager/actors/package_manager.py
+++ ThreeBotPackages/zerobot/packagemanager/actors/package_manager.py
@@ -69,6 +69,7 @@ class package_manager(j.baseclasses.threebot_actor):
             package.install()
             package.save()
             package.start()
+            package.models
             package.actors_reload()
         except Exception as e:
             self._log_error(str(e), exception=e)
