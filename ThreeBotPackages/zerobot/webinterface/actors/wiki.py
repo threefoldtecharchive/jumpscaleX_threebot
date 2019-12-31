029e65bbc81aab4d255541ffb3860be177f4aa1c
kristof de spiegeleer <>
2019-12-31 10:49:16 +0200

    threebot works with threebotme

--- ThreeBotPackages/zerobot/webinterface/actors/wiki.py
+++ ThreeBotPackages/zerobot/webinterface/actors/wiki.py
@@ -3,8 +3,7 @@ from os.path import splitext
 
 
 class wiki(j.baseclasses.threebot_actor):
-
-    # REMARK: was query before
+    @j.baseclasses.actor_method
     def find(self, name, text, schema_out=None, user_session=None):
         """
         ```in
@@ -24,7 +23,7 @@ class wiki(j.baseclasses.threebot_actor):
             res = [
                 splitext(entry)[0].lower()
                 for entry in j.threebot.servers.sonic.default_client.query("docsites", name, text)
-                if "siderbar" not in entry
+                if "sidebar" not in entry
             ]
         except Exception as e:
             print(e)
