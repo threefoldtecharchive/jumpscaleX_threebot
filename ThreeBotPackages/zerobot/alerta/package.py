cf259bf8234ff1d9df408214f23b477f9b968537
xmonader <xmonader@gmail.com>
2020-01-15 14:24:56 +0200

    Development wiki3 (#342)
    * wiki3
    
    * make sure gdrive client does not stop actor from loading
    
    * package actor only shows installed ones
    
    * update directory frontend
    
    * compare enums directly
    
    Co-authored-by: Christophe de Carvalho <zaibon@users.noreply.github.com>
    Co-authored-by: Andrew Ayoub <prog.andrew2015@yahoo.com>

--- ThreeBotPackages/zerobot/alerta/package.py
+++ ThreeBotPackages/zerobot/alerta/package.py
@@ -2,9 +2,11 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
-    def start(self):
-        """
-        called when the 3bot starts the package
-        :return:
-        """
-        j.tools.alerthandler.setup()
+    pass
+    # def start(self):
+    #     """
+    #     called when the 3bot starts the package
+    #     :return:
+    #     """
+    #     j.tools.alerthandler.setup()
+    # already done in __init__
