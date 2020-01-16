c6f9b94aa1d518be10c685674e1aff7e904b8667
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-01-16 14:42:11 +0100

    update provision example scripts

--- ThreeBotPackages/tfgrid/provision_examples/zdb.py
+++ ThreeBotPackages/tfgrid/provision_examples/zdb.py
@@ -1,6 +1,27 @@
 from Jumpscale import j
 
+from nacl import public
+from nacl.signing import VerifyKey, SigningKey
+from nacl.encoding import Base64Encoder
+from nacl.public import SealedBox
+
+
 import time
+
+# consider a node up if it has received update during the last 10 minute
+def is_up(node):
+    ago = j.data.time.epoch - (60 * 10)
+    return node.updated > ago
+
+
+def encrypt_password(password, public_key):
+    node_public_bin = j.data.hash.hex2bin(public_key)
+    node_public = VerifyKey(node_public_bin)
+    box = SealedBox(node_public.to_curve25519_public_key())
+
+    pasword_encrypted = box.encrypt(password.encode())
+    pasword_encrypted_hex = j.data.hash.bin2hex(pasword_encrypted)
+    return pasword_encrypted_hex
 
 
 def deploy_zdbs():
@@ -10,6 +31,9 @@ def deploy_zdbs():
 
     # list all nodes that have 10 GiB of SSD
     nodes = explorer.actors_all.nodes.list(sru=10).nodes
+    # filter out nodes that are down
+    nodes = list(filter(is_up, nodes))
+    # pick the 2 first one
     node_1 = nodes[0]
     node_2 = nodes[1]
 
@@ -26,7 +50,7 @@ def deploy_zdbs():
     zdb_1.node_id = node_1.node_id
     zdb_1.size = 10
     zdb_1.mode = "user"
-    zdb_1.password = "supersecret"
+    zdb_1.password = encrypt_password("supersecret", node_1.public_key_hex)
     zdb_1.disk_type = "ssd"
     zdb_1.public = False
 
@@ -35,7 +59,7 @@ def deploy_zdbs():
     zdb_2.node_id = node_2.node_id
     zdb_2.size = 10
     zdb_2.mode = "user"
-    zdb_2.password = "supersecret"
+    zdb_2.password = encrypt_password("supersecret", node_2.public_key_hex)
     zdb_2.disk_type = "ssd"
     zdb_2.public = False
 
@@ -57,3 +81,7 @@ def deploy_zdbs():
         else:
             data = j.data.serializers.json.loads(result.data_json)
             print(f"ZDB deployed at {data['IP']}:{data['Port']} namespace {data['Namespace']}")
+
+
+if __name__ == "__main__":
+    deploy_zdbs()
