e37df63b4d307643700ddb3d9e2853fa01bc4df6
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-01-22 18:37:12 +0100

    Update alerta front-end to use new redis alert handler and actor (#353)
    * update alerta actor to use new redis alert handler
    
    * update alerta UI to use new actor information
    
    * update alerta UI build
    
    * use event dispatcher to delete an alert in parent component
    
    Thanks @RafyAmgadBenjamin :)
    
    * update alerta frontend build

--- ThreeBotPackages/zerobot/alerta/frontend_src/src/routes/data.js
+++ ThreeBotPackages/zerobot/alerta/frontend_src/src/routes/data.js
@@ -1,16 +1,16 @@
 
 import axios from 'axios'
 
-export function getAlert(id) {
-    return axios.get("/zerobot/alerta_ui/actors/alerta/get_alert", { "params": { "alert_id": id } });
+export function getAlert(identifier) {
+    return axios.get("/zerobot/alerta_ui/actors/alerta/get_alert", { "params": { "identifier": identifier } });
 }
-export function getAlerts(envName = "all") {
-    return (axios.get("/zerobot/alerta_ui/actors/alerta/list_alerts_by_env", { "params": { "env_name": envName } }))
+export function getAlerts(cat = "") {
+    return (axios.get("/zerobot/alerta_ui/actors/alerta/list_alerts_by_category", { "params": { "cat": cat } }))
 }
 export function deleteAll() {
     return (axios.post("/zerobot/alerta_ui/actors/alerta/delete_all_alerts"))
 
 }
-export function deleteAlert(alertId) {
-    return (axios.post("/zerobot/alerta_ui/actors/alerta/delete_alert", { "args": { "alert_id": alertId } }))
+export function deleteAlert(identifier) {
+    return (axios.post("/zerobot/alerta_ui/actors/alerta/delete_alert", { "args": { "identifier": identifier } }))
 }
--- ThreeBotPackages/zerobot/alerta/package.py
+++ ThreeBotPackages/zerobot/alerta/package.py
@@ -3,10 +3,3 @@ from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
     pass
-    # def start(self):
-    #     """
-    #     called when the 3bot starts the package
-    #     :return:
-    #     """
-    #     j.tools.alerthandler.setup()
-    # already done in __init__
