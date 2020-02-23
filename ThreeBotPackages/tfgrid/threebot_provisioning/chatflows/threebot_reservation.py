f360a1d51ca8670734a1d14c0a0267e94b76cb67
BolaNasr <bola.nasr@live.com>
2020-02-23 18:39:45 +0200

    change in chatflow

--- ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
+++ ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
@@ -23,11 +23,16 @@ def chat(bot):
     user = j.sal.zosv2.get_user(name=name, email=email)
     expiration_provisioning = j.data.time.epoch + (60 * 10)
     expiration_reservation = j.data.time.epoch + (60 * 10)
-    reservation = j.sal.zosv2.new_reservation(user, expiration_provisioning, expiration_reservation)
+    reservation = j.sal.zosv2.new_reservation(expiration_provisioning, expiration_reservation)
 
     ip = bot.single_choice("choose your ip version", ips)
 
-    ip_address = bot.drop_down_choice("Please choose ip address of the container", j.sal.zosv2.get_all_ips(ip_range))
+    avalible_ip =  j.sal.zosv2.get_all_ips(ip_range)
+    string_ips = []
+    for ip in avalible_ip:
+        string_ips.append(ip.format())
+
+    ip_address = bot.drop_down_choice("Please choose ip address of the container", string_ips)
 
     if ip == "IPV4":
         node, public_ip = j.sal.zosv2.get_node(4)
@@ -46,10 +51,10 @@ def chat(bot):
 
     #Add volume and create container schema
     vol1 = j.sal.zosv2.add_volume(reservation, node.node_id,volume_size=10)
-    cont = j.sal.zosv2.create_container(network.name,ip_address,node.node_id, reservation, conatiner_flist, storage_url,env, entry_point)
+    cont = j.sal.zosv2.create_container(network.name,ip_address, ip_range, node.node_id, reservation, conatiner_flist, storage_url,env, entry_point)
     j.sal.zosv2.attach_volume(cont, vol1)
 
-    resv_id = j.sal.zosv2.register(reservation)
+    resv_id = j.sal.zosv2.register(user, reservation)
 
     res = """# reservation sent. ID: {}
         """.format(resv_id)
