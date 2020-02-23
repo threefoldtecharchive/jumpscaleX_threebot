f6328d6f699f4c1f49ff23295873e94e3204f3dc
BolaNasr <bola.nasr@live.com>
2020-02-23 16:04:44 +0200

    change some behaviour follow the comments of PR

--- ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
+++ ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
@@ -19,8 +19,12 @@ def chat(bot):
 
     ip_range = bot.string_ask("Please add ip range of the network")
 
+    #create new reservation
     user = j.sal.zosv2.get_user(name=name, email=email)
-    reservation = j.sal.zosv2.new_reservation(user)
+    expiration_provisioning = j.data.time.epoch + (60 * 10)
+    expiration_reservation = j.data.time.epoch + (60 * 10)
+    reservation = j.sal.zosv2.new_reservation(user, expiration_provisioning, expiration_reservation)
+
     ip = bot.single_choice("choose your ip version", ips)
 
     ip_address = bot.drop_down_choice("Please choose ip address of the container", j.sal.zosv2.get_all_ips(ip_range))
@@ -30,25 +34,26 @@ def chat(bot):
     else:
         node, public_ip = j.sal.zosv2.get_node(6)
 
+    #Create network of reservation and add peers
     reservation, network = j.sal.zosv2.create_network(reservation,ip_range,node.node_id)
-
-    network, network_config = j.sal.zosv2.access_node(network,ip_range,node)
+    network_resource = j.sal.zosv2.add_network(network,ip_range,node)
+    network_config = j.sal.zosv2.add_peer_network(network_resource, ip_range)
 
     env = {"corex_password":password,"corex_user":user_corex,"pub_key":pub_key}
-    entrypoint = "/usr/bin/zinit init -d"
-    flist = "https://hub.grid.tf/bola_nasr_1/threefoldtech-3bot-corex.flist"
+    entry_point = "/usr/bin/zinit init -d"
+    conatiner_flist = "https://hub.grid.tf/bola_nasr_1/threefoldtech-3bot-corex.flist"
+    storage_url ="zdb://hub.grid.tf:9900"
 
-    reservation =  j.sal.zosv2.create_container(network.name,ip_address,node.node_id, reservation, flist, env, entrypoint,volume_size=10)
+    #Add volume and create container schema
+    vol1 = j.sal.zosv2.add_volume(reservation, node.node_id,volume_size=10)
+    cont = j.sal.zosv2.create_container(network.name,ip_address,node.node_id, reservation, conatiner_flist, storage_url,env, entry_point)
+    j.sal.zosv2.attach_volume(cont, vol1)
 
     resv_id = j.sal.zosv2.register(reservation)
+
     res = """# reservation sent. ID: {}
         """.format(resv_id)
     bot.md_show(res)
-    if j.sal.zosv2.reservation_result(resv_id) == []:
-        res = """# You have error in reservation , Please Try again
-        """
-        bot.md_show(res)
-        return
 
     filename = "{}_{}.conf".format(name,resv_id)
     result = j.sal.zosv2.wg_config(ip_range,network_config)
