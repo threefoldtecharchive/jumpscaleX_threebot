5a870ea06ac53ca64df8e1407b50d2d7abd0ca94
Waleed Hammam <waleed.hammam@gmail.com>
2020-02-23 13:16:55 +0200

    init threebot admin dashboard

--- ThreeBotPackages/zerobot/dashboard/html/js/scripts.js
+++ ThreeBotPackages/zerobot/dashboard/html/js/scripts.js
@@ -1,4 +1,9 @@
-(function($) {
+/*!
+    * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    (function($) {
     "use strict";
 
     // Add active state to sidbar nav links
