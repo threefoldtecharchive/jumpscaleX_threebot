c175a55fc4e1e28c771741e1b5a8ab2f61859d71
BolaNasr <bola.nasr@live.com>
2020-02-24 20:03:24 +0200

    add new field in schema and make some changes in chatflow

--- ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
+++ ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
@@ -1,5 +1,13 @@
 from Jumpscale import j
+import  netaddr
 
+
+def get_all_ips(ip_range):
+    networks = netaddr.IPNetwork(ip_range)
+    ips = []
+    for ip in list(networks.iter_hosts()):
+        ips.append(ip.format())
+    return ips
 
 def chat(bot):
     """
@@ -9,9 +17,12 @@ def chat(bot):
     name = user_info["username"]
     email = user_info["email"]
     ips = ["IPV6", "IPV4"]
+    choose = ["New","Restore"]
+    explorer = j.clients.threebot.explorer
 
     if not name or not email:
         bot.md_show("Username or email not found in session. Please log in properly")
+    user_choice = bot.single_choice("you want new container or restore previous one ?", choose)
 
     pub_key = bot.string_ask("Please add your public ssh-key ")
     user_corex = bot.string_ask("username of your coreX")
@@ -20,29 +31,39 @@ def chat(bot):
     ip_range = bot.string_ask("Please add ip range of the network")
 
     #create new reservation
-    user = j.sal.zosv2.get_user(name=name, email=email)
-    expiration_provisioning = j.data.time.epoch + (60 * 10)
-    expiration_reservation = j.data.time.epoch + (60 * 10)
-    reservation = j.sal.zosv2.new_reservation(expiration_provisioning, expiration_reservation)
+    reservation = j.sal.zosv2.reservation_create()
+    identity = explorer.actors_all.phonebook.get(name=name, email=email)
+    nodes = j.sal.zosv2.nodes_finder.nodes_search()
 
     ip = bot.single_choice("choose your ip version", ips)
 
-    avalible_ip =  j.sal.zosv2.get_all_ips(ip_range)
+    if ip == "IPV4":
+        for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip4, nodes):
+            node_selected = node
+    else:
+        for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip6, nodes):
+            node_selected = node
+
+    #Create network of reservation and add peers
+    network = j.sal.zosv2.network.create(reservation, ip_range)
+    network_range = netaddr.IPNetwork(ip_range).ip
+    network_range += 256
+    network_node = str(network_range) + "/24"
+
+    avalible_ip = get_all_ips(network_node)
     string_ips = []
     for ip in avalible_ip:
         string_ips.append(ip.format())
 
     ip_address = bot.drop_down_choice("Please choose ip address of the container", string_ips)
 
-    if ip == "IPV4":
-        node, public_ip = j.sal.zosv2.get_node(4)
-    else:
-        node, public_ip = j.sal.zosv2.get_node(6)
+    j.sal.zosv2.network.add_node(network, node_selected.node_id, network_node)
+    network_range += 256
+    network_node = str(network_range) + "/24"
 
-    #Create network of reservation and add peers
-    reservation, network = j.sal.zosv2.create_network(reservation,ip_range,node.node_id)
-    network_resource = j.sal.zosv2.add_network(network,ip_range,node)
-    network_config = j.sal.zosv2.add_peer_network(network_resource, ip_range)
+    wg_quick = j.sal.zosv2.network.add_access(
+        network, node_selected.node_id, network_node, ipv4=True
+    )
 
     env = {"corex_password":password,"corex_user":user_corex,"pub_key":pub_key}
     entry_point = "/usr/bin/zinit init -d"
@@ -50,18 +71,22 @@ def chat(bot):
     storage_url ="zdb://hub.grid.tf:9900"
 
     #Add volume and create container schema
-    vol1 = j.sal.zosv2.add_volume(reservation, node.node_id,volume_size=10)
-    cont = j.sal.zosv2.create_container(network.name,ip_address, ip_range, node.node_id, reservation, conatiner_flist, storage_url,env, entry_point)
-    j.sal.zosv2.attach_volume(cont, vol1)
+    vol = j.sal.zosv2.volume.create(reservation, node_selected.node_id,volume_size=10)
+    # create container
+    cont = j.sal.zosv2.container.create(reservation=reservation, node_id=node_selected.node_id, network_name=network.name, ip_address=ip_address, flist=conatiner_flist,
+                                        storage_url=storage_url, env=env, entrypoint=entry_point)
+    j.sal.zosv2.volume.attach(reservation, cont, vol, "/sandbox/var")
 
-    resv_id = j.sal.zosv2.register(user, reservation)
+    expiration = j.data.time.epoch + (60 * 60)
+
+    resv_id = j.sal.zosv2.reservation_register(reservation, expiration)
 
     res = """# reservation sent. ID: {}
         """.format(resv_id)
     bot.md_show(res)
 
     filename = "{}_{}.conf".format(name,resv_id)
-    result = j.sal.zosv2.wg_config(ip_range,network_config)
+
     res = """
             # use the next template to configure the wg-quick config of your laptop:
             ### ```wg-quick up /etc/wireguard/{}```
@@ -71,7 +96,7 @@ def chat(bot):
     res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
     bot.md_show(res)
 
-    res = j.tools.jinja2.template_render(text=result, **locals())
+    res = j.tools.jinja2.template_render(text=wg_quick, **locals())
     bot.download_file(res, filename)
 
 
