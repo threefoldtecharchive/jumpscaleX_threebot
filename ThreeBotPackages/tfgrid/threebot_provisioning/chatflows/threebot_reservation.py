636c3b57523ebfbbb942bcf78035f529be901c3a
BolaNasr <bola.nasr@live.com>
2020-02-25 14:39:44 +0200

    add new feature in chatflow

--- ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
+++ ThreeBotPackages/tfgrid/threebot_provisioning/chatflows/threebot_reservation.py
@@ -22,85 +22,95 @@ def chat(bot):
     if not name or not email:
         bot.md_show("Username or email not found in session. Please log in properly")
     user_choice = bot.single_choice("you want new container or restore previous one ?", choose)
+    if user_choice == "Restore":
+        res = "# This Feature still in progress"
+        res = j.tools.jinja2.template_render(text=res, **locals())
+        bot.md_show(res)
 
-    pub_key = bot.string_ask("Please add your public ssh-key ")
-    user_corex = bot.string_ask("username of your coreX")
-    password = bot.secret_ask("password of your coreX")
-
-    ip_range = bot.string_ask("Please add ip range of the network")
-
-    #create new reservation
-    reservation = j.sal.zosv2.reservation_create()
-    identity = explorer.actors_all.phonebook.get(name=name, email=email)
-    nodes = j.sal.zosv2.nodes_finder.nodes_search()
-
-    ip = bot.single_choice("choose your ip version", ips)
-
-    if ip == "IPV4":
-        for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip4, nodes):
-            node_selected = node
     else:
-        for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip6, nodes):
-            node_selected = node
+        pub_key = bot.string_ask("Please add your public ssh-key ")
+        user_corex = bot.string_ask("username of your coreX")
+        password = bot.secret_ask("password of your coreX")
 
-    #Create network of reservation and add peers
-    network = j.sal.zosv2.network.create(reservation, ip_range)
-    network_range = netaddr.IPNetwork(ip_range).ip
-    network_range += 256
-    network_node = str(network_range) + "/24"
+        ip_range = bot.string_ask("Please add ip range of the network")
 
-    avalible_ip = get_all_ips(network_node)
-    string_ips = []
-    for ip in avalible_ip:
-        string_ips.append(ip.format())
+        #create new reservation
+        reservation = j.sal.zosv2.reservation_create()
+        identity = explorer.actors_all.phonebook.get(name=name, email=email)
+        nodes = j.sal.zosv2.nodes_finder.nodes_search()
 
-    ip_address = bot.drop_down_choice("Please choose ip address of the container", string_ips)
+        ip = bot.single_choice("choose your ip version", ips)
 
-    j.sal.zosv2.network.add_node(network, node_selected.node_id, network_node)
-    network_range += 256
-    network_node = str(network_range) + "/24"
+        if ip == "IPV4":
+            for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip4, nodes):
+                node_selected = node
+        else:
+            for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip6, nodes):
+                node_selected = node
 
-    wg_quick = j.sal.zosv2.network.add_access(
-        network, node_selected.node_id, network_node, ipv4=True
-    )
-    expiration = j.data.time.epoch + (3600 * 24 * 365)
-    # register the reservation
-    rid = j.sal.zosv2.reservation_register(reservation, expiration)
+        #Create network of reservation and add peers
+        network = j.sal.zosv2.network.create(reservation, ip_range)
+        network_range = netaddr.IPNetwork(ip_range).ip
+        network_range += 256
+        network_node = str(network_range) + "/24"
 
-    env = {"corex_password":password,"corex_user":user_corex,"pub_key":pub_key}
-    entry_point = "/usr/bin/zinit init -d"
-    conatiner_flist = "https://hub.grid.tf/bola_nasr_1/threefoldtech-3bot-corex.flist"
-    storage_url ="zdb://hub.grid.tf:9900"
+        avalible_ip = get_all_ips(network_node)
+        string_ips = []
+        for ip in avalible_ip:
+            string_ips.append(ip.format())
 
-    #Add volume and create container schema
-    vol = j.sal.zosv2.volume.create(reservation, node_selected.node_id,volume_size=10)
-    # create container
-    cont = j.sal.zosv2.container.create(reservation=reservation, node_id=node_selected.node_id, network_name=network.name, ip_address=ip_address, flist=conatiner_flist,
-                                        storage_url=storage_url, env=env, entrypoint=entry_point)
-    j.sal.zosv2.volume.attach(reservation, cont, vol, "/sandbox/var")
+        ip_address = bot.drop_down_choice("Please choose ip address of the container", string_ips)
 
-    expiration = j.data.time.epoch + (3600 * 24 * 365)
+        j.sal.zosv2.network.add_node(network, node_selected.node_id, network_node)
+        network_range += 256
+        network_node = str(network_range) + "/24"
 
-    resv_id = j.sal.zosv2.reservation_register(reservation, expiration)
+        wg_quick = j.sal.zosv2.network.add_access(
+            network, node_selected.node_id, network_node, ipv4=True
+        )
+        expiration = j.data.time.epoch + (3600 * 24 * 365)
+        # register the reservation
+        rid = j.sal.zosv2.reservation_register(reservation, expiration)
 
-    res = """# reservation sent. ID: {}
-        """.format(resv_id)
-    bot.md_show(res)
+        env = {"corex_password":password,"corex_user":user_corex,"pub_key":pub_key}
+        entry_point = "/usr/bin/zinit init -d"
+        conatiner_flist = "https://hub.grid.tf/bola_nasr_1/threefoldtech-3bot-corex.flist"
+        storage_url ="zdb://hub.grid.tf:9900"
 
-    filename = "{}_{}.conf".format(name,resv_id)
+        #Add volume and create container schema
+        vol = j.sal.zosv2.volume.create(reservation, node_selected.node_id,volume_size=10)
+        # create container
+        cont = j.sal.zosv2.container.create(reservation=reservation, node_id=node_selected.node_id, network_name=network.name, ip_address=ip_address, flist=conatiner_flist,
+                                            storage_url=storage_url, env=env, entrypoint=entry_point)
+        j.sal.zosv2.volume.attach(reservation, cont, vol, "/sandbox/var")
 
-    res = """
-            # use the next template to configure the wg-quick config of your laptop:
-            ### ```wg-quick up /etc/wireguard/{}```
-            Click next
-            to download your configuration
-            """.format(filename)
-    res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
-    bot.md_show(res)
+        expiration = j.data.time.epoch + (3600 * 24 * 365)
 
-    res = j.tools.jinja2.template_render(text=wg_quick, **locals())
-    bot.download_file(res, filename)
+        resv_id = j.sal.zosv2.reservation_register(reservation, expiration)
 
+        res = """# reservation sent. ID: {}
+            """.format(resv_id)
+        bot.md_show(res)
+
+        filename = "{}_{}.conf".format(name,resv_id)
+
+        res = """
+                # use the next template to configure the wg-quick config of your laptop:
+                ### ```wg-quick up /etc/wireguard/{}```
+                Click next
+                to download your configuration
+                """.format(filename)
+        res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
+        bot.md_show(res)
+
+        res = j.tools.jinja2.template_render(text=wg_quick, **locals())
+        bot.download_file(res, filename)
+
+        res = """
+                # Open your browser at ```{}:1500```
+                """.format(ip_address)
+        res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
+        bot.md_show(res)
 
 
 
