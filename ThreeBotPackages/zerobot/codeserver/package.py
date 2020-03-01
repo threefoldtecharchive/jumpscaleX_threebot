9102abcb27fb6ec1ebc4cb152165da7e3352c7e7
Rana Tarek Hassan <tarekr@codescalers.com>
2020-03-01 13:31:16 +0200

    Add auth for codeserver WIP

--- ThreeBotPackages/zerobot/codeserver/package.py
+++ ThreeBotPackages/zerobot/codeserver/package.py
@@ -14,23 +14,53 @@ class Package(j.baseclasses.threebot_package):
             website = server.get_from_port(port=port)
             locations = website.locations.get(f"codeserver_{port}")
 
-            proxy_location = locations.locations_proxy.new()
-            proxy_location.name = "codeserver"
-            proxy_location.path_url = "/codeserver/"
+            auth_location = locations.get_location_custom("codeserver")
+            auth_location.config = """
+            location /codeserver/ {
+                # FIXME: need to update nginx/openresty with auth module, know yields unkown directive
+                # auth_request /admin/codeserver/auth;
+
+                access_by_lua '
+                    local res = ngx.location.capture("/admin/codeserver/auth")
+
+                    if res.status == ngx.HTTP_OK then
+                        return
+                    else
+                        return ngx.redirect("/admin/codeserver")
+                    end
+                ';
+
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection "Upgrade";
+                proxy_set_header HOST $host;
+                proxy_set_header X-Forwarded-Proto $scheme;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+                proxy_pass HTTP://127.0.0.1:8080/;
+            }
+            """
+
+            proxy_location = locations.get_location_proxy("admin_codeserver")
+            proxy_location.path_url = "/admin/codeserver/"
             proxy_location.ipaddr_dest = "127.0.0.1"
-            proxy_location.port_dest = 8080
+            proxy_location.port_dest = 8909
             proxy_location.path_dest = "/"
             locations.configure()
             website.configure()
 
         # Start code server
-        self.startupcmd = j.servers.startupcmd.get(
-            "codeserver", cmd_start="./code-server --auth none", path="/sandbox/bin", ports=8080
-        )
+        cmd_start = "./code-server --auth none"
+        self.startupcmd = j.servers.startupcmd.get("codeserver", cmd_start=cmd_start, path="/sandbox/bin", ports=8080)
         if not j.sal.fs.exists("/sandbox/bin/code-server"):
             j.builders.apps.codeserver.install()
 
         self.startupcmd.start()
+
+        from threebot_packages.zerobot.codeserver.bottle.rooter import app_with_session
+
+        self.gevent_rack.bottle_server_add(name="codeserver_auth", port=8909, app=app_with_session, websocket=True)
 
     def stop(self):
         # Stop code server
