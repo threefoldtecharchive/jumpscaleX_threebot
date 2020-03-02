8735e1e99a0c12e1611532c39a7ab688ae753c16
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-03-02 23:02:40 +0200

    remove unused code

--- ThreeBotPackages/zerobot/admin/frontend_src/sources/models/useractivity.js
+++
@@ -1,9 +0,0 @@
-export function getUserActivity(){
-	return users;
-}
-
-const users = [
-	{ id:"7", country:"Brazil", users:70 },
-	{ id:"8", country:"Canada", users:80 },
-	{ id:"9", country:"China", users:90 }
-];
