64fbae7b9b79d8022a211980f3854d048f1a0251
hamdy <hamdy.a.farag@gmail.com>
2020-03-03 13:54:12 +0200

    simulation package changes

--- ThreeBotPackages/threefold/simulator/package.py
+++ ThreeBotPackages/threefold/simulator/package.py
@@ -9,17 +9,17 @@ class Package(j.baseclasses.threebot_package):
     def start(self):
         """
         called when the 3bot starts
+        Open notebook through /simulator/threefold/notebook/
+        Open voila directly through /simulator/threefold/show/
         :return:
         """
-
-        j.tools.tfsimulation.start(voila=False, background=True, base_url="/simulator/notebook/")
-        j.tools.tfsimulation.start(voila=True, background=True, base_url="/simulator/notebook/")
+        j.tools.tfsimulation.start(
+            voila=False, background=True, base_url="/simulator/threefold/notebook/", ip="127.0.0.1"
+        )
+        j.tools.tfsimulation.start(voila=True, background=True, base_url="/simulator/threefold/show/", ip="127.0.0.1")
 
         # find token (sleep for 5 seconds until server starts)
         time.sleep(5)
-        t, _ = subprocess.Popen("jupyter notebook list", shell=True, stdout=subprocess.PIPE).communicate()
-
-        token = re.findall(r"token=.*? ", t.decode())[0]
 
         for port in (443, 80):
             website = self.openresty.get_from_port(port)
@@ -28,17 +28,24 @@ class Package(j.baseclasses.threebot_package):
 
             jupyter_proxy_location = locations.get_location_proxy("jupyter")
 
-            jupyter_proxy_location.path_url = "/simulator/notebook/"
+            jupyter_proxy_location.path_url = "/simulator/threefold/notebook/"
             jupyter_proxy_location.ipaddr_dest = "127.0.0.1"
             jupyter_proxy_location.port_dest = 8888
-            jupyter_proxy_location.path_dest = "/simulator/notebook/"
+            jupyter_proxy_location.path_dest = "/simulator/threefold/notebook/"
             jupyter_proxy_location.scheme = "http"
+
+            # voila
+            voila_proxy_location = locations.get_location_proxy("voila")
+            voila_proxy_location.path_url = "/simulator/threefold/show/"
+            voila_proxy_location.ipaddr_dest = "127.0.0.1"
+            voila_proxy_location.port_dest = 8866
+            voila_proxy_location.path_dest = "/simulator/threefold/show/"
+            voila_proxy_location.scheme = "http"
+
             website.configure()
 
         j.data.bcdb.get(name="system")
-        print("*********************************************************************************")
-        print(f"Simulator Url : /simulator/notebook?{token}")
-        print("**********************************************************************************")
+
         self.openresty.reload()
 
     def stop(self):
