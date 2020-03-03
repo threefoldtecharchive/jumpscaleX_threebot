d9dc1e2a1b5cc60718860bbd4e2aeb7cd6e9e9e0
Waleed Hammam <waleed.hammam@gmail.com>
2020-03-03 15:12:13 +0200

    cleanup to dashboard

--- ThreeBotPackages/zerobot/admin/frontend_src/sources/views/dash/bcdbHealth.js
+++ ThreeBotPackages/zerobot/admin/frontend_src/sources/views/dash/bcdbHealth.js
@@ -2,45 +2,48 @@ import {
     JetView
 } from "webix-jet";
 
-export default class BcdbHealthView extends JetView {
+export default class healthInfoView extends JetView {
     config() {
-        const bcdb_info = {
-            id: "bcdbHealth",
+        const health_info = {
+            id: "healthInfo",
             responsive: true,
-            view: "template",
+            view: "list",
             responsive: true,
             type: {
-                height: 200,
+                height: 60,
             },
-            template: ""
+            template: `
+            <p><font size="3"><b>#key#: </b></font> #value#</p>
+            `
         }
 
         return {
             type: "space",
             rows: [{
-                template: "<div style='width:auto;text-align:center'><h3>BCDB Health<h3/></div>",
+                template: "<div style='width:auto;text-align:center'><h3>Health Checks<h3/></div>",
                 height: 50
             },
-                bcdb_info]
+                health_info]
         }
     }
     init(view) {
         var self = this;
 
-        this.bcdb_info = this.$$("bcdbHealth");
+        this.health_info = this.$$("healthInfo");
 
         webix.ajax().get("/zerobot/admin/actors/health/bcdb_health", function (data) {
             data = JSON.parse(data);
 
             if (data.state === "OK") {
-                self.bcdb_info.setHTML(`
-                <span class='webix_icon wxi-checkbox-marked' style="color:green;width: 100%;font-size: 75px"></span>
-                
-            `)
+                self.health_info.add({
+                    key: "BCDB Status",
+                    value: `<span class='webix_icon wxi-checkbox-marked' style="color:green">OK</span>`
+                })
             } else {
-                self.bcdb_info.setHTML(`
-                <span class='webix_icon wxi-close-circle' style="color:red;width: 100%;font-size: 75px"></span>
-            `)
+                self.health_info.add({
+                    key: "BCDB",
+                    value: `<span class='webix_icon wxi-close-circle' style="color:red">Error</span>`
+                })
             }
         });
 
