97d2bd6668fea08129ce1409762b8e6428941d24
alichaddad <chaddada@greenitglobe.com>
2020-03-16 13:44:02 +0200

    extend taiga package actor and add caching

--- ThreeBotPackages/tfgrid_solutions/taiga/actors/taiga.py
+++ ThreeBotPackages/tfgrid_solutions/taiga/actors/taiga.py
@@ -1,4 +1,6 @@
 import json
+import dateutil.parser
+from functools import lru_cache
 from Jumpscale import j
 
 
@@ -24,62 +26,40 @@ def _format_output(data, output_type):
 class taiga(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
         self.taiga_client = j.clients.taiga.get("circles_admin")
-        self._projects = {}
-        self._milestones = {}
-        self._priorities = {}
-        self._assignees = {}
-        self._issue_statuses = {}
-        self._story_statuses = {}
-        self._task_statuses = {}
-        self._user_stories = {}
 
+    @lru_cache(maxsize=128)
     def _get_project(self, project_id):
-        if project_id not in self._projects:
-            project = self.taiga_client.api.projects.get(project_id).name
-            self._projects[project_id] = project
-        return self._projects[project_id]
+        return self.taiga_client.api.projects.get(project_id).name
 
+    @lru_cache(maxsize=128)
     def _get_milestone(self, milestone_id):
         if milestone_id:
-            if milestone_id not in self._milestones:
-                milestone = self.taiga_client.api.milestones.get(milestone_id).name
-                self._milestones[milestone_id] = milestone
-            return self._milestones[milestone_id]
+            return self.taiga_client.api.milestones.get(milestone_id).name
         else:
             return "No milestone"
 
+    @lru_cache(maxsize=128)
     def _get_priority(self, priority_id):
-        if priority_id not in self._priorities:
-            priority = self.taiga_client.api.priorities.get(priority_id).name
-            self._priorities[priority_id] = priority
-        return self._priorities[priority_id]
+        return self.taiga_client.api.priorities.get(priority_id).name
 
+    @lru_cache(maxsize=128)
     def _get_assignee(self, assignee_id):
         if assignee_id:
-            if assignee_id not in self._assignees:
-                assignee = self.taiga_client.api.users.get(assignee_id).username
-                self._assignees[assignee_id] = assignee
-            return self._assignees[assignee_id]
+            return self.taiga_client.api.users.get(assignee_id).username
         else:
             return "No assignee"
 
+    @lru_cache(maxsize=128)
     def _get_issue_status(self, status_id):
-        if status_id not in self._issue_statuses:
-            status = self.taiga_client.api.issue_statuses.get(status_id).name
-            self._issue_statuses[status_id] = status
-        return self._issue_statuses[status_id]
+        return self.taiga_client.api.issue_statuses.get(status_id).name
 
+    @lru_cache(maxsize=128)
     def _get_user_stories_status(self, status_id):
-        if status_id not in self._story_statuses:
-            status = self.taiga_client.api.user_story_statuses.get(status_id).name
-            self._story_statuses[status_id] = status
-        return self._story_statuses[status_id]
+        return self.taiga_client.api.user_story_statuses.get(status_id).name
 
+    @lru_cache(maxsize=128)
     def _get_task_status(self, status_id):
-        if status_id not in self._task_statuses:
-            status = self.taiga_client.api.task_statuses.get(status_id).name
-            self._task_statuses[status_id] = status
-        return self._task_statuses[status_id]
+        return self.taiga_client.api.task_statuses.get(status_id).name
 
     @j.baseclasses.actor_method
     def get_user_circles(self, user_id, output_type="csv", user_session=None):
@@ -175,7 +155,6 @@ class taiga(j.baseclasses.threebot_actor):
         output_type = "csv" (S)
         ```
         """
-
         _validate_output_type(output_type)
         user_tasks = self.taiga_client.api.tasks.list(assigned_to=user_id)
         data = []
@@ -195,3 +174,70 @@ class taiga(j.baseclasses.threebot_actor):
             )
         return _format_output(data, output_type)
 
+    @j.baseclasses.actor_method
+    def move_story_to_cirlce(self, story_id, project_id, user_session=None):
+        """
+        ```in
+        story_id = (I)
+        project_id = (I)
+        ```
+        """
+
+        def _get_project_status(project_statuses, status):
+            for project_status in project_statuses:
+                if project_status.name == status:
+                    return project_status.id
+
+        try:
+            user_story = self.taiga_client.api.user_stories.get(story_id)
+        except Exception:
+            j.exceptions.BadRequest("Couldn't find user story with id: {}".format(story_id))
+
+        project_stories_statuses = self.taiga_client.api.user_story_statuses.list(project=project_id)
+        status = self._get_user_stories_status(user_story.status)
+        story_status_id = _get_project_status(project_stories_statuses, status)
+
+        try:
+            migrate_story = self.taiga_client.api.user_stories.create(
+                project=project_id,
+                subject=user_story.subject,
+                assigned_to=user_story.assigned_to,
+                milestone=user_story.milestone,
+                status=story_status_id,
+                tags=user_story.tags,
+            )
+        except Exception:
+            j.exceptions.BadRequest("No project with id: {} found".format(project_id))
+        try:
+            comments = self.taiga_client.api.history.user_story.get(story_id)
+            comments = sorted(comments, key=lambda c: dateutil.parser.isoparse(c["created_at"]))
+
+            for comment in comments:
+                migrate_story.add_comment(comment["comment_html"])
+
+            project_tasks_statuses = self.taiga_client.api.task_statuses.list(project=project_id)
+            for task in user_story.list_tasks():
+                status = self._get_task_status(task.status)
+                task_status_id = _get_project_status(project_tasks_statuses, status)
+                migrate_task = migrate_story.add_task(
+                    subject=task.subject,
+                    status=task_status_id,
+                    due_date=task.due_date,
+                    milestone=task.milestone,
+                    assigned_to=task.assigned_to,
+                    tags=task.tags,
+                    project=migrate_story.project,
+                    user_story=migrate_story.id,
+                )
+                comments = self.taiga_client.api.history.task.get(migrate_task.id)
+                comments = sorted(comments, key=lambda c: dateutil.parser.isoparse(c["created_at"]))
+
+                for comment in comments:
+                    migrate_task.add_comment(comment["comment_html"])
+
+        except Exception as e:
+            self.taiga_client.api.user_stories.delete(migrate_story.id)
+            j.exceptions.RuntimeError("Failed to migrate story error was: {}".format(str(e)))
+
+        self.taiga_client.api.user_stories.delete(story_id)
+        return migrate_story.id
