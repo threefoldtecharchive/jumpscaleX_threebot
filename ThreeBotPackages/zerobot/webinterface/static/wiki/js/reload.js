252b6beb73c52ed866a3fe7c94795916911b5198
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-03-24 20:19:38 +0200

    wikis: add pull button to wikis...
    also add more examples for include macro

--- ThreeBotPackages/zerobot/webinterface/static/wiki/js/reload.js
+++ ThreeBotPackages/zerobot/webinterface/static/wiki/js/reload.js
@@ -1,10 +1,20 @@
-function reloadWiki(name) {
+
+function reloadAction(name, action) {
     const spinner = $(`#spinner_${name.replace('.', '_')}`);
     spinner.show()
 
-    callActor("zerobot.webinterface", "wiki_content", "reload", {
+    callActor("zerobot.webinterface", "wiki_content", action, {
         wiki_name: name
     }, function () {
         spinner.hide();
     });
+}
+
+
+function reloadWiki(name) {
+    return reloadAction(name, "reload");
+}
+
+function pullWiki(name) {
+    return reloadAction(name, "pull")
 }
