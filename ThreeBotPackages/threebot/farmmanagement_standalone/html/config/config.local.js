abea8d0d5d88454dd2b7948108c1c8404664717f
Dean Verjans <developer.dean@outlook.com>
2020-03-24 20:12:05 +0100

    Converted farmmanagement to standalone version

---
+++ ThreeBotPackages/threebot/farmmanagement_standalone/html/config/config.local.js
@@ -0,0 +1,8 @@
+export default ({
+  threebotBaseUrl: `${window.location.origin}/`,
+  packagemanager: "zerobot/packagemanager/actors/package_manager/",
+  identityActor: "zerobot/webinterface/actors/identity",
+  phonebookActor: "https://explorer.testnet.grid.tf/tfgrid/      "
+  // botFrontEnd: 'https://login.threefold.me/',
+  // botBackend: 'https://login.threefold.me',
+})
---
+++ ThreeBotPackages/threebot/farmmanagement_standalone/html/views/farmmanagement/index.vue
@@ -0,0 +1,3 @@
+<template src="/threebot/farmmanagement_standalone/views/farmmanagement/farmmanagement.html"></template>
+<script src="/threebot/farmmanagement_standalone/views/farmmanagement/farmmanagement.js"></script>
+<style src="/threebot/farmmanagement_standalone/views/farmmanagement/farmmanagement.css" scoped lang="css"></style>
