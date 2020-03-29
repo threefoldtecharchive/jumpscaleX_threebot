a0620648ebd62e0c2d85f8b8fced1c426f56d069
alichaddad <chaddada@greenitglobe.com>
2020-03-29 17:17:46 +0200

    show user chatflow data before starting deployment

--- ThreeBotPackages/tfgrid_solutions/kubernetes_cluster/chatflows/kubernetes_cluster_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/kubernetes_cluster/chatflows/kubernetes_cluster_deploy.py
@@ -6,6 +6,7 @@ import ipaddress
 def chat(bot):
     """
     """
+    user_form_data = {}
     user_info = bot.user_info()
     name = user_info["username"]
     email = user_info["email"]
@@ -17,30 +18,37 @@ def chat(bot):
     if not email:
         raise j.exceptions.Value("Email shouldn't be empty")
 
-    ip_version = bot.single_choice(
+    user_form_data["IP version"] = bot.single_choice(
         "This wizard will help you deploy a kubernetes cluster, do you prefer to access your 3bot using IPv4 or IPv6? If unsure, choose IPv4",
         ips,
     )
 
-    workers_number = bot.int_ask("Please specify the number of worker nodes", default=1)  # minimum should be 1
-    cluster_size = workers_number + 1  # number of workers + the master node
-    ssh_keys = bot.upload_file(
+    user_form_data["Workers number"] = bot.int_ask(
+        "Please specify the number of worker nodes", default=1
+    )  # minimum should be 1
+    cluster_size = user_form_data["Workers number"] + 1  # number of workers + the master node
+    user_form_data["SSH keys"] = bot.upload_file(
         """"Please add your public ssh key, this will allow you to access the deployed container using ssh. 
             Just upload the ssh keys file with each key on a seperate line"""
-    ).split("\n")
+    )
+    ssh_keys_list = user_form_data["SSH keys"].split("\n")
 
-    cluster_secret = bot.string_ask("Please add the cluster secret", default="secret")
+    user_form_data["Cluster secret"] = bot.string_ask("Please add the cluster secret", default="secret")
+
+    bot.md_show_confirm(user_form_data)
 
     # create new reservation
     reservation = j.sal.zosv2.reservation_create()
     identity = explorer.users.get(name=name, email=email)
 
     # Select nodes
-    nodes_selected = j.sal.reservation_chatflow.nodes_get(workers_number + 1, farm_id=71, ip_version=ip_version)
+    nodes_selected = j.sal.reservation_chatflow.nodes_get(
+        cluster_size, farm_id=71, ip_version=user_form_data["IP version"]
+    )
 
     # Create network of reservation and add peers
     reservation, configs = j.sal.reservation_chatflow.network_configure(
-        bot, reservation, nodes_selected, customer_tid=identity.id, ip_version=ip_version
+        bot, reservation, nodes_selected, customer_tid=identity.id, ip_version=user_form_data["IP version"]
     )
     rid = configs["rid"]
 
@@ -50,10 +58,10 @@ def chat(bot):
         reservation=reservation,
         node_id=nodes_selected[0].node_id,
         network_name=configs["name"],
-        cluster_secret=cluster_secret,
+        cluster_secret=user_form_data["Cluster secret"],
         ip_address=configs["ip_addresses"][0],
         size=cluster_size,
-        ssh_keys=ssh_keys,
+        ssh_keys=ssh_keys_list,
     )
 
     # Workers are in the rest of the nodes
@@ -62,11 +70,11 @@ def chat(bot):
             reservation=reservation,
             node_id=nodes_selected[i].node_id,
             network_name=configs["name"],
-            cluster_secret=cluster_secret,
+            cluster_secret=user_form_data["Cluster secret"],
             ip_address=configs["ip_addresses"][i],
             size=cluster_size,
             master_ip=master.ipaddress,
-            ssh_keys=ssh_keys,
+            ssh_keys=ssh_keys_list,
         )
 
     # register the reservation
