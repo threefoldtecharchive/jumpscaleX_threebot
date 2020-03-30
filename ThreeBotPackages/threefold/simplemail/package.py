61d0ff1d2972cd876eba98e3c13e6fb8fd8ff67d
xmonader <xmonader@gmail.com>
2020-03-30 18:22:02 +0200

    fix blog, simplemail loading

--- ThreeBotPackages/threefold/simplemail/package.py
+++ ThreeBotPackages/threefold/simplemail/package.py
@@ -2,9 +2,6 @@ from Jumpscale import j
 
 
 class Package(j.baseclasses.threebot_package):
-    def install(self):
-        j.builders.runtimes.python3.pip_package_install("logbook")
-
     def start(self):
         # start local mail server
         server = j.servers.simplemail.get("default", address="0.0.0.0", port=25)
