1482893fcd17febf76553fc92c6ad3ab70fe4580
ahelsayd <ahmed.m.elsayed93@gmail.com>
2020-04-07 09:16:57 +0200

    make webgateway chatflow uses tf_solutions models

--- ThreeBotPackages/tfgrid_solutions/webgateway/chatflows/webgateway.py
+++ ThreeBotPackages/tfgrid_solutions/webgateway/chatflows/webgateway.py
@@ -8,41 +8,44 @@ def chat(bot):
         bot.md_show("You need to init your threebot to be able to use this chatflow")
         return
 
-    containers_data = {}
     explorer = j.clients.explorer.default
-    reservations = explorer.reservations.list(customer_tid=j.tools.threebot.me.default.tid, next_action="DEPLOY")
+    bcdb = j.data.bcdb.get('tfgrid_solutions')
 
-    for reservation in reservations:
-        containers = reservation.data_reservation.containers
-        for container in containers:
-            container_id = f"{reservation.id}-{container.workload_id}"
+    solutions = []
+    containers = {}
+
+    for model_name in bcdb.models:
+        if not model_name.startswith("tfgrid.solutions"):
+            continue
+        model = bcdb.models[model_name]
+        solutions.extend(model.find())
+
+    for solution in solutions:
+        reservation = explorer.reservations.get(solution.rid)
+        for container in reservation.data_reservation.containers:
             network = container.network_connection[0]
+            workload_id = f"{reservation.id}-{container.workload_id}"
+
             if not network.public_ip6:
                 continue
 
             for result in reservation.results:
-                if result.workload_id == container_id and result.state.value == 1:
+                if result.workload_id == workload_id and result.state.value == 1:
                     break
             else:
                 continue
             
-            name = "Container"
-            if "ubuntu" in container.flist:
-                name = "Ubuntu container"
-            elif "minio" in container.flist:
-                name = "Minio container"
+            containers[solution.name] = result.data_json["ipv6"]
 
-            containers_data[container_id] = {
-                "name": f"{name} {container_id}",
-                "ipv6": result.data_json["ipv6"]
-            }
-
-    selected = bot.drop_down_choice('Select container', [c["name"] for c in containers_data.values()])
-    container = containers_data[selected.split()[-1]]
+    if not containers:
+        bot.md_show("You don't have any ipv6 containers")
+        return
+    
+    selected = bot.drop_down_choice('Select container', list(containers.keys()))
 
     name = bot.string_ask("Name")
     port = bot.int_ask("Port")
-    ip_address = container['ipv6']
+    ip_address = containers[selected]
     threebot_name = j.tools.threebot.me.default.tname
     signature = j.tools.threebot.me.default.nacl.sign_hex(threebot_name)
     domain = f"{name}.{threebot_name}.testnet.grid.tf"
