1bde616b9abbaa44a2f9c23537c20b1ef7a92922
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-04-11 12:46:01 +0200

    farm_management fix switch node to free to use

--- ThreeBotPackages/threebot/farmmanagement/actors/farm_management.py
+++ ThreeBotPackages/threebot/farmmanagement/actors/farm_management.py
@@ -3,15 +3,12 @@ from Jumpscale import j
 
 class farm_management(j.baseclasses.threebot_actor):
     def _init(self, **kwargs):
-        # TODO: need to be able to choose which client
         self._explorer = j.clients.explorer.default
-        # TODO: need to be able to choose which identity
-        self._me = j.tools.threebot.me.default
         self._farm_model = j.data.schema.get_from_url("tfgrid.directory.farm.1")
 
     def update_farm(self, farm_id, farm):
         farm = self._farm_model.new(datadict=farm)
-        self._explorer.farms.update(farm, self._me)
+        self._explorer.farms.update(farm)
 
     def mark_node_free(self, node_id, free):
-        return self.nodes.configure_free_to_use(node_id, free, me=self._me)
+        return self._explorer.nodes.configure_free_to_use(node_id=node_id, free=free)
