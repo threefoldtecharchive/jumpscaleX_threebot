b79755ffa24db09b1ec196b19055d191aba4e1af
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-04-14 16:43:54 +0200

    allow multiple domains in threefold wikis package

--- ThreeBotPackages/threefold/wikis/package.py
+++ ThreeBotPackages/threefold/wikis/package.py
@@ -3,7 +3,7 @@ import re
 from Jumpscale import j
 
 
-WIKIS = {"info_threefold": "wiki.threefold.io", "info_tfgridsdk": "wiki.grid.tf"}
+WIKIS = {"info_threefold": ["wiki.threefold.io"], "info_tfgridsdk": ["wiki.grid.tf"]}
 
 BRANCH = "development"
 HOST = "github.com"
@@ -31,39 +31,43 @@ class Package(j.baseclasses.threebot_package):
             j.clients.git.getContentPathFromURLorPath(get_repo_url(name), branch=BRANCH, pull=True)
 
     def start(self):
-        for name, domain in WIKIS.items():
+        for name, domains in WIKIS.items():
             url = get_repo_url(name)
             path = j.clients.git.getContentPathFromURLorPath(url)
             build_mdbook(path)
 
-            for port in (80, 443):
-                website = self.openresty.websites.get(f"{name}_wiki_{port}")
-                website.port = port
-                website.ssl = port == 443
-                website.domain = domain
-                locations = website.locations.get(name=f"{name}_wiki_locations_{port}")
+            for domain in domains:
+                for port in (80, 443):
+                    domain_without_dots = domain.replace(".", "_")
+                    website_name = f"{name}_{domain_without_dots}_{port}"
+                    website = self.openresty.websites.get(website_name)
+                    website.port = port
+                    website.ssl = port == 443
+                    website.domain = domain
 
-                static_location = locations.get_location_static(f"{name}_book_location_{port}")
-                static_location.path_url = "/"
-                static_location.path_location = j.sal.fs.joinPaths(path, "book")
+                    locations = website.locations.get(name=f"{website_name}_locations")
 
-                default_website = self.openresty.get_from_port(port)
-                include_location = locations.get_location_custom(f"{name}_wiki_includes_{port}")
-                include_location.config = (
-                    f"include {default_website.path_cfg_dir}/{default_website.name}_locations/*.conf;"
-                )
+                    static_location = locations.get_location_static(f"{website_name}_root")
+                    static_location.path_url = "/"
+                    static_location.path_location = j.sal.fs.joinPaths(path, "book")
 
-                locations.configure()
-                website.configure()
+                    default_website = self.openresty.get_from_port(port)
+                    include_location = locations.get_location_custom(f"{website_name}_include")
+                    include_location.config = (
+                        f"include {default_website.path_cfg_dir}/{default_website.name}_locations/*.conf;"
+                    )
 
-                default_locations = default_website.locations.get(f"default_{name}_wiki_locations_{port}")
+                    locations.configure()
+                    website.configure()
 
-                default_static_location = default_locations.get_location_static(f"{name}_book_location_{port}")
-                default_static_location.path_url = f"/threefold/{name}"
-                default_static_location.path_location = j.sal.fs.joinPaths(path, "book")
+                    default_locations = default_website.locations.get(f"{website_name}_locations_default")
 
-                default_locations.configure()
-                default_website.configure()
+                    default_static_location = default_locations.get_location_static(f"{static_location.name}_default")
+                    default_static_location.path_url = f"/threefold/{name}"
+                    default_static_location.path_location = j.sal.fs.joinPaths(path, "book")
+
+                    default_locations.configure()
+                    default_website.configure()
 
             # add a handler for github webhooks for every wiki
             # https://github.com/threefoldtech/jumpscaleX_threebot/tree/unstable/ThreeBotPackages/zerobot/webhooks/wiki
