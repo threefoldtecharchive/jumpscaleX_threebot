2460365c9b443c0c3e7f4ec14f785794c907b4a0
Jo De Boeck <deboeck.jo@gmail.com>
2020-04-14 16:11:23 +0200

    Chatflows network: Reuse reservation wait to check if reservation expired
    reservation_wait already checks expiration prevents busy loops no need
    to reinvent that in this flow
    
    See: #583
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/network_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/network_deploy.py
@@ -1,4 +1,5 @@
 from Jumpscale import j
+from Jumpscale.servers.gedis.GedisChatBot import StopChatFlow
 import time
 
 
@@ -7,7 +8,6 @@ def chat(bot):
     """
     user_form_data = {}
     user_info = bot.user_info()
-    model = j.threebot.packages.tfgrid_solutions.tfgrid_solutions.bcdb_model_get("tfgrid.solutions.network.1")
 
     identity = j.sal.reservation_chatflow.validate_user(user_info)
     network_name = bot.string_ask("Please enter a network name")
@@ -22,34 +22,25 @@ def chat(bot):
     # create new reservation
     reservation = j.sal.zosv2.reservation_create()
     ip_range = j.sal.reservation_chatflow.ip_range_get(bot)
-    config = j.sal.reservation_chatflow.network_create(
-        network_name, reservation, ip_range, identity.id, ipversion, expiration=expiration
-    )
 
     # Check if reservation failed
-    network_created = False
-    while not network_created:
-        resv_id = config["rid"]
-        explorer = j.clients.explorer.default
-        reservation_results = explorer.reservations.get(resv_id).results
-        port_error = False
-        for result in reservation_results:
-            if result.state == "ERROR" and "wireguard listen port already in use" in result.message:
-                port_error = True
-                break
-        if port_error:
-            # Cancel failed reservation
-            j.sal.zosv2.reservation_cancel(resv_id)
-            reservation = j.sal.zosv2.reservation_create()
-            config = j.sal.reservation_chatflow.network_create(
-                network_name, reservation, ip_range, identity.id, ipversion, expiration=expiration
-            )
-            time.sleep(1)
+    while True:
+        config = j.sal.reservation_chatflow.network_create(
+            network_name, reservation, ip_range, identity.id, ipversion, expiration=expiration
+        )
+        try:
+            j.sal.reservation_chatflow.reservation_wait(bot, config["rid"])
+            break
+        except StopChatFlow as e:
+            if "wireguard listen port already in use" in e.msg:
+                j.sal.zosv2.reservation_cancel(config["rid"])
+                time.sleep(5)
+                continue
+            raise
 
-        else:
-            network_created = len(reservation_results) == 1
-
-    j.sal.reservation_chatflow.reservation_save(resv_id, network_name, "tfgrid.solutions.network.1", user_form_data)
+    j.sal.reservation_chatflow.reservation_save(
+        config["rid"], network_name, "tfgrid.solutions.network.1", user_form_data
+    )
 
     # inform user howto connect
     res = """
@@ -58,12 +49,10 @@ def chat(bot):
             Click next
             to download your configuration
             """
-    res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
-    bot.md_show(res)
+    bot.md_show(j.core.text.strip(res))
 
-    res = j.tools.jinja2.template_render(text=config["wg"], **locals())
     filename = "{}_{}.conf".format(identity.name.split(".3bot")[0], config["rid"])
-    bot.download_file(res, filename)
+    bot.download_file(config["wg"], filename)
 
     res = """
             # In order to have the network active and accessible from your local machine. To do this, execute this command:
@@ -73,5 +62,4 @@ def chat(bot):
         filename
     )
 
-    res = j.tools.jinja2.template_render(text=j.core.text.strip(res), **locals())
-    bot.md_show(res)
+    bot.md_show(j.core.text.strip(res))
