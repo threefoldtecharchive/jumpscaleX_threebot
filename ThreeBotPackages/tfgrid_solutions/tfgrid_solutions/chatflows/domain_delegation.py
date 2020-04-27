2fc7fbf82bd02926f5fd2dfc51cc6633dd2fa6d6
m-motawea <magedmotawea@gmail.com>
2020-04-27 17:23:25 +0200

    fixes for gateway chatflows

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/domain_delegation.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/domain_delegation.py
@@ -3,16 +3,24 @@ from Jumpscale import j
 def chat(bot):
     """
     """
-    gateways = {g.node_id: g for g in j.sal.zosv2._explorer.gateway.list()}
+    gateways = {}
 
-    # if not gateways stop
     domain = bot.string_ask("Please enter a domain name")
-    gateway_id = bot.single_choice(
-        "Please choose a gateway", list(gateways.keys())
+    gw_ask_list = []
+    for g in j.sal.zosv2._explorer.gateway.list():
+        gateways[g.node_id] = g
+        city = g.location.city if g.location.city else "Unknown"
+        country = g.location.country if g.location.country else "Unknown"
+        continent = g.location.continent if g.location.continent else "Unkown"
+        gw_ask_list.append(f"id: {g.node_id} Continent: {continent} City: {city} Country: {country}")
+
+    gateway = bot.single_choice(
+        "Please choose a gateway", list(gw_ask_list)
     )
+    gateway_id = gateway.split()[1]
     reservation = j.sal.zosv2.reservation_create()
     j.sal.zosv2.gateway.delegate_domain(reservation=reservation, node_id=gateway_id, domain=domain)
     res = f"""
-    # Use These name servers as your dns to connect to your domain: {gateways[gateway_id].dns_nameserver}
+    # Please create an NS Recored in your dns manager for domain: {domain} nameserver: {gateways[gateway_id].dns_nameserver}
     """
     bot.md_show(res)
