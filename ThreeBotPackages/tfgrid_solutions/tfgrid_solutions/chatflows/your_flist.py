33f59a1934e72ac4ea472bb43228c9351a94522f
BolaNasr <bola.nasr@live.com>
2020-04-28 16:48:04 +0200

    move all behaviour of payment to sal

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/your_flist.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/your_flist.py
@@ -120,17 +120,7 @@ def chat(bot):
         reservation, expiration, customer_tid=identity.id, currency=currency, bot=bot
     )
     resv_id = reservation_create.reservation_id
-    payment = j.sal.reservation_chatflow.payments_show(bot, reservation_create)
-    if payment["free"]:
-        pass
-    elif payment["wallet"]:
-        j.sal.zosv2.billing.payout_farmers(payment["wallet"], reservation_create)
-        j.sal.reservation_chatflow.payment_wait(bot, resv_id, threebot_app=False)
-    else:
-        j.sal.reservation_chatflow.payment_wait(
-            bot, resv_id, threebot_app=True, reservation_create_resp=reservation_create
-        )
-
+    j.sal.reservation_chatflow.reservation_pay(bot, reservation_create)
     j.sal.reservation_chatflow.reservation_wait(bot, resv_id)
     j.sal.reservation_chatflow.reservation_save(
         resv_id, user_form_data["Solution name"], "tfgrid.solutions.flist.1", user_form_data
