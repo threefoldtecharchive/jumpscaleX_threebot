8e3c6e6d8887ffd1ad421bc0a1d6513588ec4790
m-motawea <magedmotawea@gmail.com>
2020-04-29 10:16:43 +0200

    use gateway choose from reservation chatflow

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/domain_delegation.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/domain_delegation.py
@@ -7,27 +7,11 @@ def chat(bot):
     user_form_data = {}
     user_info = bot.user_info()
     identity = j.sal.reservation_chatflow.validate_user(user_info)
-    gateways = {}
-    url = "tfgrid.domains.delegate.1"
-    model = j.clients.bcdbmodel.get(url=url, name="tfgrid_solutions")
     domain = bot.string_ask("Please enter a domain name")
     user_form_data["domain"] = domain
-    gw_ask_list = []
-    for g in j.sal.zosv2._explorer.gateway.list():
-        gateways[g.node_id] = g
-        city = g.location.city if g.location.city else "Unknown"
-        country = g.location.country if g.location.country else "Unknown"
-        continent = g.location.continent if g.location.continent else "Unkown"
-        if g.free_to_use:
-            currency = "FreeTFT"
-        else:
-            currency = "TFT"
-        gtext = f"Continent: ({continent}) Country: ({country}) City: ({city}) Currency: ({currency}) ID: ({g.node_id})"
-        gw_ask_list.append(gtext)
 
-    gateway = bot.single_choice("Please choose a gateway", list(gw_ask_list))
-    gateway_id = gateway.split()[-1][1:-1]
-    gateway = gateways[gateway_id]
+    gateway = j.sal.reservation_chatflow.gateway_select(bot)
+    gateway_id = gateway.node_id
     user_form_data["gateway"] = gateway_id
     expirationdelta = int(bot.time_delta_ask("Please enter solution expiration time.", default="1d"))
     expiration = j.data.time.epoch + expirationdelta
@@ -37,10 +21,6 @@ def chat(bot):
         currency = "FreeTFT"
     else:
         currency = "TFT"
-
-    new_domain = model.new()
-    new_domain.gateway = gateways[gateway_id]
-    new_domain.domain = domain
 
     reservation = j.sal.zosv2.reservation_create()
     j.sal.zosv2.gateway.delegate_domain(reservation=reservation, node_id=gateway_id, domain=domain)
@@ -52,7 +32,6 @@ def chat(bot):
     j.sal.reservation_chatflow.reservation_save(
         resv_id, f"tcprouter:{resv_id}", "tfgrid.solutions.flist.1", user_form_data
     )
-    new_domain.save()
     res = """\
     # Delegated your domain successfully
 
@@ -63,5 +42,5 @@ def chat(bot):
     - {{dns}}
     {% endfor %}
     """
-    res = j.tools.jinja2.template_render(text=res, gateway=gateways[gateway_id], domain=domain, reservation=reservation)
+    res = j.tools.jinja2.template_render(text=res, gateway=gateway, domain=domain, reservation=reservation)
     bot.md_show(j.core.text.strip(res))
