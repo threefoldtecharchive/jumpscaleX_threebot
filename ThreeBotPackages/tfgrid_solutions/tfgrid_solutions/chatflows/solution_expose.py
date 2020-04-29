70e1e776fdd0bfdddc0281af67a9ed8ccbc7c2f0
m-motawea <magedmotawea@gmail.com>
2020-04-29 11:17:54 +0200

    update chatflow for solutions expose

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/solution_expose.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/solution_expose.py
@@ -1,4 +1,5 @@
 from Jumpscale import j
+import uuid
 
 kinds = {
     "minio": "tfgrid.solutions.minio.1",
@@ -31,36 +32,65 @@ def chat(bot):
     solution = sols[solution_name]
     user_form_data["solution_name"] = solution_name
 
+    port = ports.get(kind)
+    if not port:
+        port = bot.int_ask("Which port you want to expose")
+        user_form_data["port"] = port
+
+    # List all available domains
+    gateways = {g.node_id: g for g in j.sal.zosv2._explorer.gateway.list()}
+    user_domains = j.sal.reservation_chatflow.delegate_domains_list(identity.id)
+    domain_ask_list = []
+    for dom in user_domains:
+        domain_ask_list.append(f"Delegated Domain: {dom}")
+
+    managed_domains = dict()
+    for g in gateways.values():
+        for dom in g.managed_domains:
+            managed_domains[dom] = g
+            domain_ask_list.append(f"Managed Domain: {dom}")
+    domain_ask_list.append("Crystal Domain")
+
+    choosed_domain = bot.single_choice(
+        "Please choose the domain you wish to use", domain_ask_list
+    )
+    temp = choosed_domain.split()
+    domain_type = temp[0].strip()
+    if domain_type == "Crystal":
+        domain = bot.string_ask(
+            f''' Warning: you will have to create an A record to point to the gateway you'll choose.
+            Please specify the domain name you wish to bind to:'''
+        )
+        domain_gateway = j.sal.reservation_chatflow.gateway_select(bot)
+
+    else:
+        domain_name = temp[-1].strip()
+        if domain_type == "Managed":
+            domain_obj = managed_domains[domain_name]
+            domain_gateway = gateways[domain_obj.node_id]
+        elif domain_type == "Delegated":
+            domain_obj = user_domains[domain_name]
+            domain_gateway = gateways[domain_obj.node_id]
+        domain = bot.string_ask(
+            f"Please specify the sub domain name you wish to bind to will be (subdomain).{domain_name}"
+        )
+        if "." in domain:
+            # TODO Implement exception message
+            raise Exception("Invalid")
+        domain = domain + "." + domain_name
+
+    # use gateway currency
+    currency = "TFT"
+    if domain_gateway.free_to_use:
+        currency = "FreeTFT"
+
     expirationdelta = int(bot.time_delta_ask("Please enter solution expiration time.", default="1d"))
     expiration = j.data.time.epoch + expirationdelta
     user_form_data["expiration"] = expiration
+
+    # create tcprouter
     reservation_data = j.data.serializers.json.loads(solution["reservation"])["data_reservation"]
-
-    gateways = {g.node_id: g for g in j.sal.zosv2._explorer.gateway.list()}
-
-    domain_type = bot.single_choice("Which type of domain you wish to bind to", ["sub", "delegate"])
-    if domain_type == "sub":
-        base_domain = f"{j.me.tname.replace('3bot', '')}{j.core.myenv.config.get('THREEBOT_DOMAIN')}"
-        domain = bot.string_ask(
-            f"Please specify the sub domain name you wish to bind to will be (subdomain).{base_domain}"
-        )
-        domain = domain + "." + base_domain
-        gateway_id = bot.single_choice("Please choose a gateway", list(gateways.keys()))
-        gateway = gateways[gateway_id]
-    elif domain_type == "delegate":
-        url = "tfgrid.domains.delegate.1"
-        model = j.clients.bcdbmodel.get(url=url, name="tfgrid_solutions")
-        domains = model.find()
-        domains_dict = {d.domain: d for d in domains}
-        domain = bot.single_choice("Please choose the domain you wish to bind", list(domains_dict.keys()))
-        domain_obj = domains_dict[domain]
-        gateway = domain_obj.gateway
-        gateway_id = gateway.node_id
-
-    # create a container with tcprouter flist
-    currency = reservation_data["currencies"][0]
-    query = {"mru": 1, "cru": 1, "currency": currency, "free_to_use": gateway.free_to_use}
-
+    query = {"mru": 1, "cru": 1, "currency": currency, "free_to_use": domain_gateway.free_to_use}
     node_selected = j.sal.reservation_chatflow.nodes_get(1, **query)[0]
     network_name = reservation_data["containers"][0]["network_connection"][0]["network_id"]
     container_address = reservation_data["containers"][0]["network_connection"][0]["ipaddress"]
@@ -68,22 +98,19 @@ def chat(bot):
     network = j.sal.reservation_chatflow.network_get(bot, identity.id, network_name)
     network.add_node(node_selected)
     network.update(identity.id, currency=currency)
-    ip_address = network.ask_ip_from_node(node_selected, "Please choose IP Address for your solution")
-    user_form_data["ip"] = ip_address
-    if domain_type == "sub":
-        j.sal.zosv2.gateway.sub_domain(reservation, gateway_id, domain, [ip_address])
+    ip_address = network.get_free_ip(node_selected)
+    if not ip_address:
+        # TODO Implement exception message
+        raise Exception("No available free ips")
 
-    secret_env = {}
-    port = ports.get(kind)
-    if not port:
-        port = bot.int_ask("Which port you want to expose")
-        user_form_data["port"] = port
-    secret = bot.string_ask("Please specify a secret for the connection")
+
+    secret = f"{identity.id}:{uuid.uuid4().hex}"
     user_form_data["secret"] = secret
+    secret_env = {}
     secret_encrypted = j.sal.zosv2.container.encrypt_secret(node_selected.node_id, user_form_data["secret"])
     secret_env["TRC_SECRET"] = secret_encrypted
     entrypoint = (
-        f"/bin/trc -local {container_address}:{port} -remote {gateway.dns_nameserver[0]}:{gateway.tcp_router_port}"
+        f"/bin/trc -local {container_address}:{port} -remote {domain_gateway.dns_nameserver[0]}:{domain_gateway.tcp_router_port}"
     )
 
     j.sal.zosv2.container.create(
@@ -95,8 +122,9 @@ def chat(bot):
         entrypoint=entrypoint,
         secret_env=secret_env,
     )
+
     # create proxy
-    j.sal.zosv2.gateway.tcp_proxy_reverse(reservation, gateway_id, domain, user_form_data["secret"])
+    j.sal.zosv2.gateway.tcp_proxy_reverse(reservation, domain_gateway.node_id, domain, user_form_data["secret"])
     resv_id = j.sal.reservation_chatflow.reservation_register_and_pay(
         reservation, expiration, customer_tid=identity.id, currency=currency, bot=bot
     )
