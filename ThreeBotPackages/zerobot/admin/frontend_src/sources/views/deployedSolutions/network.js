b70125d22e7eba9394ce7bf65342da5bde6b4d45
Mahmoud Ayoub <mahmoud.a@incubaid.com>
2020-05-04 16:42:13 +0200

    Implemented new view -changed UI of deployed solutions -created solutionsDetailsView -parsing data in networkview

--- ThreeBotPackages/zerobot/admin/frontend_src/sources/views/deployedSolutions/network.js
+++ ThreeBotPackages/zerobot/admin/frontend_src/sources/views/deployedSolutions/network.js
@@ -1,28 +1,9 @@
 import { JetView } from "webix-jet";
+import { solutions } from '../../services/deployedSolutions'
+import SolutionDetailsView from './SolutionDetails'
 
 export default class DeployedNetworkView extends JetView {
     config() {
-
-        let data = [
-            {
-                id: "1", type: "folder", value: "Network1", data: [
-                    { id: "p_0_1", type: "file", value: "ID" },
-                    { id: "p_0_0", type: "file", value: "name" }
-                ]
-            },
-            {
-                id: "2", type: "folder", value: "Network2", data: [
-                    { id: "p_1_1", type: "file", value: "ID" },
-                    { id: "p_1_0", type: "file", value: "name" }
-                ]
-            },
-            {
-                id: "3", type: "folder", value: "Network3", data: [
-                    { id: "p_2_1", type: "file", value: "ID" },
-                    { id: "p_2_0", type: "file", value: "name" }
-                ]
-            }
-        ];
 
         const logo = {
             view: "template",
@@ -34,25 +15,65 @@ export default class DeployedNetworkView extends JetView {
         }
 
         const view = {
-            view: "grouplist",
-            data: data,
-            css: 'solutions-list',
-            scroll: 'auto',
-            width: 700,
-            borderless: true
-        };
+            view: "dataview",
+            id: "networklist",
+            width: 1000,
+            height: 600,
+            scroll:false,
+            select: 1,
+            css: "solutions-list",
+            type: {
+                width: 480,
+                height: 120,
+                template: "<div class='overall'><div class='title'>#name#</div><div class='ip'>#iprange# </div> </div>"
+            }
+        }
 
         return {
             type: "space",
             rows:
-            [
-                logo,
-                {cols:[{}, view, {}]}
-            ]
+                [
+                    logo,
+                    { cols: [{}, view, {}] }
+                ]
         };
     }
 
     init(view) {
+        let self = this
+        self.networkList = $$("networklist")
+        self.SolutionDetailsView = self.ui(SolutionDetailsView)
 
+
+        solutions.listSolution('network').then((data) => {
+            const solutions = data.json().solutions
+            self.parseData = []
+            for (let i = 0; i < solutions.length; i++) {
+                const solution = solutions[i];
+                let dict = {}
+                let reservation = JSON.parse(String(solution.reservation))
+                let nodes = reservation.data_reservation.networks[0].network_resources
+                dict.id = reservation.id
+                dict.name = solution.name
+                dict.expiration_provisioning = reservation.data_reservation.expiration_provisioning
+                dict.expiration_reservation = reservation.data_reservation.expiration_reservation
+                dict.currencies = reservation.data_reservation.currencies.join(' ')
+                dict.expiration = JSON.parse(solution.form_info)['Solution expiration']
+                dict.iprange = reservation.data_reservation.networks[0].iprange
+                dict.nodes = []
+                for (let i = 0; i < nodes.length; i++) {
+                    const node = nodes[i];
+                    dict.nodes.push({'node_id':node.node_id,'iprange':node.iprange})
+                }
+                self.parseData.push(dict)
+            }
+            self.networkList.parse(self.parseData)
+
+        });
+        
+        self.networkList.attachEvent("onItemDblClick", function (id) {
+            let ret = self.parseData.find(network => network.id == id)
+            self.SolutionDetailsView.showInfo(ret)
+        });
     }
 }
