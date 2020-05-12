c739457154a697c449903e623cb7c28c7f8646ee
ahelsayd <ahmed.m.elsayed93@gmail.com>
2020-05-12 15:58:37 +0200

    support auto complete for multiple choices in chatflows

--- ThreeBotPackages/zerobot/webinterface/chatflows/components/MultipleListChoices.vue
+++ ThreeBotPackages/zerobot/webinterface/chatflows/components/MultipleListChoices.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Message :payload="payload"></Message>
-    <v-select v-model="val" :items="payload.options" :rules="rules" validate-on-blur small-chips multiple outlined/>
+    <v-autocomplete v-if="payload.kwargs.auto_complete" v-model="val" :items="payload.options" :rules="rules" validate-on-blur outlined multiple></v-autocomplete>
+    <v-select v-else v-model="val" :items="payload.options" :rules="rules" validate-on-blur small-chips multiple outlined/>
   </div>
 </template>
 
