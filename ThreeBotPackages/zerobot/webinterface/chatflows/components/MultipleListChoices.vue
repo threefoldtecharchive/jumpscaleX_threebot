6a2751f34ad505d7bf28611265603f45fe52c9b9
ahelsayd <ahmed.m.elsayed93@gmail.com>
2020-05-12 16:35:21 +0200

    imporve farm selection in chatflows

--- ThreeBotPackages/zerobot/webinterface/chatflows/components/MultipleListChoices.vue
+++ ThreeBotPackages/zerobot/webinterface/chatflows/components/MultipleListChoices.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Message :payload="payload"></Message>
-    <v-autocomplete v-if="payload.kwargs.auto_complete" v-model="val" :items="payload.options" :rules="rules" validate-on-blur outlined multiple></v-autocomplete>
+    <v-autocomplete v-if="payload.kwargs.auto_complete" v-model="val" :items="payload.options" :rules="rules" validate-on-blur outlined multiple small-chips></v-autocomplete>
     <v-select v-else v-model="val" :items="payload.options" :rules="rules" validate-on-blur small-chips multiple outlined/>
   </div>
 </template>
