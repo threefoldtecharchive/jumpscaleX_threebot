eb2fa8dbe82910d396db15fc666380f063084636
BolaNasr <bola.nasr@live.com>
2020-05-13 15:25:30 +0200

    split chatflows to steps

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/your_flist.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/your_flist.py
@@ -4,134 +4,180 @@ import requests
 from Jumpscale.servers.gedis.GedisChatBot import StopChatFlow
 
 
-def chat(bot):
-    """
-    """
+class FlistDeploy(j.servers.chatflow.get_class()):
+    steps = [
+        "flist_start",
+        "flist_network",
+        "flist_solution_name",
+        "flist_url",
+        "container_resources",
+        "container_interactive",
+        "container_env",
+        "expiration_time",
+        "container_fram",
+        "container_ip",
+        "container_pay",
+        "container_acess",
+    ]
     user_form_data = {}
-    user_info = bot.user_info()
-    user_form_data["chatflow"] = "flist"
-    env = dict()
     model = j.threebot.packages.tfgrid_solutions.tfgrid_solutions.bcdb_model_get("tfgrid.solutions.flist.1")
-    j.sal.reservation_chatflow.validate_user(user_info)
-    bot.md_show("# This wizard will help you deploy a container using any flist provided", md=True)
-    network = j.sal.reservation_chatflow.network_select(bot, j.me.tid)
+    env = dict()
 
-    currency = network.currency
+    @j.baseclasses.chatflow_step()
+    def flist_start(self):
+        user_info = self.user_info()
+        self.user_form_data["chatflow"] = "flist"
+        j.sal.reservation_chatflow.validate_user(user_info)
+        self.md_show("# This wizard will help you deploy a container using any flist provided", md=True)
 
-    user_form_data["Solution name"] = j.sal.reservation_chatflow.solution_name_add(bot, model)
-    user_form_data["Flist link"] = bot.string_ask(
-        "Please add the link to your flist to be deployed. For example: https://hub.grid.tf/usr/example.flist",
-        required=True,
-    )
+    @j.baseclasses.chatflow_step(title="Network")
+    def flist_network(self):
+        self.network = j.sal.reservation_chatflow.network_select(self, j.me.tid)
+        self.currency = self.network.currency
 
-    if "hub.grid.tf" not in user_form_data["Flist link"]:
-        raise StopChatFlow("This flist is not correct. Please make sure you enter a valid link to an existing flist")
+    @j.baseclasses.chatflow_step(title="Solution name")
+    def flist_solution_name(self):
+        self.user_form_data["Solution name"] = j.sal.reservation_chatflow.solution_name_add(self, self.model)
 
-    response = requests.head(user_form_data["Flist link"])
-    if response.status_code != 200:
-        raise StopChatFlow("This flist doesn't exist. Please make sure you enter a valid link to an existing flist")
+    @j.baseclasses.chatflow_step(title="Flist url")
+    def flist_url(self):
+        self.user_form_data["Flist link"] = self.string_ask(
+            "Please add the link to your flist to be deployed. For example: https://hub.grid.tf/usr/example.flist",
+            required=True,
+        )
 
-    form = bot.new_form()
-    cpu = form.int_ask("Please add how many CPU cores are needed", default=1)
-    memory = form.int_ask("Please add the amount of memory in MB", default=1024)
-    form.ask()
-    user_form_data["CPU"] = cpu.value
-    user_form_data["Memory"] = memory.value
+        if "hub.grid.tf" not in self.user_form_data["Flist link"]:
+            raise StopChatFlow(
+                "This flist is not correct. Please make sure you enter a valid link to an existing flist"
+            )
 
-    user_form_data["Interactive"] = bot.single_choice(
-        "Would you like access to your container through the web browser (coreX)?", ["YES", "NO"], required=True
-    )
-    if user_form_data["Interactive"] == "NO":
-        user_form_data["Entry point"] = bot.string_ask("Please add your entrypoint for your flist")
-    else:
-        user_form_data["Port"] = "7681"
-        user_form_data["Entry point"] = ""
-    user_form_data["Env variables"] = bot.string_ask(
-        """To set environment variables on your deployed container, enter comma-separated variable=value
-        For example: var1=value1, var2=value2.
-        Leave empty if not needed"""
-    )
+        response = requests.head(self.user_form_data["Flist link"])
+        if response.status_code != 200:
+            raise StopChatFlow("This flist doesn't exist. Please make sure you enter a valid link to an existing flist")
 
-    expiration = bot.datetime_picker(
-        "Please enter solution expiration time.",
-        required=True,
-        min_time=[3600, "Date/time should be at least 1 hour from now"],
-        default=j.data.time.epoch + 3900,
-    )
-    user_form_data["Solution expiration"] = j.data.time.secondsToHRDelta(expiration - j.data.time.epoch)
+    @j.baseclasses.chatflow_step(title="Container resources")
+    def container_resources(self):
+        form = self.new_form()
+        self.cpu = form.int_ask("Please add how many CPU cores are needed", default=1)
+        self.memory = form.int_ask("Please add the amount of memory in MB", default=1024)
+        form.ask()
+        self.user_form_data["CPU"] = self.cpu.value
+        self.user_form_data["Memory"] = self.memory.value
 
-    if user_form_data["Env variables"]:
-        var_list = user_form_data["Env variables"].split(",")
-        var_dict = {}
-        for item in var_list:
-            splitted_item = item.split("=")
-            if len(splitted_item) == 2:
-                var_dict[splitted_item[0]] = splitted_item[1]
+    @j.baseclasses.chatflow_step(title="Container ineractive & EntryPoint")
+    def container_interactive(self):
+        self.user_form_data["Interactive"] = self.single_choice(
+            "Would you like access to your container through the web browser (coreX)?", ["YES", "NO"], required=True
+        )
+        if self.user_form_data["Interactive"] == "NO":
+            self.user_form_data["Entry point"] = self.string_ask("Please add your entrypoint for your flist")
+        else:
+            self.user_form_data["Port"] = "7681"
+            self.user_form_data["Entry point"] = ""
 
-        env.update(var_dict)
+    @j.baseclasses.chatflow_step(title="Environment variables")
+    def container_env(self):
+        self.user_form_data["Env variables"] = self.string_ask(
+            """To set environment variables on your deployed container, enter comma-separated variable=value
+            For example: var1=value1, var2=value2.
+            Leave empty if not needed"""
+        )
+        if self.user_form_data["Env variables"]:
+            var_list = self.user_form_data["Env variables"].split(",")
+            var_dict = {}
+            for item in var_list:
+                splitted_item = item.split("=")
+                if len(splitted_item) == 2:
+                    var_dict[splitted_item[0]] = splitted_item[1]
 
-    # create new reservation
-    reservation = j.sal.zosv2.reservation_create()
-    hru = math.ceil(memory.value / 1024)
-    cru = cpu.value
-    sru = 1  # needed space for a container is 250MiB
-    farms = j.sal.reservation_chatflow.farm_names_get(1, bot, hru=hru, cru=cru, sru=sru, currency=currency)
-    node = j.sal.reservation_chatflow.nodes_get(1, farm_names=farms, hru=hru, cru=cru, sru=sru, currency=currency)[0]
-    network.add_node(node)
-    ip_address = network.ask_ip_from_node(node, "Please choose your IP Address for this solution")
-    user_form_data["IP Address"] = ip_address
+            self.env.update(var_dict)
 
-    conatiner_flist = user_form_data["Flist link"]
-    storage_url = "zdb://hub.grid.tf:9900"
-    if user_form_data["Interactive"] == "YES":
-        interactive = True
-    else:
-        interactive = False
+    @j.baseclasses.chatflow_step(title="Expiration time")
+    def expiration_time(self):
+        self.expiration = self.datetime_picker(
+            "Please enter solution expiration time.",
+            required=True,
+            min_time=[3600, "Date/time should be at least 1 hour from now"],
+            default=j.data.time.epoch + 3900,
+        )
+        self.user_form_data["Solution expiration"] = j.data.time.secondsToHRDelta(self.expiration - j.data.time.epoch)
 
-    bot.md_show_confirm(user_form_data)
+    @j.baseclasses.chatflow_step(title="Container farm")
+    def container_fram(self):
+        # create new reservation
+        self.reservation = j.sal.zosv2.reservation_create()
+        hru = math.ceil(self.memory.value / 1024)
+        cru = self.cpu.value
+        sru = 1  # needed space for a container is 250MiB
+        farms = j.sal.reservation_chatflow.farm_names_get(1, self, hru=hru, cru=cru, sru=sru, currency=self.currency)
+        self.node = j.sal.reservation_chatflow.nodes_get(
+            1, farm_names=farms, hru=hru, cru=cru, sru=sru, currency=self.currency
+        )[0]
 
-    # update network
-    network.update(j.me.tid, currency=currency, bot=bot)
+    @j.baseclasses.chatflow_step(title="Container IP & Confirmation about conatiner details", disable_previous=True)
+    def container_ip(self):
+        self.network.add_node(self.node)
+        self.ip_address = self.network.ask_ip_from_node(self.node, "Please choose your IP Address for this solution")
+        self.user_form_data["IP Address"] = self.ip_address
 
-    # create container
-    j.sal.zosv2.container.create(
-        reservation=reservation,
-        node_id=node.node_id,
-        network_name=network.name,
-        ip_address=ip_address,
-        flist=conatiner_flist,
-        storage_url=storage_url,
-        env=env,
-        interactive=interactive,
-        entrypoint=user_form_data["Entry point"],
-        cpu=user_form_data["CPU"],
-        memory=user_form_data["Memory"],
-    )
-    metadata = dict()
-    metadata["chatflow"] = user_form_data["chatflow"]
-    metadata["Solution name"] = user_form_data["Solution name"]
-    metadata["Solution expiration"] = user_form_data["Solution expiration"]
+        self.conatiner_flist = self.user_form_data["Flist link"]
+        self.storage_url = "zdb://hub.grid.tf:9900"
+        if self.user_form_data["Interactive"] == "YES":
+            self.interactive = True
+        else:
+            self.interactive = False
 
-    res = j.sal.reservation_chatflow.solution_model_get(
-        user_form_data["Solution name"], "tfgrid.solutions.flist.1", metadata
-    )
-    reservation = j.sal.reservation_chatflow.reservation_metadata_add(reservation, res)
-    resv_id = j.sal.reservation_chatflow.reservation_register_and_pay(
-        reservation, expiration, customer_tid=j.me.tid, currency=currency, bot=bot
-    )
-    j.sal.reservation_chatflow.reservation_save(
-        resv_id, user_form_data["Solution name"], "tfgrid.solutions.flist.1", user_form_data
-    )
+        self.md_show_confirm(self.user_form_data)
 
-    if interactive:
-        res = f"""\
-            # Container has been deployed successfully: your reservation id is: {resv_id}
-            Open your browser at [http://{ip_address}:7681](http://{ip_address}:7681)
-            """
-        bot.md_show(j.core.text.strip(res), md=True)
-    else:
-        res = f"""\
-            # Container has been deployed successfully: your reservation id is: {resv_id}
-            Your IP is  ```{ip_address}```
-            """
-        bot.md_show(j.core.text.strip(res), md=True)
+    @j.baseclasses.chatflow_step(title="Container pay", disable_previous=True)
+    def container_pay(self):
+        # update network
+        self.network.update(j.me.tid, currency=self.currency, bot=self)
+
+        # create container
+        j.sal.zosv2.container.create(
+            reservation=self.reservation,
+            node_id=self.node.node_id,
+            network_name=self.network.name,
+            ip_address=self.ip_address,
+            flist=self.conatiner_flist,
+            storage_url=self.storage_url,
+            env=self.env,
+            interactive=self.interactive,
+            entrypoint=self.user_form_data["Entry point"],
+            cpu=self.user_form_data["CPU"],
+            memory=self.user_form_data["Memory"],
+        )
+        metadata = dict()
+        metadata["chatflow"] = self.user_form_data["chatflow"]
+        metadata["Solution name"] = self.user_form_data["Solution name"]
+        metadata["Solution expiration"] = self.user_form_data["Solution expiration"]
+
+        res = j.sal.reservation_chatflow.solution_model_get(
+            self.user_form_data["Solution name"], "tfgrid.solutions.flist.1", metadata
+        )
+        reservation = j.sal.reservation_chatflow.reservation_metadata_add(self.reservation, res)
+        self.resv_id = j.sal.reservation_chatflow.reservation_register_and_pay(
+            reservation, self.expiration, customer_tid=j.me.tid, currency=self.currency, bot=self
+        )
+        j.sal.reservation_chatflow.reservation_save(
+            self.resv_id, self.user_form_data["Solution name"], "tfgrid.solutions.flist.1", self.user_form_data
+        )
+
+    @j.baseclasses.chatflow_step(title="Container access", disable_previous=True)
+    def container_acess(self):
+        if self.interactive:
+            res = f"""\
+                # Container has been deployed successfully: your reservation id is: {self.resv_id}
+                Open your browser at [http://{self.ip_address}:7681](http://{self.ip_address}:7681)
+                """
+            self.md_show(j.core.text.strip(res), md=True)
+        else:
+            res = f"""\
+                # Container has been deployed successfully: your reservation id is: {resv_id}
+                Your IP is  ```{self.ip_address}```
+                """
+            self.md_show(j.core.text.strip(res), md=True)
+
+
+chat = FlistDeploy
