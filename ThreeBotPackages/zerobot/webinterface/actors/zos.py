5dd99938941b61d68773b05ad3712c15a667894f
ahelsayd <ahmed.m.elsayed93@gmail.com>
2020-05-19 15:41:08 +0200

    address remarks

--- ThreeBotPackages/zerobot/webinterface/actors/zos.py
+++ ThreeBotPackages/zerobot/webinterface/actors/zos.py
@@ -13,31 +13,15 @@ class zos(j.baseclasses.threebot_actor):
         """list farms
         """
         farms = self.explorer.farms.list()
-        return json.dumps([farm.name for farm in farms])
+        return json.dumps([farm._ddict for farm in farms])
         
 
     @j.baseclasses.actor_method
-    def find_nodes(self, farm_name=None, cru=None, sru=None, mru=None, hru=None, schema_out=None, user_session=None):
-        """find nodes
-
-        ```in
-        farm_name = (S)
-        cru = (I)
-        mru = (I)
-        hru = (I)
-        sru = (I)
-        ```
+    def list_nodes(self, schema_out=None, user_session=None):
+        """list nodes
         """
         nodes = []
-        results = self.zos.nodes_finder.nodes_by_capacity(
-            farm_name=farm_name,
-            cru=cru,
-            mru=mru,
-            hru=hru,
-            sru=sru
-        )
-
-        results = filter(self.zos.nodes_finder.filter_is_up, results)
+        results = filter(self.zos.nodes_finder.filter_is_up, self.zos.nodes_finder.nodes_search())
         for result in results:
-            nodes.append(result.node_id)
+            nodes.append(result._ddict)
         return nodes
--- ThreeBotPackages/zerobot/webinterface/chatflows/components/NodeSelector.vue
+++ ThreeBotPackages/zerobot/webinterface/chatflows/components/NodeSelector.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
       <Message :payload="payload"></Message>
-      <v-autocomplete v-model="val" :hint="hint" :rules="rules" :persistent-hint="true" label="Nodes" :items="nodes" validate-on-blur outlined small-chips :multiple="multiple"></v-autocomplete>
+      <v-autocomplete v-model="val" :hint="hint" :rules="rules" :persistent-hint="true" label="Nodes" item-value="node_id" item-text="node_id" :items="filtered" validate-on-blur outlined small-chips :multiple="multiple"></v-autocomplete>
       
       <h3 class="title font-regular mb-3 mt-5">Filter Nodes</h3>
-      <v-autocomplete v-model="filters.farm" label="Farm" :items="farms" validate-on-blur outlined small-chips></v-autocomplete>
+      <v-autocomplete v-model="filters.farm_id" label="Farm" :items="farms" item-value="id" item-text="name" validate-on-blur outlined small-chips></v-autocomplete>
       <v-slider class="mt-5" label="CRU" v-model="filters.cru" max="64" thumb-label="always"></v-slider>
       <v-slider class="mt-3" label="MRU" v-model="filters.mru" max="512" thumb-label="always"></v-slider>
       <v-slider class="mt-3" label="HRU" v-model="filters.hru" max="5000" thumb-label="always"></v-slider>
@@ -26,49 +26,43 @@
           mru: null,
           hru: null,
           sru: null,
-          country: null,
-          farm: null
+          farm_id: null
         },
         nodes: [],
         farms: []
       }
     },
-    watch: {
-      filters: {
-        handler () {
-          this.findNodes()
-        },
-        deep: true
-      }
-    },
     computed: {
       hint () {
         if (this.multiple) {
-          return `${this.nodes.length} Nodes, ${this.val.length} Selected`
+          return `${this.filtered.length} Nodes, ${this.val.length} Selected`
         } else {
-          return `${this.nodes.length} Nodes, ${this.val.length ? 1 : 0} Selected`
+          return `${this.filtered.length} Nodes, ${this.val && this.val.length ? 1 : 0} Selected`
         }
+      },
+      filtered () {
+        return this.nodes.filter((node) => {
+          if (this.filters.farm_id && node.farm_id !== this.filters.farm_id) return false
+          if (node.total_resources.cru - node.used_resources.cru < this.filters.cru) return false
+          if (node.total_resources.mru - node.used_resources.mru < this.filters.mru) return false
+          if (node.total_resources.sru - node.used_resources.sru < this.filters.sru) return false
+          if (node.total_resources.hru - node.used_resources.hru < this.filters.hru) return false
+          return true
+        })
       },
       multiple () {
         return this.payload.kwargs.multiple === true
       }
     },
     methods: {
-      findNodes () {
+      getNodes () {
         axios({
-          url: `${baseUrl}/zos/find_nodes`,
-          params: {
-            farm_name: this.filters.farm,
-            cru: this.filters.cru,
-            mru: this.filters.mru,
-            hru: this.filters.hru,
-            sru: this.filters.sru
-          }
+          url: `${baseUrl}/zos/list_nodes`,
         }).then((response) => {
           this.nodes = response.data
         })
       },
-      get_farms () {
+      getFarms () {
         axios({
           url: `${baseUrl}/zos/list_farms`,
         }).then((response) => {
@@ -77,7 +71,8 @@
       }
     },
     mounted () {
-      this.get_farms()
+      this.getFarms()
+      this.getNodes()
     }
   }
 </script>
