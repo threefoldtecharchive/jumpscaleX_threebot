80b8c2795cdcbda5bf31cf84e0b10498b3cba9fd
Rana Tarek Hassan <tarekr@codescalers.com>
2020-05-31 18:42:31 +0200

    Add support in minio chatflow for master/slave setup

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/minio_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/minio_deploy.py
@@ -7,6 +7,7 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         "deployment_start",
         "network_selection",
         "solution_name",
+        "setup_type",
         "storage_type",
         "access_credentials",
         "container_resources",
@@ -37,6 +38,15 @@ class MinioDeploy(j.servers.chatflow.get_class()):
     def solution_name(self):
         model = j.threebot.packages.tfgrid_solutions.tfgrid_solutions.bcdb_model_get("tfgrid.solutions.minio.1")
         self.user_form_data["Solution name"] = j.sal.reservation_chatflow.solution_name_add(self, model)
+
+    @j.baseclasses.chatflow_step(title="Setup type")
+    def setup_type(self):
+        self.user_form_data["Setup type"] = self.drop_down_choice(
+            "Please choose the type of setup you need. Single setup is the basic setup while master/slave setup includes TLOG use to be able to reconstruct the metadata",
+            ["Single Setup", "Master/Slave Setup"],
+            required=True,
+            default="Single Setup",
+        )
 
     @j.baseclasses.chatflow_step(title="Storage")
     def storage_type(self):
@@ -87,6 +97,11 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         self.user_form_data["Locations"] = int(data_number.value)
         self.user_form_data["Locations allowed to fail"] = int(parity.value)
         self.user_form_data["ZDB number"] = int(data_number.value) + int(parity.value)
+        if self.user_form_data["Setup type"] == "Single Setup":
+            self.number_of_minio_nodes = 1
+        elif self.user_form_data["Setup type"] == "Master/Slave Setup":
+            self.user_form_data["ZDB number"] += 1
+            self.number_of_minio_nodes = 2
 
     @j.baseclasses.chatflow_step(title="Expiration")
     def expiration_datetime(self):
@@ -122,7 +137,12 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         nodequery["cru"] = self.user_form_data["CPU"]
         nodequery["sru"] = 1
         cont_farms = j.sal.reservation_chatflow.farm_names_get(1, self, message="minio container", **nodequery)
-        self.cont_node = j.sal.reservation_chatflow.nodes_get(number_of_nodes=1, farm_names=cont_farms, **nodequery)[0]
+        minio_nodes = j.sal.reservation_chatflow.nodes_get(
+            number_of_nodes=self.number_of_minio_nodes, farm_names=cont_farms, **nodequery
+        )
+        self.cont_node = minio_nodes[0]
+        if self.user_form_data["Setup type"] == "Master/Slave Setup" and len(minio_nodes) > 1:
+            self.slave_node = minio_nodes[1]
 
     @j.baseclasses.chatflow_step(title="Minio container IP")
     def ip_selection(self):
@@ -137,6 +157,11 @@ class MinioDeploy(j.servers.chatflow.get_class()):
             self.cont_node, "Please choose IP Address for your solution"
         )
         self.user_form_data["IP Address"] = self.ip_address
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            self.slave_ip_address = self.network_copy.ask_ip_from_node(
+                self.slave_node, "Please choose IP Address for your backup (slave)"
+            )
+            self.user_form_data["Slave IP Address"] = self.slave_ip_address
 
     @j.baseclasses.chatflow_step(title="Confirmation")
     def overview(self):
@@ -164,6 +189,10 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         self.volume = j.sal.zosv2.volume.create(
             self.reservation, self.cont_node.node_id, size=10, type=self.user_form_data["Disk type"]
         )
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            self.slave_volume = j.sal.zosv2.volume.create(
+                self.reservation, self.slave_node.node_id, size=10, type=self.user_form_data["Disk type"]
+            )
 
         # register the reservation for zdb db
         self.zdb_rid = j.sal.reservation_chatflow.reservation_register_and_pay(
@@ -177,21 +206,25 @@ class MinioDeploy(j.servers.chatflow.get_class()):
     @j.baseclasses.chatflow_step(title="Reserve minio container", disable_previous=True)
     def minio_reservation(self):
         # read the IP address of the 0-db namespaces after they are deployed to be used in the creation of the minio container
-        namespace_config = []
+        self.namespace_config = []
         for result in self.reservation_result:
             if result.category == "ZDB":
                 data = result.data_json
                 cfg = f"{data['Namespace']}:{self.password}@[{data['IP']}]:{data['Port']}"
-                namespace_config.append(cfg)
+                self.namespace_config.append(cfg)
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            self.tlog_access = self.namespace_config.pop(-1)
 
-        entry_point = "/bin/entrypoint"
-        flist_url = "https://hub.grid.tf/tf-official-apps/minio-2020-01-25T02-50-51Z.flist"
+        flist_url = "https://hub.grid.tf/tf-official-apps/minio:latest.flist"
 
         minio_secret_encrypted = j.sal.zosv2.container.encrypt_secret(
             self.cont_node.node_id, self.user_form_data["Secret"]
         )
-        shards_encrypted = j.sal.zosv2.container.encrypt_secret(self.cont_node.node_id, ",".join(namespace_config))
+        shards_encrypted = j.sal.zosv2.container.encrypt_secret(self.cont_node.node_id, ",".join(self.namespace_config))
         secret_env = {"SHARDS": shards_encrypted, "SECRET_KEY": minio_secret_encrypted}
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            tlog_access_encrypted = j.sal.zosv2.container.encrypt_secret(self.cont_node.node_id, self.tlog_access)
+            secret_env["TLOG"] = tlog_access_encrypted
 
         # create container
         cont = j.sal.zosv2.container.create(
@@ -200,7 +233,7 @@ class MinioDeploy(j.servers.chatflow.get_class()):
             network_name=self.network.name,
             ip_address=self.ip_address,
             flist=flist_url,
-            entrypoint=entry_point,
+            entrypoint="",
             cpu=self.user_form_data["CPU"],
             memory=self.user_form_data["Memory"],
             env={
@@ -214,6 +247,39 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         j.sal.zosv2.volume.attach_existing(
             container=cont, volume_id=f"{self.zdb_rid}-{self.volume.workload_id}", mount_point="/data"
         )
+
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            slave_secret_encrypted = j.sal.zosv2.container.encrypt_secret(
+                self.slave_node.node_id, self.user_form_data["Secret"]
+            )
+            slave_shards_encrypted = j.sal.zosv2.container.encrypt_secret(
+                self.slave_node.node_id, ",".join(self.namespace_config)
+            )
+            tlog_access_encrypted = j.sal.zosv2.container.encrypt_secret(self.slave_node.node_id, self.tlog_access)
+            secret_env = {
+                "SHARDS": slave_shards_encrypted,
+                "SECRET_KEY": slave_secret_encrypted,
+                "MASTER": tlog_access_encrypted,
+            }
+            slave_cont = j.sal.zosv2.container.create(
+                reservation=self.reservation,
+                node_id=self.slave_node.node_id,
+                network_name=self.network.name,
+                ip_address=self.slave_ip_address,
+                flist=flist_url,
+                entrypoint="",
+                cpu=self.user_form_data["CPU"],
+                memory=self.user_form_data["Memory"],
+                env={
+                    "DATA": str(self.user_form_data["Locations"]),
+                    "PARITY": str(self.user_form_data["Locations allowed to fail"]),
+                    "ACCESS_KEY": self.user_form_data["Access key"],
+                },
+                secret_env=secret_env,
+            )
+            j.sal.zosv2.volume.attach_existing(
+                container=slave_cont, volume_id=f"{self.zdb_rid}-{self.slave_volume.workload_id}", mount_point="/data"
+            )
 
         res = j.sal.reservation_chatflow.solution_model_get(
             self.user_form_data["Solution name"], "tfgrid.solutions.minio.1", self.user_form_data
@@ -231,6 +297,10 @@ class MinioDeploy(j.servers.chatflow.get_class()):
         res = f"""\
             # Minio cluster has been deployed successfully. Your reservation id is: {self.resv_id}
             Open your browser at [http://{self.ip_address}:9000](http://{self.ip_address}:9000). It may take a few minutes.
+            """
+        if self.user_form_data["Setup type"] == "Master/Slave Setup":
+            res += f"""\
+            You can access the slave machine at [http://{self.slave_ip_address}:9000](http://{self.slave_ip_address}:9000)
             """
         self.md_show(j.core.text.strip(res))
 
