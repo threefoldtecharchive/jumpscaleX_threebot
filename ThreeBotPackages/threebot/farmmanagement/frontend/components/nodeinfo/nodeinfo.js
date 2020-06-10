6121737a21f9af5549815c7fbfb60e3f200082a3
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-06-10 16:12:30 +0200

    farm management: show CPR in node info

--- ThreeBotPackages/threebot/farmmanagement/frontend/components/nodeinfo/nodeinfo.js
+++ ThreeBotPackages/threebot/farmmanagement/frontend/components/nodeinfo/nodeinfo.js
@@ -1,5 +1,6 @@
 module.exports = new Promise(async (resolve, reject) => {
   const vuex = await import("/weblibs/vuex/vuex.esm.browser.js");
+  
   resolve({
     name: 'nodeinfo',
     props: ['node'],
@@ -16,14 +17,27 @@ module.exports = new Promise(async (resolve, reject) => {
         this.node.free_to_use = false;
       }
     },
+    computed: {
+      cpr_total: function () {
+        const cu = this.ru2cu(this.node.totalResources)
+        return 1.5 * cu.cu + cu.su
+      },
+    },
     methods: {
       ...vuex.mapActions("farmmanagement", ["setNodeFree"]),
+
+      ru2cu(r){
+        const cu = {
+          cu : Math.round(Math.min(r.mru/4*0.95, r.cru * 2) * 1000) / 1000,
+          su : Math.round(( r.hru / 1093 + r.sru / 91) * 1000) / 1000,
+        }
+        return cu
+      },
 
       getPercentage(type) {
           const reservedResources = this.node.reservedResources[type]
           const totalResources = this.node.totalResources[type]
           if (reservedResources === 0 && totalResources === 0) return 0
-          return (reservedResources / totalResources) * 100
       },
 
       async setFree() {
