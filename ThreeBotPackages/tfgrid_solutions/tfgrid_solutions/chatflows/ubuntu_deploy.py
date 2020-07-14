73611021cb4743fd0de429d02acbc9efb052d7d3
m-motawea <magedmotawea@gmail.com>
2020-07-14 17:25:31 +0200

    fix ubuntu metadata

--- ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/ubuntu_deploy.py
+++ ThreeBotPackages/tfgrid_solutions/tfgrid_solutions/chatflows/ubuntu_deploy.py
@@ -121,7 +121,10 @@ class UbuntuDeploy(j.servers.chatflow.get_class()):
     @j.baseclasses.chatflow_step(title="Reservation")
     def reservation(self):
         container_flist = f"{self.HUB_URL}/3bot-{self.version}.flist"
-        metadata = {"SolutionType": "ubuntu", "SolutionName": self.solution_name}
+        metadata = {
+            "name": self.solution_name,
+            "form_info": {"chatflow": "flist", "Solution name": self.solution_name},
+        }
         self.resv_id = j.sal.chatflow_deployer.deploy_container(
             pool_id=self.pool_id,
             node_id=self.selected_node.node_id,
